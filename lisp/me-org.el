ef87d475875f02b0547d77dbd39489631863ca43
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-02 18:31:38 +0200

    Update org module

--- lisp/me-org.el
+++ lisp/me-org.el
@@ -19,65 +19,38 @@
         org-preview-latex-image-directory (expand-file-name "org/latex/" minemacs-etc-dir)
         org-list-allow-alphabetical t)
   :config
-  (message "Org is loaded!"))
+  (defvar +org-responsive-image-percentage 0.4)
+  (defvar +org-responsive-image-width-limits '(400 . 700)) ;; '(min . max)
 
-(use-package org-roam
-  :straight t
-  :requires org
-  :commands (org-roam
-             org-roam-ref-find
-             org-roam-node-find
-             org-roam-node-open
-             org-roam-node-insert
-             org-roam-node-random))
+  (defun +org--responsive-image-h ()
+    (when (derived-mode-p 'org-mode)
+      (setq-local
+       org-image-actual-width
+       (max (car +org-responsive-image-width-limits)
+            (min (cdr +org-responsive-image-width-limits)
+                 (truncate (* (window-pixel-width)
+                              +org-responsive-image-percentage)))))))
 
-;; (use-package websocket
-;;   :straight t
-;;   :after org-roam-ui)
+  (add-hook 'window-configuration-change-hook
+            #'+org--responsive-image-h))
 
-(use-package org-roam-ui
-  :straight t
-  :commands (org-roam-ui-open org-roam-ui-sync-mode)
-  :after org-roam
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
-
-;; From https://org-roam.discourse.group/t/configure-deft-title-stripping-to-hide-org-roam-template-headers/478/10
-;; (use-package deft
-;;   :after org
-;;   :bind ("C-c n d" . deft)
-;;   :init
-;;   (setq deft-directory org-roam-directory
-;;         deft-recursive t
-;;         deft-use-filter-string-for-filename t
-;;         deft-default-extension "org")
-;;   :config
-;;   (defun +deft-parse-title (file contents)
-;;     "Parse the given FILE and CONTENTS and determine the title.
-;;      If `deft-use-filename-as-title' is nil, the title is taken to
-;;      be the first non-empty line of the FILE.  Else the base name of the FILE is
-;;      used as title."
-;;     (let ((begin (string-match "^#\\+[tT][iI][tT][lL][eE]: .*$" contents)))
-;;       (if begin
-;;           (string-trim (substring contents begin (match-end 0)) "#\\+[tT][iI][tT][lL][eE]: *" "[\n\t ]+")
-;;         (deft-base-filename file))))
-
-;;   (advice-add 'deft-parse-title :override #'+deft-parse-title)
-
-;;   (setq deft-strip-summary-regexp
-;;         (concat "\\("
-;;                 "[\n\t]" ;; blank
-;;                 "\\|^#\\+[[:alpha:]_]+:.*$" ;; org-mode metadata
-;;                 "\\|^:PROPERTIES:\n\\(.+\n\\)+:END:\n" ;; org-roam ID
-;;                 "\\|\\[\\[\\(.*\\]\\)" ;; any link
-;;                 "\\)")))
 
 (use-package org-contrib
   :straight t
   :after org)
+
+
+(use-package org-appear
+  :straight t
+  :hook (org-mode . org-appear-mode)
+  :config
+  (setq org-appear-autoemphasis t
+        org-appear-autosubmarkers t
+        org-appear-autolinks nil)
+  ;; for proper first-time setup, `org-appear--set-elements'
+  ;; needs to be run after other hooks have acted.
+  (run-at-time nil nil #'org-appear--set-elements))
+
 
 (use-package org-modern
   :straight t
