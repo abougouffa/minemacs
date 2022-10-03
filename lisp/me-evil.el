eaa78d04205924dd17a2350916108ef0a9872b95
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-04 01:40:03 +0200

    fix: evil-mc keybindings

--- lisp/me-evil.el
+++ lisp/me-evil.el
@@ -22,42 +22,41 @@
 
 
 (use-package evil-nerd-commenter
-  :after (evil minemacs-loaded)
+  :after evil minemacs-loaded
   :straight t)
 
 
 (use-package evil-mc
   :straight t
-  :after evil
-  :general
+  :after evil evil-collection
+  :config
+  ;; (evil-collection-swap-key nil 'evil-normal-state-map
+  ;;   "gz" "gZ")
   (me-map-def
     :states '(normal visual)
     :keymaps 'evil-mc-key-map
-    "gz" '(nil :which-key "evil-mc")
-    "gzd" #'evil-mc-make-and-goto-next-match
-    "gzD" #'evil-mc-make-and-goto-prev-match
-    "gzs" #'evil-mc-skip-and-goto-next-match
-    "gzS" #'evil-mc-skip-and-goto-prev-match
-    "gzc" #'evil-mc-skip-and-goto-next-cursor
-    "gzC" #'evil-mc-skip-and-goto-prev-cursor
-    "gzj" #'evil-mc-make-cursor-move-next-line
-    "gzk" #'evil-mc-make-cursor-move-prev-line
-    "gzm" #'evil-mc-make-all-cursors
-    "gzn" #'evil-mc-make-and-goto-next-cursor
-    "gzN" #'evil-mc-make-and-goto-last-cursor
-    "gzp" #'evil-mc-make-and-goto-prev-cursor
-    "gzP" #'evil-mc-make-and-goto-first-cursor
-    "gzq" #'evil-mc-undo-all-cursors)
+    :prefix "gz"
+    "" '(nil :which-key "evil-mc")
+    "d" #'evil-mc-make-and-goto-next-match
+    "D" #'evil-mc-make-and-goto-prev-match
+    "s" #'evil-mc-skip-and-goto-next-match
+    "S" #'evil-mc-skip-and-goto-prev-match
+    "c" #'evil-mc-skip-and-goto-next-cursor
+    "C" #'evil-mc-skip-and-goto-prev-cursor
+    "j" #'evil-mc-make-cursor-move-next-line
+    "k" #'evil-mc-make-cursor-move-prev-line
+    "m" #'evil-mc-make-all-cursors
+    "n" #'evil-mc-make-and-goto-next-cursor
+    "N" #'evil-mc-make-and-goto-last-cursor
+    "p" #'evil-mc-make-and-goto-prev-cursor
+    "P" #'evil-mc-make-and-goto-first-cursor
+    "q" #'evil-mc-undo-all-cursors)
   (me-map-def
     :states 'visual
     :keymaps 'evil-mc-key-map
-    "gzi" #'evil-mc-make-cursor-in-visual-selection-beg
-    "gza" #'evil-mc-make-cursor-in-visual-selection-end)
-  :config
-  (add-hook 'prog-mode-hook 'turn-on-evil-mc-mode)
-  (add-hook 'text-mode-hook 'turn-on-evil-mc-mode)
-  (add-hook 'magit-mode-hook 'turn-off-evil-mc-mode)
-
+    :prefix "gz"
+    "i" #'evil-mc-make-cursor-in-visual-selection-beg
+    "a" #'evil-mc-make-cursor-in-visual-selection-end)
   ;; Add support to repeat these commands when prefixed with a number
   (dolist (cmd '(evil-mc-make-and-goto-first-cursor
                  evil-mc-make-and-goto-last-cursor
@@ -73,7 +72,9 @@
      cmd :around
      (lambda (fn)
        (dotimes (i (if (integerp current-prefix-arg) current-prefix-arg 1))
-         (funcall fn))))))
+         (funcall fn)))))
+  ;; Enable globally
+  (global-evil-mc-mode 1))
 
 (use-package evil-escape
   :after (evil minemacs-loaded)
