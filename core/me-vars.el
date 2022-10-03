bc3fdc3a0de286a0ea55e23b5b452db97b060eda
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-04 01:11:24 +0200

    feat: clean vars

--- core/me-vars.el
+++ core/me-vars.el
@@ -1,11 +1,12 @@
 ;; -*- lexical-binding: t; -*-
 
 ;;; MinEmacs directories
-(defvar minemacs-config-dir (or (getenv "MINEMACS_DIR")
-                                ;; Temporary use this
-                                (when (file-directory-p user-emacs-directory)
-                                  user-emacs-directory)
-                                (expand-file-name "~/.minemacs.d/")))
+(defvar minemacs-config-dir
+  (or (getenv "MINEMACS_DIR")
+      ;; Temporary use this
+      (when (file-directory-p user-emacs-directory)
+        user-emacs-directory)
+      (expand-file-name "~/.minemacs.d/")))
 
 (unless (file-exists-p minemacs-config-dir)
   (mkdir minemacs-config-dir t))
@@ -14,18 +15,23 @@
 (defvar minemacs-var-dir (expand-file-name "var/" user-emacs-directory))
 (defvar minemacs-cache-dir (expand-file-name "cache/" minemacs-var-dir))
 
-(defvar minemacs-lazy-funs '()
-  "A list of lazy functions, will be executed after `minemacs-loaded' gets triggered.
-i.e. After loading all the configs.")
-
-;; This will be set by the virtual package `minemacs-loaded'
-(defvar minemacs-loaded-p nil
-  "MinEmacs has been loaded.")
-
 (defconst LINUX-P (memq system-type '(gnu gnu/linux)))
 (defconst BSD-P (memq system-type '(darwin berkeley-unix)))
 (defconst WIN-P (memq system-type '(cygwin windwos-nt ms-dos)))
 (defconst MAC-P (eq system-type 'darwin))
 
+(defcustom minemacs-after-startup nil
+  "This hook will be run after loading Emacs."
+  :group 'minemacs
+  :type 'hook)
+
+(defcustom minemacs-first-file nil
+  "This hook will be fired when the first file is open."
+  :group 'minemacs
+  :type 'hook)
+
+;; This will be set by the virtual package `minemacs-loaded'
+(defvar minemacs-loaded-p nil
+  "MinEmacs has been loaded.")
 
 (provide 'me-vars)
