1fe29cf033824eaf5901eb62254ba87fab875ce2
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-03 02:44:30 +0200

    tweak: use Zstd for undo-fu-session

--- lisp/me-editor.el
+++ lisp/me-editor.el
@@ -5,19 +5,17 @@
   :straight t
   :general
   (me-global-def "ov" '(vundo :which-key "Visual Undo"))
-  :init
-  (defconst +vundo-unicode-symbols
-    '((selected-node   . ?●)
-      (node            . ?○)
-      (vertical-stem   . ?│)
-      (branch          . ?├)
-      (last-branch     . ?╰)
-      (horizontal-stem . ?─)))
-
   :config
-  (setq vundo-glyph-alist +vundo-unicode-symbols
-        vundo-compact-display t
-        vundo-window-max-height 6))
+  (setq vundo-compact-display t
+        vundo-window-max-height 6
+        vundo-glyph-alist
+        '((selected-node   . ?●)
+          (node            . ?○)
+          (vertical-stem   . ?│)
+          (branch          . ?├)
+          (last-branch     . ?╰)
+          (horizontal-stem . ?─))))
+
 
 (use-package undo-fu
   :straight t
@@ -25,10 +23,13 @@
   (with-eval-after-load 'evil
     (evil-set-undo-system 'undo-fu)))
 
+
 (use-package undo-fu-session
   :straight t
   :after undo-fu
   :config
+  (setq undo-fu-session-compression 'zst
+        undo-fu-session-directory (expand-file-name "undo-fu-session" minemacs-var-dir))
   (global-undo-fu-session-mode 1))
 
 
