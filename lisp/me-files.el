e4acd5342760e12ef2a4e2371fff70e985e75f60
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-04 15:05:10 +0200

    feat: add dirvish support

---
+++ lisp/me-files.el
@@ -0,0 +1,30 @@
+;; -*- lexical-binding: t; -*-
+
+
+(use-package dirvish
+  :straight t
+  :after minemacs-loaded
+  :general
+  (me-global-def
+    ;; Open
+    "o-"  '(dirvish :which-key "Dirvish")
+    "op"  '(dirvish-side :which-key "Side panel")
+    "oq"  '(dirvish-quick-access :which-key "Dirvish quick access")
+    ;; Search
+    "sd"  '(dirvish-fd :which-key "Dirvish fd"))
+  :config
+  (require 'dirvish-icons)
+  (setq dirvish-cache-dir (expand-file-name "dirvish/" minemacs-cache-dir)
+        dirvish-fd-default-dir (expand-file-name "~")
+        dirvish-mode-line-format
+        '(:left (sort file-time symlink) :right (omit yank index)))
+  ;; Set attributes
+  (setq dirvish-attributes
+        (delete-dups
+         (append
+          dirvish-attributes
+          '(vc-state git-msg all-the-icons))))
+  (dirvish-override-dired-mode))
+
+
+(provide 'me-files)
