a1a2f1ae3110f143acd2965fd2a9b7b97bf8bb84
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-04 20:58:18 +0200

    feat: launch mu4e in background in daemon mode

--- lisp/me-daemon.el
+++ lisp/me-daemon.el
@@ -13,5 +13,26 @@
           (me-info! "Starting Emacs daemon in background.")
           (server-start)))))))
 
+(defun me-daemon-startup ()
+  ;; mu4e
+  (when (require 'mu4e nil t)
+    ;; Automatically start `mu4e' in background.
+    (run-at-time
+     (* 60 1) ;; Launch after 1m
+     (* 60 3) ;; Check each 3m
+     (lambda ()
+       (unless (mu4e-running-p)
+         (let ((inhibit-message nil))
+           (mu4e--start)
+           (me-info! "Started `mu4e' in background."))))))
+
+  ;; RSS
+  (when (and (require 'elfeed nil t) nil)
+    (run-at-time nil (* 2 60 60) #'elfeed-update))) ;; Check every 2h
+
+(when (daemonp)
+  ;; At daemon startup
+  (add-hook 'emacs-startup-hook #'me-daemon-startup))
+
 
 (provide 'me-daemon)
