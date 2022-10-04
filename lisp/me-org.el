e2cd34de668429bce7522f6c8344af98b0691dbc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-04 21:00:01 +0200

    tweak: add keybindings for Org mode

--- lisp/me-org.el
+++ lisp/me-org.el
@@ -7,6 +7,12 @@
 (use-package org
   :straight t
   :after minemacs-loaded ;; load Org after finishing Emacs startup
+  :general
+  (me-local-def :keymaps 'org-mode-map
+    "l"  '(nil :which-key "link")
+    "ll" '(org-insert-link :which-key "Insert link")
+    "e"  '(nil :which-key "export")
+    "ee" '(org-export-dispatch :which-key "Export dispatch"))
   :preface
   ;; Set to nil so we can detect user changes (in config.el)
   (defvar org-directory nil)
