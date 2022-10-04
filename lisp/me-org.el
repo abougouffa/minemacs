4b0e9b065a2afcb468e5ce216cc08943261a8ca8
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-05 00:30:00 +0200

    tweak: org-mode related stuff

--- lisp/me-org.el
+++ lisp/me-org.el
@@ -1,4 +1,4 @@
-;;; org.el --- Org related stuff -*- lexical-binding: t; -*-
+;;; me-org.el --- Org related stuff -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2022  Abdelhak Bougouffa
 
@@ -11,8 +11,8 @@
   (me-local-def :keymaps 'org-mode-map
     "l"  '(nil :which-key "link")
     "ll" '(org-insert-link :which-key "Insert link")
-    "e"  '(nil :which-key "export")
-    "ee" '(org-export-dispatch :which-key "Export dispatch"))
+    "e" '(org-export-dispatch :which-key "Export dispatch"))
+
   :preface
   ;; Set to nil so we can detect user changes (in config.el)
   (defvar org-directory nil)
@@ -26,6 +26,7 @@
   (let ((dir (expand-file-name "org/" minemacs-cache-dir)))
     (unless (file-directory-p dir)
       (mkdir dir t)))
+
   :config
   (setq org-use-property-inheritance t ; it's convenient to have properties inherited
         org-log-done 'time             ; having the time an item is done sounds convenient
@@ -80,7 +81,110 @@
                               +org-responsive-image-percentage)))))))
 
   (add-hook 'window-configuration-change-hook
-            #'+org--responsive-image-h))
+            #'+org--responsive-image-h)
+
+  (defun +parse-the-fun (str)
+    "Parse the LaTeX environment STR.
+  Return an AST with newlines counts in each level."
+    (let (ast)
+      (with-temp-buffer
+        (insert str)
+        (goto-char (point-min))
+        (while (re-search-forward
+                (rx "\\"
+                    (group (or "\\" "begin" "end" "nonumber"))
+                    (zero-or-one "{" (group (zero-or-more not-newline)) "}"))
+                nil t)
+          (let ((cmd (match-string 1))
+                (env (match-string 2)))
+            (cond ((string= cmd "begin")
+                   (push (list :env (intern env)) ast))
+                  ((string= cmd "\\")
+                   (let ((curr (pop ast)))
+                     (push (plist-put curr :newline (1+ (or (plist-get curr :newline) 0))) ast)))
+                  ((string= cmd "nonumber")
+                   (let ((curr (pop ast)))
+                     (push (plist-put curr :nonumber (1+ (or (plist-get curr :nonumber) 0))) ast)))
+                  ((string= cmd "end")
+                   (let ((child (pop ast))
+                         (parent (pop ast)))
+                     (push (plist-put parent :childs (cons child (plist-get parent :childs))) ast)))))))
+      (plist-get (car ast) :childs)))
+
+  (defun +scimax-org-renumber-environment (orig-func &rest args)
+     "A function to inject numbers in LaTeX fragment previews."
+     (let ((results '())
+           (counter -1))
+       (setq results
+             (cl-loop for (begin . env) in
+                      (org-element-map (org-element-parse-buffer) 'latex-environment
+                        (lambda (env)
+                          (cons
+                           (org-element-property :begin env)
+                           (org-element-property :value env))))
+                      collect
+                      (cond
+                       ((and (string-match "\\\\begin{equation}" env)
+                             (not (string-match "\\\\tag{" env)))
+                        (cl-incf counter)
+                        (cons begin counter))
+                       ((string-match "\\\\begin{align}" env)
+                        (cl-incf counter)
+                        (let ((p (car (+parse-the-fun env))))
+                          ;; Parse the `env', count new lines in the align env as equations, unless
+                          (cl-incf counter (- (or (plist-get p :newline) 0)
+                                              (or (plist-get p :nonumber) 0))))
+                        (cons begin counter))
+                       (t
+                        (cons begin nil)))))
+       (when-let ((number (cdr (assoc (point) results))))
+         (setf (car args)
+               (concat
+                (format "\\setcounter{equation}{%s}\n" number)
+                (car args)))))
+     (apply orig-func args))
+
+  (defun +scimax-toggle-latex-equation-numbering (&optional enable)
+    "Toggle whether LaTeX fragments are numbered."
+    (interactive)
+    (if (or enable (not (get '+scimax-org-renumber-environment 'enabled)))
+        (progn
+          (advice-add 'org-create-formula-image :around #'+scimax-org-renumber-environment)
+          (put '+scimax-org-renumber-environment 'enabled t)
+          (message "LaTeX numbering enabled."))
+      (advice-remove 'org-create-formula-image #'+scimax-org-renumber-environment)
+      (put '+scimax-org-renumber-environment 'enabled nil)
+      (message "LaTeX numbering disabled.")))
+
+  (defun +scimax-org-inject-latex-fragment (orig-func &rest args)
+    "Advice function to inject latex code before and/or after the equation in a latex fragment.
+  You can use this to set \\mathversion{bold} for example to make
+  it bolder. The way it works is by defining
+  :latex-fragment-pre-body and/or :latex-fragment-post-body in the
+  variable `org-format-latex-options'. These strings will then be
+  injected before and after the code for the fragment before it is
+  made into an image."
+    (setf (car args)
+          (concat
+           (or (plist-get org-format-latex-options :latex-fragment-pre-body) "")
+           (car args)
+           (or (plist-get org-format-latex-options :latex-fragment-post-body) "")))
+    (apply orig-func args))
+
+  (defun +scimax-toggle-inject-latex ()
+    "Toggle whether you can insert latex in fragments."
+    (interactive)
+    (if (not (get '+scimax-org-inject-latex-fragment 'enabled))
+        (progn
+          (advice-add 'org-create-formula-image :around #'+scimax-org-inject-latex-fragment)
+          (put '+scimax-org-inject-latex-fragment 'enabled t)
+          (message "Inject latex enabled"))
+      (advice-remove 'org-create-formula-image #'+scimax-org-inject-latex-fragment)
+      (put '+scimax-org-inject-latex-fragment 'enabled nil)
+      (message "Inject latex disabled")))
+
+  ;; Enable renumbering by default
+  (+scimax-toggle-latex-equation-numbering t))
 
 
 (use-package org-contrib
@@ -125,25 +229,25 @@
           ("options"             . "⌥")
           ("startup"             . "⏻")
           ("macro"               . "𝓜")
-          ("bind"                . #("" 0 1 (display (raise -0.1))))
+          ("bind"                . "")
           ("bibliography"        . "")
-          ("print_bibliography"  . #("" 0 1 (display (raise -0.1))))
+          ("print_bibliography"  . "")
           ("cite_export"         . "⮭")
-          ("print_glossary"      . #("ᴬᶻ" 0 1 (display (raise -0.1))))
-          ("glossary_sources"    . #("" 0 1 (display (raise -0.14))))
+          ("print_glossary"      . "ᴬᶻ")
+          ("glossary_sources"    . "ᴬᶻ")
           ("export_file_name"    . "⇒")
           ("include"             . "⇤")
           ("setupfile"           . "⇐")
           ("html_head"           . "🅷")
           ("html"                . "🅗")
           ("latex_class"         . "🄻")
-          ("latex_class_options" . #("🄻" 1 2 (display (raise -0.14))))
+          ("latex_class_options" . "🄻ⓞ")
           ("latex_header"        . "🅻")
           ("latex_header_extra"  . "🅻⁺")
           ("latex"               . "🅛")
           ("beamer_theme"        . "🄱")
-          ("beamer_color_theme"  . #("🄱" 1 2 (display (raise -0.12))))
-          ("beamer_font_theme"   . "🄱𝐀")
+          ("beamer_color_theme"  . "🄱ⓣ")
+          ("beamer_font_theme"   . "🄱ⓕ")
           ("beamer_header"       . "🅱")
           ("beamer"              . "🅑")
           ("attr_latex"          . "🄛")
@@ -152,17 +256,23 @@
           ("name"                . "⁍")
           ("header"              . "›")
           ("caption"             . "☰")
-          ("RESULTS"             . "🠶")
+          ("RESULTS"             . "➤")
           ("language"            . "𝙇")
           ("hugo_base_dir"       . "𝐇")
           ("latex_compiler"      . "⟾")
-          ("results"             . "🠶")
+          ("results"             . "➤")
           ("filetags"            . "#")
           ("created"             . "⏱")
           ("export_select_tags"  . "✔")
-          ("export_exclude_tags" . "❌"))))
+          ("export_exclude_tags" . "⛌"))))
+
+
+;; for latex fragments
+(use-package org-fragtog
+  :straight t
+  :hook (org-mode . org-fragtog-mode))
 
 
 (provide 'me-org)
 
-;;; org.el ends here
+;;; me-org.el ends here
