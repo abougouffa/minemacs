3ae323c7a9189d71a063d3d6f7d97c852f2d008c
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-04 18:45:00 +0200

    feat: edit files as root (from Doom)

--- core/me-utils.el
+++ core/me-utils.el
@@ -239,6 +239,52 @@ current buffer's, reload dir-locals."
     (message "Dir-locals will be reloaded after saving.")
     (add-hook 'after-save-hook 'me-dir-locals-reload-for-all-buffers-in-this-directory nil t)))
 
+(defun me--sudo-file-path (file)
+  (let ((host (or (file-remote-p file 'host) "localhost")))
+    (concat "/" (when (file-remote-p file)
+                  (concat (file-remote-p file 'method) ":"
+                          (if-let (user (file-remote-p file 'user))
+                              (concat user "@" host)
+                            host)
+                          "|"))
+            "sudo:root@" host
+            ":" (or (file-remote-p file 'localname)
+                    file))))
+
+;;;###autoload
+(defun me-sudo-find-file (file)
+  "Open FILE as root."
+  (interactive "FOpen file as root: ")
+  (find-file (me--sudo-file-path file)))
+
+;;;###autoload
+(defun me-sudo-this-file ()
+  "Open the current file as root."
+  (interactive)
+  (find-file
+   (me--sudo-file-path
+    (or buffer-file-name
+        (when (or (derived-mode-p 'dired-mode)
+                  (derived-mode-p 'wdired-mode))
+          default-directory)))))
+
+;;;###autoload
+(defun me-sudo-save-buffer ()
+  "Save this file as root."
+  (interactive)
+  (let ((file (me--sudo-file-path buffer-file-name)))
+    (if-let (buffer (find-file-noselect file))
+        (let ((origin (current-buffer)))
+          (copy-to-buffer buffer (point-min) (point-max))
+          (unwind-protect
+              (with-current-buffer buffer
+                (save-buffer))
+            (unless (eq origin buffer)
+              (kill-buffer buffer))
+            (with-current-buffer origin
+              (revert-buffer t t))))
+      (user-error "Unable to open %S" file))))
+
 ;; (add-hook 'emacs-lisp-mode-hook #'me-dir-locals-enable-autoreload)
 ;; (add-hook 'lisp-data-mode-hook #'me-dir-locals-enable-autoreload)
 
