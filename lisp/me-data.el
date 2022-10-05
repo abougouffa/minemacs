478ea5b437e2f95b10aafc8dd365f57863b6428e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-05 16:20:17 +0200

    feat: add yaml-mode and keybindings to data

--- lisp/me-data.el
+++ lisp/me-data.el
@@ -9,13 +9,18 @@
   :mode ("\\.csv\\'" . csv-mode)
   :general
   (me-local-def :keymaps 'csv-mode-map
-    "r" '(me-csv-rainbow :which-key "CSV Rainbow"))
+    "r" '(me-csv-rainbow :which-key "CSV Rainbow")
+    "a" #'csv-align-fields
+    "u" #'csv-unalign-fields
+    "s" #'csv-sort-fields
+    "S" #'csv-sort-numeric-fields
+    "k" #'csv-kill-fields
+    "t" #'csv-transpose)
   :config
   ;; TODO: Need to fix the case of two commas, example "a,b,,c,d"
-  (require 'cl-lib)
-  (require 'color)
-
   (defun me-csv-rainbow (&optional separator)
+    (require 'cl-lib)
+    (require 'color)
     (interactive (list (when current-prefix-arg (read-char "Separator: "))))
     (font-lock-mode 1)
     (let* ((separator (or separator ?\,))
@@ -27,6 +32,25 @@
                for c in colors
                for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
                do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c))))))))))
+
+
+(use-package yaml-mode
+  :straight t
+  :mode "Procfile\\'")
+
+
+(use-package json-mode
+  :straight t
+  :mode "\\.js\\(?:on\\|[hl]int\\(?:rc\\)?\\)\\'"
+  :general
+  (me-local-def :keymaps 'json-mode-map
+    "p" #'json-mode-show-path
+    "t" #'json-toggle-boolean
+    "d" #'json-mode-kill-path
+    "x" #'json-nullify-sexp
+    "+" #'json-increment-number-at-point
+    "-" #'json-decrement-number-at-point
+    "f" #'json-mode-beautify))
 
 
 (provide 'me-data)
