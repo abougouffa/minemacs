20eff1dbcc300a6d11ccb469bc9ef96c8a7e7475
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-07 21:59:56 +0200

    tweak: nov

--- modules/me-docs.el
+++ modules/me-docs.el
@@ -16,7 +16,66 @@
 
 (use-package nov
   :straight t
-  :defer t)
+  :mode ("\\.epub\\'" . nov-mode)
+  :config
+  (me-map-key :keymaps 'nov-mode-map :states 'normal
+    "RET" #'nov-scroll-up)
+
+  (defun doom-modeline-segment--nov-info ()
+    (concat " "
+            (propertize (cdr (assoc 'creator nov-metadata))
+                        'face 'doom-modeline-project-parent-dir)
+            " "
+            (cdr (assoc 'title nov-metadata))
+            " "
+            (propertize (format "%d/%d" (1+ nov-documents-index) (length nov-documents))
+                        'face 'doom-modeline-info)))
+
+  (advice-add 'nov-render-title :override #'ignore)
+
+  (defun +nov-mode-setup ()
+    (face-remap-add-relative 'variable-pitch
+                             :family "Merriweather"
+                             :height 1.4
+                             :width 'semi-expanded)
+    (face-remap-add-relative 'default :height 1.3)
+    (setq-local line-spacing 0.2
+                next-screen-context-lines 4
+                shr-use-colors nil)
+    (require 'visual-fill-column nil t)
+    (setq-local visual-fill-column-center-text t
+                visual-fill-column-width 80
+                nov-text-width 80)
+    (visual-fill-column-mode 1)
+    (hl-line-mode -1)
+
+    (setq-local
+     mode-line-format
+     `((:eval
+        (doom-modeline-segment--workspace-name))
+       (:eval
+        (doom-modeline-segment--window-number))
+       (:eval
+        (doom-modeline-segment--nov-info))
+       ,(propertize
+         " %P "
+         'face 'doom-modeline-buffer-minor-mode)
+       ,(propertize
+         " "
+         'face (if (doom-modeline--active) 'mode-line 'mode-line-inactive)
+         'display `((space
+                     :align-to
+                     (- (+ right right-fringe right-margin)
+                        ,(* (let ((width (doom-modeline--font-width)))
+                              (or (and (= width 1) 1)
+                                  (/ width (frame-char-width) 1.0)))
+                            (string-width
+                             (format-mode-line
+                              (cons ""
+                                    '(:eval (doom-modeline-segment--major-mode))))))))))
+       (:eval (doom-modeline-segment--major-mode)))))
+
+  (add-hook 'nov-mode-hook #'+nov-mode-setup))
 
 
 (provide 'me-docs)
