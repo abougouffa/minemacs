3d0f1ef9e148da83845dc7cb8ea73678555f6ea9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-07 15:01:39 +0200

    clean: user config

--- config.el
+++ config.el
@@ -23,52 +23,8 @@
 (defvar +my/biblio-notes-path     (expand-file-name "~/PhD/bibliography/notes/"))
 (defvar +my/biblio-styles-path    (expand-file-name "~/Zotero/styles/"))
 
-(setq org-directory "~/Dropbox/Org")
-
-(setq auth-sources '("~/.authinfo.gpg")
-      auth-source-do-cache t
-      auth-source-cache-expiry 86400 ; All day, defaut is 2h (7200)
-      password-cache t
-      password-cache-expiry 86400)
-
-(dolist (fn '(evil-window-split evil-window-vsplit))
-  (advice-add fn :after (lambda (&rest _) (consult-buffer))))
-
-(defvar +messages--auto-tail-enabled nil)
-
-(defun +messages--auto-tail-a (&rest arg)
-  "Make *Messages* buffer auto-scroll to the end after each message."
-  (let* ((buf-name (buffer-name (messages-buffer)))
-         ;; Create *Messages* buffer if it does not exist
-         (buf (get-buffer-create buf-name)))
-    ;; Activate this advice only if the point is _not_ in the *Messages* buffer
-    ;; to begin with. This condition is required; otherwise you will not be
-    ;; able to use `isearch' and other stuff within the *Messages* buffer as
-    ;; the point will keep moving to the end of buffer :P
-    (when (not (string= buf-name (buffer-name)))
-      ;; Go to the end of buffer in all *Messages* buffer windows that are
-      ;; *live* (`get-buffer-window-list' returns a list of only live windows).
-      (dolist (win (get-buffer-window-list buf-name nil :all-frames))
-        (with-selected-window win
-          (goto-char (point-max))))
-      ;; Go to the end of the *Messages* buffer even if it is not in one of
-      ;; the live windows.
-      (with-current-buffer buf
-        (goto-char (point-max))))))
-
-(defun +messages-auto-tail-toggle ()
-  "Auto tail the '*Messages*' buffer."
-  (interactive)
-  (if +messages--auto-tail-enabled
-      (progn
-        (advice-remove 'message '+messages--auto-tail-a)
-        (setq +messages--auto-tail-enabled nil)
-        (message "+messages-auto-tail: Disabled."))
-    (advice-add 'message :after '+messages--auto-tail-a)
-    (setq +messages--auto-tail-enabled t)
-    (message "+messages-auto-tail: Enabled.")))
-
-(setq source-directory (expand-file-name "~/Softwares/src/emacs/"))
+(setq org-directory "~/Dropbox/Org"
+      source-directory (expand-file-name "~/Softwares/aur/emacs-git/src/emacs-git/"))
 
 (setq browse-url-chrome-program "brave")
 
