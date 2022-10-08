777641bd6207b324f0fa9166922d3db3e533b5dd
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-08 18:29:28 +0200

    tweak: daemon

--- modules/me-daemon.el
+++ modules/me-daemon.el
@@ -2,45 +2,39 @@
 
 
 (defun me-daemon--start ()
-  (run-at-time
-   (* 60 2) nil
-   (lambda ()
-     (unless (daemonp)
-       (let ((inhibit-message nil))
-         (me-info! "Starting Emacs daemon in background.")
-         (server-start))))))
+  (with-eval-after-load 'minemacs-loaded-stage-2
+    (unless (daemonp)
+      (let ((inhibit-message nil))
+        (me-info! "Starting Emacs daemon in background.")
+        (server-start)))))
 
 
 (defun me-daemon--setup-background-apps ()
-  (run-at-time
-   (* 60 1) nil ;; After 2m
-   (lambda ()
-     ;; mu4e
-     (when (require 'mu4e nil t)
-       ;; Automatically start `mu4e' in background.
-       (run-at-time
-        nil
-        (* 60 3) ;; Check each 3m
-        (lambda ()
-          (unless (mu4e-running-p)
-            (let ((inhibit-message nil))
-              (mu4e--start)
-              (me-info! "Started `mu4e' in background."))))))
+  (with-eval-after-load 'minemacs-loaded-stage-1
+    ;; mu4e
+    (when (require 'mu4e nil t)
+      (run-at-time
+       nil
+       (* 60 3) ;; Check each 3m
+       (lambda ()
+         (unless (mu4e-running-p)
+           (let ((inhibit-message nil))
+             (mu4e--start)
+             (me-info! "Started `mu4e' in background."))))))
 
-     ;; RSS
-     (when (and (require 'elfeed nil t) nil)
-       (run-at-time
-        (* 60 5)
-        (* 60 60 2)
-        #'elfeed-update))))) ;; Check every 2h
+    ;; RSS
+    (when (require 'elfeed nil t)
+      (run-at-time
+       (* 60 5)
+       (* 60 60 3)
+       #'elfeed-update)))) ;; Check every 2h
 
 
 ;; At daemon startup
-(add-hook 'minemacs-after-startup-hook #'me-daemon--setup-background-apps)
+(add-hook 'emacs-startup-hook #'me-daemon--setup-background-apps)
 
 (unless (daemonp)
-  (add-hook 'minemacs-after-startup-hook #'me-daemon--start))
+  (add-hook 'emacs-startup-hook #'me-daemon--start))
 
-;; minemacs-after-startup-hook
 
 (provide 'me-daemon)
