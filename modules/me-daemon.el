7d3a4b55a6582a171bbb0e7a503a286ddbf9a19f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-08 17:03:15 +0200

    tweak: daemon

--- modules/me-daemon.el
+++ modules/me-daemon.el
@@ -1,16 +1,25 @@
 ;; -*- lexical-binding: t; -*-
 
 
-(when (daemonp)
-  ;; At daemon startup
-  (add-hook
-   'emacs-startup-hook
-   (defun me--daemon-startup ()
+(defun me-daemon--start ()
+  (run-at-time
+   (* 60 2) nil
+   (lambda ()
+     (unless (daemonp)
+       (let ((inhibit-message nil))
+         (me-info! "Starting Emacs daemon in background.")
+         (server-start))))))
+
+
+(defun me-daemon--setup-background-apps ()
+  (run-at-time
+   (* 60 1) nil ;; After 2m
+   (lambda ()
      ;; mu4e
      (when (require 'mu4e nil t)
        ;; Automatically start `mu4e' in background.
        (run-at-time
-        (* 60 1) ;; Launch after 1m
+        nil
         (* 60 3) ;; Check each 3m
         (lambda ()
           (unless (mu4e-running-p)
@@ -20,7 +29,18 @@
 
      ;; RSS
      (when (and (require 'elfeed nil t) nil)
-       (run-at-time nil (* 2 60 60) #'elfeed-update))))) ;; Check every 2h
+       (run-at-time
+        (* 60 5)
+        (* 60 60 2)
+        #'elfeed-update))))) ;; Check every 2h
 
+
+;; At daemon startup
+(add-hook 'minemacs-after-startup-hook #'me-daemon--setup-background-apps)
+
+(unless (daemonp)
+  (add-hook 'minemacs-after-startup-hook #'me-daemon--start))
+
+;; minemacs-after-startup-hook
 
 (provide 'me-daemon)
