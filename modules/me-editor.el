7f95a87e24e9c5e19f00b2758263fbff6cd412c7
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-08 23:25:01 +0200

    feat: tempel templates

--- modules/me-editor.el
+++ modules/me-editor.el
@@ -34,6 +34,37 @@
   (global-undo-fu-session-mode 1))
 
 
+(use-package tempel
+  :straight t
+  :custom
+  (tempel-trigger-prefix "<") ;; Require trigger prefix before template name when completing.
+  (tempel-path (expand-file-name "templates/*.eld" user-emacs-directory))
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert))
+  :init
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-complete
+                      completion-at-point-functions)))
+
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  (global-tempel-abbrev-mode))
+
+
+
 (use-package unicode-fonts
   :straight t
   :after minemacs-loaded
