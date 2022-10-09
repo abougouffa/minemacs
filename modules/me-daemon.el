a0895902b78a304ae4368b2deb167ece85848d47
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-09 20:31:53 +0200

    fix: start mu4e early to avoid stealing the minibuffer

--- modules/me-daemon.el
+++ modules/me-daemon.el
@@ -1,27 +1,16 @@
 ;; -*- lexical-binding: t; -*-
 
 
-(defun me-daemon--start ()
-  (with-eval-after-load 'minemacs-loaded-stage-2
-    (unless (daemonp)
-      (let ((inhibit-message t))
-        (me-info! "Starting Emacs daemon in background.")
-        (server-start)))))
-
-
 (defun me-daemon--setup-background-apps ()
-  (with-eval-after-load 'minemacs-loaded-stage-1
+  (with-eval-after-load 'minemacs-loaded
     ;; mu4e
     (when (require 'mu4e nil t)
-      (run-at-time
-       nil
-       (* 60 3) ;; Check each 3m
-       (lambda ()
-         (unless (mu4e-running-p)
-           (let ((inhibit-message t))
-             (mu4e--start)
-             (me-info! "Started `mu4e' in background."))))))
+      (unless (mu4e-running-p)
+        (let ((inhibit-message t))
+          (mu4e--start)
+          (me-info! "Started `mu4e' in background.")))))
 
+  (with-eval-after-load 'minemacs-loaded-stage-1
     ;; RSS
     (when (require 'elfeed nil t)
       (run-at-time
@@ -30,14 +19,17 @@
        (lambda ()
          (let ((inhibit-message t))
            (me-info! "Updating RSS feed.")
-           (elfeed-update))))))) ;; Check every 2h
+           (elfeed-update)))))) ;; Check every 2h
+
+  (with-eval-after-load 'minemacs-loaded-stage-2
+    (unless (daemonp)
+      (let ((inhibit-message t))
+        (me-info! "Starting Emacs daemon in background.")
+        (server-start)))))
 
 
 ;; At daemon startup
 (add-hook 'emacs-startup-hook #'me-daemon--setup-background-apps)
-
-(unless (daemonp)
-  (add-hook 'emacs-startup-hook #'me-daemon--start))
 
 
 (provide 'me-daemon)
