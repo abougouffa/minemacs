bc9b0dd44653f1d032d23a93e148c41d08dc3c67
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-11 18:21:32 +0200

    tweak: use xwidgets as default browser

--- core/me-defaults.el
+++ core/me-defaults.el
@@ -148,6 +148,11 @@ or file path may exist now."
            (abort-recursive-edit))
        (apply sub-read args)))))
 
+(when feat/xwidgets
+  ;; Make xwidget-webkit the default browser
+  (setq browse-url-browser-function #'xwidget-webkit-browse-url)
+  (defalias 'browse-web #'xwidget-webkit-browse-url))
+
 (with-eval-after-load 'minemacs-loaded
   ;; Enable battery (if available) in mode-line
   (me-with-shutup!
