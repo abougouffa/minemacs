e94e38f84156b3721c29c88b32fc3e9406082b20
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-11 18:24:39 +0200

    refactor: EAF

--- modules/me-eaf.el
+++ modules/me-eaf.el
@@ -4,13 +4,22 @@
 (use-package eaf
   :straight (:host github :repo "emacs-eaf/emacs-application-framework" :files (:defaults "*"))
   :unless feat/lucid
-  :defer t
   :commands (eaf-install-and-update
-             eaf-open)
+             eaf-open
+             eaf-open-jupyter
+             eaf-open-mindmap
+             eaf-file-sender-qrcode
+             eaf-file-sender-qrcode-in-dired
+             eaf-open-browser
+             +eaf-open-mail-as-html)
+  :general
+  (me-map
+    "oo" '(eaf-open :which-key "Open with EAF"))
   :custom
-  (eaf-apps-to-install '(browser mindmap jupyter org-previewer
-                         markdown-previewer file-sender video-player))
+  (eaf-apps-to-install (message "CUSTOM"))
   ;; Generic
+  (eaf-apps-to-install '(browser pdf-viewer mindmap jupyter org-previewer
+                         markdown-previewer file-sender video-player))
   (eaf-start-python-process-when-require t)
   (eaf-kill-process-after-last-buffer-closed t)
   (eaf-fullscreen-p nil)
@@ -31,6 +40,33 @@
   (eaf-webengine-enable-plugin t)
   (eaf-webengine-enable-javascript t)
   (eaf-webengine-enable-javascript-access-clipboard t)
+  ;; Jupyter
+  (eaf-jupyter-font-family (plist-get me-fonts :font-family))
+  (eaf-jupyter-font-size 14)
+  ;; Video player
+  (eaf-video-player-keybinding
+   '(("p" . "toggle_play")
+     ("q" . "close_buffer")
+     ("h" . "play_backward")
+     ("l" . "play_forward")
+     ("j" . "decrease_volume")
+     ("k" . "increase_volume")
+     ("f" . "toggle_fullscreen")
+     ("R" . "restart")))
+  ;; PDF viewer
+  (eaf-pdf-show-progress-on-page nil)
+  (eaf-pdf-dark-exclude-image t)
+  (eaf-pdf-notify-file-changed t)
+  ;; Browser
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-enable-adblocker t)
+  (eaf-browser-ignore-history-list '("google.com/search" "file://"))
+  (eaf-browser-translate-language "en")
+  (eaf-browser-blank-page-url "https://www.duckduckgo.com")
+  (eaf-browser-chrome-history-file (expand-file-name "eaf/browser/Default/History" minemacs-var-dir))
+  (eaf-browser-default-search-engine "duckduckgo")
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-aria2-auto-file-renaming t)
   :config
   ;; Fix EVIL keybindings
   (with-eval-after-load 'evil
@@ -43,86 +79,35 @@
              ("browser" (if (eaf-call-sync "execute_function" eaf--buffer-id "is_focus")
                             (kbd "SPC")
                           (kbd eaf-evil-leader-key)))
-             ("pdf-viewer" (kbd eaf-evil-leader-key))
-             ("image-viewer" (kbd eaf-evil-leader-key))
-             ("music-player" (kbd eaf-evil-leader-key))
-             ("video-player" (kbd eaf-evil-leader-key))
-             ("file-sender" (kbd eaf-evil-leader-key))
-             ("mindmap" (kbd eaf-evil-leader-key))
+             ((or "pdf-viewer"
+                  "image-viewer"
+                  "music-player"
+                  "video-player"
+                  "file-sender"
+                  "jupyter"
+                  "mindmap")
+              (kbd eaf-evil-leader-key))
              (_ (kbd "SPC")))
-         (kbd "SPC"))))))
+         (kbd "SPC")))))
 
 
-(use-package eaf-all-the-icons
-  :after eaf
-  :config
+  ;; Extensions
+  (require 'eaf-org)
+  (require 'eaf-all-the-icons)
   (when (display-graphic-p)
     (mapc (lambda (v) (eaf-all-the-icons-icon (car v)))
-          eaf-all-the-icons-alist)))
+          eaf-all-the-icons-alist))
 
+  ;; Apps
+  (require 'eaf-org-previewer)
+  (require 'eaf-markdown-previewer)
+  (require 'eaf-jupyter)
+  (require 'eaf-mindmap)
+  (require 'eaf-file-sender)
+  (require 'eaf-video-player)
 
-(use-package eaf-org
-  :defer t)
+  (require 'eaf-browser)
 
-
-(use-package eaf-org-previewer
-  :defer t)
-
-
-(use-package eaf-markdown-previewer
-  :defer t)
-
-
-(use-package eaf-jupyter
-  :commands (eaf-open-jupyter)
-  :custom
-  (eaf-jupyter-font-family (plist-get me-fonts :font-family))
-  (eaf-jupyter-font-size 14))
-
-
-(use-package eaf-mindmap
-  :commands (eaf-open-mindmap))
-
-
-(use-package eaf-file-sender
-  :commands (eaf-file-sender-qrcode
-             eaf-file-sender-qrcode-in-dired))
-
-
-(use-package eaf-video-player
-  :after eaf
-  :custom
-  (eaf-video-player-keybinding
-   '(("p" . "toggle_play")
-     ("q" . "close_buffer")
-     ("h" . "play_backward")
-     ("l" . "play_forward")
-     ("j" . "decrease_volume")
-     ("k" . "increase_volume")
-     ("f" . "toggle_fullscreen")
-     ("R" . "restart"))))
-
-
-(use-package eaf-browser
-  :after eaf
-  :commands (eaf-open-browser
-             +eaf-open-mail-as-html)
-  :general
-  (me-map-local :keymaps '(mu4e-headers-mode-map mu4e-view-mode-map)
-    "h" '(+eaf-open-mail-as-html :which-key "Open mail as HTML")
-    "o" '(eaf-open-browser :which-key "Open URL (EAF)"))
-  :custom
-  (eaf-browser-continue-where-left-off t)
-  (eaf-browser-dark-mode nil) ;; "follow"
-  (eaf-browser-enable-adblocker t)
-  (eaf-browser-ignore-history-list '("google.com/search" "file://"))
-  (eaf-browser-translate-language "en")
-  (eaf-browser-blank-page-url "https://www.duckduckgo.com")
-  (eaf-browser-chrome-history-file (expand-file-name "eaf/browser/Default/History" minemacs-var-dir))
-  (eaf-browser-default-search-engine "duckduckgo")
-  (eaf-browser-continue-where-left-off t)
-  (eaf-browser-aria2-auto-file-renaming t)
-  :config
   (unless feat/xwidgets
     ;; Make EAF Browser my default browser
     (setq browse-url-browser-function #'eaf-open-browser)
@@ -137,16 +122,15 @@
           (browse-url-browser-function #'eaf-open-browser))
       (if msg
           (mu4e-action-view-in-browser msg)
-        (message "No message at point.")))))
+        (message "No message at point."))))
 
+  ;; (with-eval-after-load 'mu4e
+  ;;   (me-map-local :keymaps '(mu4e-headers-mode-map mu4e-view-mode-map)
+  ;;     "h" '(+eaf-open-mail-as-html :which-key "Open mail as HTML")
+  ;;     "o" '(eaf-open-browser :which-key "Open URL (EAF)")))
 
-(use-package eaf-pdf-viewer
-  :disabled t
-  :after eaf
-  (eaf-pdf-show-progress-on-page nil)
-  (eaf-pdf-dark-exclude-image t)
-  (eaf-pdf-notify-file-changed t)
-  :config
+  (require 'eaf-pdf-viewer)
+
   (with-eval-after-load 'org
     ;; Use EAF PDF Viewer in Org
     (defun +eaf--org-open-file-fn (file &optional link)
