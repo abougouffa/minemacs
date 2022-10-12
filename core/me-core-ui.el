71e9b3310028c9f32c4b0af21252158b40cad449
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-12 20:19:54 +0200

    tweak: UI

--- core/me-core-ui.el
+++ core/me-core-ui.el
@@ -112,15 +112,15 @@
   (ef-themes-mixed-fonts nil)
   (ef-themes-variable-pitch-ui nil)
   (ef-themes-headings ; read the manual's entry or the doc string
-   '((0 . (variable-pitch light 1.9))
-     (1 . (variable-pitch light 1.8))
-     (2 . (variable-pitch regular 1.7))
-     (3 . (variable-pitch regular 1.6))
-     (4 . (variable-pitch regular 1.5))
-     (5 . (variable-pitch 1.4)) ; absence of weight means `bold'
-     (6 . (variable-pitch 1.3))
-     (7 . (variable-pitch 1.2))
-     (t . (variable-pitch 1.1))))
+   '((0 . (default light 1.3))
+     (1 . (default light 1.2))
+     (2 . (default regular 1.1))
+     (3 . (default regular 1.05))
+     (4 . (default 1))
+     (5 . (default 1)) ; absence of weight means `bold'
+     (6 . (default 1))
+     (7 . (default 1))
+     (t . (default 1))))
   :config
   ;; Disable all other themes to avoid awkward blending:
   (mapc #'disable-theme custom-enabled-themes)
