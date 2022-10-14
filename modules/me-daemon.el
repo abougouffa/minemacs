78329b8b41b85cbc0acedb25b8e63df570259fe8
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-14 21:30:06 +0200

    refactor: daemon

--- modules/me-daemon.el
+++ modules/me-daemon.el
@@ -3,33 +3,31 @@
 
 (defun me-daemon--setup-background-apps ()
   (with-eval-after-load 'minemacs-loaded
-    ;; mu4e
-    (run-at-time
-     nil
-     nil
-     (lambda ()
-       (when (require 'mu4e nil t)
-         (unless (mu4e-running-p)
-           (let ((inhibit-message t))
-             (mu4e :background)
-             (me-info! "Started `mu4e' in background.")))))))
+    (me-eval-when-idle!
+     ;; mu4e
+     (when (require 'mu4e nil t)
+       (unless (mu4e-running-p)
+         (let ((inhibit-message t))
+           (mu4e t)
+           (me-info! "Started `mu4e' in background."))))))
 
   (with-eval-after-load 'minemacs-loaded-stage-1
     ;; RSS
-    (when (require 'elfeed nil t)
-      (run-at-time
-       (* 60 5)
-       (* 60 60 3)
-       (lambda ()
-         (let ((inhibit-message t))
-           (me-info! "Updating RSS feed.")
-           (elfeed-update))))))
+    (me-eval-when-idle!
+     (run-at-time
+      (* 60 5)
+      (* 60 60 3)
+      (lambda ()
+        (let ((inhibit-message t))
+          (me-info! "Updating RSS feed.")
+          (elfeed-update))))))
 
   (with-eval-after-load 'minemacs-loaded-stage-2
-    (unless (daemonp)
-      (let ((inhibit-message t))
-        (me-info! "Starting Emacs daemon in background.")
-        (server-start nil t)))))
+    (me-eval-when-idle!
+     (unless (daemonp)
+       (let ((inhibit-message t))
+         (me-info! "Starting Emacs daemon in background.")
+         (server-start nil t))))))
 
 
 ;; At daemon startup
