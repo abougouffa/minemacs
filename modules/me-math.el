1b922fc2b7c01c4ba0a193ede7e821555b1c44bc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-21 21:24:46 +0200

    feat: add jupyter support (via `ein`)

--- modules/me-math.el
+++ modules/me-math.el
@@ -25,3 +25,18 @@
   :config
   ;; Hook the `maxima-inferior-mode' to get syntax highlighting
   (add-hook 'imaxima-startup-hook #'maxima-inferior-mode))
+
+
+(use-package ein
+  :straight t
+  :commands (ein:run ein:login ein:ipynb-mode)
+  :mode ("\\.ipynb\\'" . ein:ipynb-mode)
+  :config
+  (+map-local :keymaps 'ein:ipynb-mode-map
+    "o" #'ein:process-find-file-callback
+    "O" #'ein:process-open-notebook
+    "r" #'ein:gat-run-remote
+    "l" #'ein:gat-run-local))
+
+
+(provide 'me-math)
