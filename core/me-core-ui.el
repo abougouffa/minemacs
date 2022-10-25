2213cd7f1443201702c5c27f07bd3f99b24b26a5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-10-25 13:36:25 +0200

    tweak: core UI

--- core/me-core-ui.el
+++ core/me-core-ui.el
@@ -73,75 +73,75 @@
   (modus-themes-subtle-line-numbers t)
   (modus-themes-region '(bg-only no-extend)) ;; accented
   (modus-themes-variable-pitch-ui nil)
-  (modus-themes-fringes 'subtle)
+  (modus-themes-fringes nil)
   (modus-themes-diffs nil)
   (modus-themes-italic-constructs t)
   (modus-themes-bold-constructs t)
   (modus-themes-intense-mouseovers t)
   (modus-themes-paren-match '(bold intense))
-  (modus-themes-syntax '(green-strings))
+  (modus-themes-syntax '(green-strings alt-syntax))
   (modus-themes-links '(neutral-underline background))
   (modus-themes-mode-line '(borderless padded))
   (modus-themes-tabs-accented nil) ;; default
   (modus-themes-completions
-   '((matches . (extrabold intense accented))
+   '((matches   . (extrabold intense accented))
      (selection . (semibold accented intense))
-     (popup . (accented))))
+     (popup     . (accented))))
   (modus-themes-headings '((1 . (rainbow 1.4))
                            (2 . (rainbow 1.3))
                            (3 . (rainbow 1.2))
                            (4 . (rainbow bold 1.1))
                            (t . (rainbow bold))))
-  (modus-themes-org-blocks 'gray-background)
+  (modus-themes-org-blocks 'tinted-background)
   (modus-themes-org-agenda
    '((header-block . (semibold 1.4))
-     (header-date . (workaholic bold-today 1.2))
-     (event . (accented italic varied))
-     (scheduled . rainbow)
-     (habit . traffic-light)))
+     (header-date  . (workaholic bold-today 1.2))
+     (event        . (accented italic varied))
+     (scheduled    . rainbow)
+     (habit        . traffic-light)))
   (modus-themes-markup '(intense background))
   (modus-themes-mail-citations 'intense)
-  (modus-themes-lang-checkers '(background))
+  (modus-themes-lang-checkers '(intense))
 
   :config
   (defun +modus-themes-tweak-packages ()
     (modus-themes-with-colors
-     (set-face-attribute 'cursor nil :background (modus-themes-color 'blue))
-     (set-face-attribute 'font-lock-type-face nil :foreground (modus-themes-color 'magenta-alt))
-     (custom-set-faces
-      ;; Tweak `evil-mc-mode'
-      `(evil-mc-cursor-default-face ((,class :background ,magenta-intense-bg)))
-      ;; Tweak `git-gutter-mode'
-      `(git-gutter-fr:added ((,class :foreground ,green-fringe-bg)))
-      `(git-gutter-fr:deleted ((,class :foreground ,red-fringe-bg)))
-      `(git-gutter-fr:modified ((,class :foreground ,yellow-fringe-bg)))
-      ;; Tweak `diff-hl-mode'
-      `(diff-hl-insert ((,class :foreground ,green-fringe-bg)))
-      `(diff-hl-delete ((,class :foreground ,red-fringe-bg)))
-      `(diff-hl-change ((,class :foreground ,yellow-fringe-bg)))
-      ;; Tweak `doom-modeline'
-      `(doom-modeline-evil-normal-state ((,class :foreground ,green-alt-other)))
-      `(doom-modeline-evil-insert-state ((,class :foreground ,red-alt-other)))
-      `(doom-modeline-evil-visual-state ((,class :foreground ,magenta-alt)))
-      `(doom-modeline-evil-operator-state ((,class :foreground ,blue-alt)))
-      `(doom-modeline-evil-motion-state ((,class :foreground ,blue-alt-other)))
-      `(doom-modeline-evil-replace-state ((,class :foreground ,yellow-alt)))
-      ;; Tweak `solaire-mode'
-      `(solaire-default-face ((,class :inherit default :background ,bg-alt :foreground ,fg-dim)))
-      `(solaire-line-number-face ((,class :inherit solaire-default-face :foreground ,fg-unfocused)))
-      `(solaire-hl-line-face ((,class :background ,bg-active)))
-      `(solaire-org-hide-face ((,class :background ,bg-alt :foreground ,bg-alt)))
-      ;; Tweak `display-fill-column-indicator-mode'
-      `(fill-column-indicator ((,class :height 0.3 :background ,bg-inactive :foreground ,bg-inactive)))
-      ;; Tweak `mmm-mode'
-      `(mmm-cleanup-submode-face ((,class :background ,yellow-refine-bg)))
-      `(mmm-code-submode-face ((,class :background ,bg-active)))
-      `(mmm-comment-submode-face ((,class :background ,blue-refine-bg)))
-      `(mmm-declaration-submode-face ((,class :background ,cyan-refine-bg)))
-      `(mmm-default-submode-face ((,class :background ,bg-alt)))
-      `(mmm-init-submode-face ((,class :background ,magenta-refine-bg)))
-      `(mmm-output-submode-face ((,class :background ,red-refine-bg)))
-      `(mmm-special-submode-face ((,class :background ,green-refine-bg))))))
+      (set-face-attribute 'cursor nil :background (modus-themes-color 'blue))
+      (set-face-attribute 'font-lock-type-face nil :foreground (modus-themes-color 'magenta-alt))
+      (custom-set-faces
+       ;; Tweak `evil-mc-mode'
+       `(evil-mc-cursor-default-face ((,class :background ,magenta-intense-bg)))
+       ;; Tweak `git-gutter-mode'
+       `(git-gutter-fr:added ((,class :foreground ,green-fringe-bg)))
+       `(git-gutter-fr:deleted ((,class :foreground ,red-fringe-bg)))
+       `(git-gutter-fr:modified ((,class :foreground ,yellow-fringe-bg)))
+       ;; Tweak `diff-hl-mode'
+       `(diff-hl-insert ((,class :foreground ,green-fringe-bg)))
+       `(diff-hl-delete ((,class :foreground ,red-fringe-bg)))
+       `(diff-hl-change ((,class :foreground ,yellow-fringe-bg)))
+       ;; Tweak `doom-modeline'
+       `(doom-modeline-evil-normal-state ((,class :foreground ,green-alt-other)))
+       `(doom-modeline-evil-insert-state ((,class :foreground ,red-alt-other)))
+       `(doom-modeline-evil-visual-state ((,class :foreground ,magenta-alt)))
+       `(doom-modeline-evil-operator-state ((,class :foreground ,blue-alt)))
+       `(doom-modeline-evil-motion-state ((,class :foreground ,blue-alt-other)))
+       `(doom-modeline-evil-replace-state ((,class :foreground ,yellow-alt)))
+       ;; Tweak `solaire-mode'
+       `(solaire-default-face ((,class :inherit default :background ,bg-alt :foreground ,fg-dim)))
+       `(solaire-line-number-face ((,class :inherit solaire-default-face :foreground ,fg-unfocused)))
+       `(solaire-hl-line-face ((,class :background ,bg-active)))
+       `(solaire-org-hide-face ((,class :background ,bg-alt :foreground ,bg-alt)))
+       ;; Tweak `display-fill-column-indicator-mode'
+       `(fill-column-indicator ((,class :height 0.3 :background ,bg-inactive :foreground ,bg-inactive)))
+       ;; Tweak `mmm-mode'
+       `(mmm-cleanup-submode-face ((,class :background ,yellow-refine-bg)))
+       `(mmm-code-submode-face ((,class :background ,bg-active)))
+       `(mmm-comment-submode-face ((,class :background ,blue-refine-bg)))
+       `(mmm-declaration-submode-face ((,class :background ,cyan-refine-bg)))
+       `(mmm-default-submode-face ((,class :background ,bg-alt)))
+       `(mmm-init-submode-face ((,class :background ,magenta-refine-bg)))
+       `(mmm-output-submode-face ((,class :background ,red-refine-bg)))
+       `(mmm-special-submode-face ((,class :background ,green-refine-bg))))))
 
   (add-hook 'modus-themes-after-load-theme-hook #'+modus-themes-tweak-packages)
 
@@ -156,8 +156,8 @@
   (ef-themes-mixed-fonts nil)
   (ef-themes-variable-pitch-ui nil)
   (ef-themes-headings ; read the manual's entry or the doc string
-   '((0 . (default light 1.3))
-     (1 . (default light 1.2))
+   '((0 . (default semilight 1.3))
+     (1 . (default semilight 1.2))
      (2 . (default regular 1.1))
      (3 . (default regular 1.05))
      (4 . (default 1))
@@ -169,31 +169,7 @@
   ;; Disable all other themes to avoid awkward blending:
   (mapc #'disable-theme custom-enabled-themes)
 
-  (ef-themes-select 'ef-light))
-
-
-(use-package lambda-line
-  :straight (:type git :host github :repo "lambda-emacs/lambda-line")
-  :defer t
-  :custom
-  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
-  (lambda-line-position 'top) ;; Set position of status-line
-  (lambda-line-abbrev t) ;; abbreviate major modes
-  (lambda-line-hspace "  ") ;; add some cushion
-  (lambda-line-prefix t) ;; use a prefix symbol
-  (lambda-line-prefix-padding t)
-  (lambda-line-status-invert t)
-  (lambda-line-gui-ro-symbol " ⨂") ;; symbols
-  (lambda-line-gui-rw-symbol " ◯")
-  (lambda-line-gui-mod-symbol " ⬤")
-  (lambda-line-space-top +.2) ;; padding on top and bottom of line
-  (lambda-line-space-bottom -.2)
-  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
-  (lambda-line-vc-symbol "  ")
-  (lambda-line-visual-bell nil)
-  :config
-  (lambda-line-mode 1)
-  (lambda-line-clockface-update-fontset "ClockFaceRect"))
+  (ef-themes-select 'ef-duo-light))
 
 
 ;; Modeline
