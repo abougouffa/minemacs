4e01470fbf87141ec1e12d2c94676765b165f257
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-11-07 20:27:29 +0100

    feat: add user skeleton files (for `.minemacs.d`)

---
+++ skel/config.el
@@ -0,0 +1,116 @@
+;;; config.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 Abdelhak Bougouffa
+
+;; Personal info
+(setq user-full-name "Abdelhak Bougouffa"
+      user-mail-address "abougouffa@fedoraproject.org")
+
+;; Set the default GPG key ID, see "gpg --list-secret-keys"
+;; (setq-default epa-file-encrypt-to '("XXXX"))
+
+;; Set fonts
+(setq minemacs-fonts ;; or Cascadia Code, Fira Code, FiraCode Nerd Font, Iosevka Fixed Curly Slab
+      '(:font-family "Iosevka Fixed Curly Slab" ;; "Iosevka Fixed Curly Slab"
+        :font-size 16
+        :variable-pitch-font-family "IBM Plex Serif"
+        :variable-pitch-font-size 16))
+
+;; If you installed Emacs from source, you can add the source code
+;; directory to enable jumping to symbols defined in Emacs' C code.
+;; (setq source-directory "~/Sources/emacs-git/")
+
+;; I use Brave, and never use Chrome, so I replace chrome program with "brave"
+;; (setq browse-url-chrome-program "brave")
+
+;; Module: `me-natural-langs' -- Package: `spell-fu'
+(with-eval-after-load 'spell-fu
+  ;; We can use MinEmacs' helper macro `+spell-fu-register-dictionaries'
+  ;; to enable multi-language spell checking.
+  (+spell-fu-register-dictionaries "en" "fr"))
+
+;; Module: `me-rss' -- Package: `elfeed'
+(with-eval-after-load 'elfeed
+  ;; Add news feeds for `elfeed'
+  (setq elfeed-feeds
+        '("https://itsfoss.com/feed"
+          "https://lwn.net/headlines/rss"
+          "https://linuxhandbook.com/feed"
+          "https://www.omgubuntu.co.uk/feed"
+          "https://this-week-in-rust.org/rss.xml"
+          "https://planet.emacslife.com/atom.xml")))
+
+;; Module: `me-email' -- Package: `mu4e'
+(with-eval-after-load 'mu4e
+  ;; Load personal aliases, a file containing aliases, for example:
+  ;; alias gmail "Firstname Lastname <some.user.name@gmail.com>"
+  ;; alias work  "Firstname Lastname <some.user.name@work.com>"
+
+  ;; (setq mail-personal-alias-file (concat minemacs-config-dir "private/mail-aliases.mailrc"))
+
+  ;; Register email accounts with mu4e
+
+  (setq +mu4e-auto-bcc-address "always.bcc@this.email" ;; Add an email address always included as BCC
+        +mu4e-gmail-accounts '(("account1@gmail.com" . "/gmail")
+                               ("account@somesite.org" . "/gmail")))
+
+  ;; Use MinEmacs' `+mu4e-register-account' helper function to register multiple accounts
+  (+mu4e-register-account
+   "Google mail" ;; Account name
+   "gmail" ;; Maildir
+   `((user-mail-address     . "account1@gmail.com")
+     (mu4e-sent-folder      . "/gmail/Sent Mail")
+     (mu4e-drafts-folder    . "/gmail/Drafts")
+     (mu4e-trash-folder     . "/gmail/Trash")
+     ;; These settings aren't mandatory if a `msmtp' config is used.
+     (smtpmail-smtp-server  . "smtp.googlemail.com")
+     (smtpmail-smtp-service . 587)
+     ;; Define account aliases
+     (+mu4e-account-aliases . ("account1-alias@somesite.org"
+                               "account1-alias@othersite.org"))
+     ;; Org-msg greeting and signature
+     (org-msg-greeting-fmt  . "Hi%s,")
+     ;; Generate signature
+     (org-msg-signature     . ,(+org-msg-make-signature
+                                "Regards," ;; Closing phrase
+                                "Firstname" ;; First name
+                                "Lastname" ;; Last name
+                                "/R&D Engineer at Some company/")))))
+
+;; Module: `me-org' -- Package: `org'
+(with-eval-after-load 'org
+  ;; Set Org-mode directory
+  (setq org-directory "~/Org/") ; let's put files here
+  ;; Customize Org stuff
+  ;; (setq org-todo-keywords
+  ;;       '((sequence "IDEA(i)" "TODO(t)" "NEXT(n)" "PROJ(p)" "STRT(s)" "WAIT(w)" "HOLD(h)" "|" "DONE(d)" "KILL(k)")
+  ;;         (sequence "[ ](T)" "[-](S)" "|" "[X](D)")
+  ;;         (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
+
+  (setq org-export-headline-levels 5))
+
+;; Module: `me-notes' -- Package: `org-roam'
+(with-eval-after-load 'org-roam
+  (setq org-roam-directory "~/Org/slip-box/"
+        org-roam-db-location (concat org-roam-directory "org-roam.db"))
+
+  ;; Register capture template (via Org-Protocol)
+  ;; Add this as bookmarklet in your browser
+  ;; javascript:location.href='org-protocol://roam-ref?template=r&ref=%27+encodeURIComponent(location.href)+%27&title=%27+encodeURIComponent(document.title)+%27&body=%27+encodeURIComponent(window.getSelection())
+  (setq org-roam-capture-ref-templates
+        '(("r" "ref" plain "%?"
+           :if-new (file+head "web/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+created: %U\n\n${body}\n")
+           :unnarrowed t))))
+
+;; Module: `me-ros' -- Package: `ros'
+(with-eval-after-load 'ros
+  (setq ros-workspaces
+        (list
+         (ros-dump-workspace
+          :tramp-prefix "/docker:ros@ros-machine:"
+          :workspace "~/ros_ws"
+          :extends '("/opt/ros/noetic/"))
+         (ros-dump-workspace
+          :tramp-prefix "/docker:ros@ros-machine:"
+          :workspace "~/ros2_ws"
+          :extends '("/opt/ros/foxy/")))))
