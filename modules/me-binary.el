1ecf768207122069b382bd66305675106958b773
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-11-10 21:42:46 +0100

    tweak(binary): setup after `minemacs-loaded`

--- modules/me-binary.el
+++ modules/me-binary.el
@@ -1,5 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; +binary-* are autoloaded
-(add-to-list 'magic-fallback-mode-alist '(+binary-objdump-buffer-p . objdump-disassemble-mode) t)
-(add-to-list 'magic-fallback-mode-alist '(+binary-hexl-buffer-p . +binary-hexl-mode-maybe) t)
+(with-eval-after-load 'minemacs-loaded
+  (add-to-list 'magic-fallback-mode-alist '(+binary-objdump-buffer-p . objdump-disassemble-mode) t)
+  (add-to-list 'magic-fallback-mode-alist '(+binary-hexl-buffer-p . +binary-hexl-mode-maybe) t))
