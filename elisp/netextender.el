1901d05721fc1c5762f00590182115df6c36f5f9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-11-16 20:10:33 +0100

    tweak(vpn): generate command if it doesn't exist

--- elisp/netextender.el
+++ elisp/netextender.el
@@ -4,20 +4,37 @@
 (defvar netextender-process-name "netextender")
 (defvar netextender-buffer-name " *NetExtender*")
 (defvar netextender-command '("~/.local/bin/netextender"))
+(defvar netextender-passphrase-file "~/.ssh/sslvpn.gpg")
+
+;; If the command doesn't exist, generate it.
+(unless (file-exists-p (car netextender-command))
+  (setq netextender-command (cons (make-temp-file "netextender") (cdr netextender-command)))
+  (set-file-modes (car netextender-command) #o755) ;; Make it executable
+  (with-temp-buffer
+    (insert
+     (format "#!/bin/bash
+
+if ! command -v netExtender &> /dev/null; then
+  echo \"netExtender not found, installing from AUR using 'yay'\"
+  yay -S netextender
+fi
+
+MY_LOGIN_PARAMS_FILE=\"%s\"
+
+echo \"Y\\n\" | netExtender --auto-reconnect $(gpg -q --for-your-eyes-only --no-tty -d \"${MY_LOGIN_PARAMS_FILE}\")"
+             (expand-file-name netextender-passphrase-file)))
+    (write-file (car netextender-command))))
 
 
 (defun netextender-check-system ()
   "Return non-nil if system setup is OK."
   (let* ((pppd-command "/usr/sbin/pppd")
          (pppd-modes (file-modes pppd-command)))
-    (if (executable-find (car netextender-command))
-        ;; pppd must be run as root (via setuid)
-        (if (and pppd-modes (zerop (logand (lsh 1 11) pppd-modes))) ;; Check if the setuid bit isn't set
-            (prog1 nil ;; return nil
-              (message "pppd needs root permissions, please set the setuid bit of %s." pppd-command))
-          t)
-      (prog1 nil ;; return nil
-        (message "The custom NetExtender command %s is not found!" (car netextender-command))))))
+    ;; pppd must be run as root (via setuid)
+    (if (and pppd-modes (zerop (logand (lsh 1 11) pppd-modes))) ;; Check if the setuid bit isn't set
+        (prog1 nil ;; return nil
+          (message "pppd needs root permissions, please set the setuid bit of %s." pppd-command))
+      t)))
 
 
 ;;;###autoload
@@ -43,7 +60,6 @@
             (message "Killed NetExtender VPN session.")
           (user-error "Cannot kill NetExtender."))
       (message "No running NetExtender session."))))
-
 
 
 ;;;###autoload
