7cde831ecd3c21ed582bf19b87bfd9a8b79b0a40
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-02 21:24:50 +0100

    feat(prog): prefer builtin tree-sitter

---
+++ modules/extras/me-external-tree-sitter.el
@@ -0,0 +1,38 @@
+;; -*- lexical-binding: t; -*-
+
+;; Use external tree-sitter package if not included in Emacs
+(use-package tree-sitter
+  :straight t
+  :hook ((python-mode
+          c-mode
+          c++-mode
+          csharp-mode
+          rust-mode
+          json-mode
+          xml-mode
+          sh-mode
+          typescript-ts-mode
+          js-mode) . tree-sitter-mode))
+
+(use-package tree-sitter-hl
+  :hook (tree-sitter-mode . tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :straight t
+  :after tree-sitter)
+
+(use-package ts-fold
+  :straight (:type git :host github :repo "emacs-tree-sitter/ts-fold")
+  :hook (prog-mode . +ts-fold-ensure)
+  :init
+  (defun +ts-fold-ensure ()
+    (if (derived-mode-p 'emacs-lisp-mode)
+        (hs-minor-mode) ;; Fall back to builtin
+      (ts-fold-mode))))
+
+;; Needed by `ts-fold'
+(use-package fringe-helper
+  :straight t)
+
+
+(provide 'me-external-tree-sitter)
--- modules/me-prog.el
+++ modules/me-prog.el
@@ -5,46 +5,12 @@
 ;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
 
 ;;; Tree sitter
-(use-package tree-sitter
-  :straight t
-  :hook ((python-mode
-          c-mode
-          c++-mode
-          csharp-mode
-          rust-mode
-          json-mode
-          xml-mode
-          sh-mode
-          typescript-ts-mode
-          js-mode) . tree-sitter-mode))
-
-;; (if feat/treesitter
-;;     (use-package treesit-langs
-;;       :straight (:host github :repo "kiennq/tree-sitter-langs" :files ("tree-sitter-langs-build.el" "treesit-*.el" "queries"))
-;;       :after treesit))
-
-(use-package tree-sitter-hl
-  :hook (tree-sitter-mode . tree-sitter-hl-mode))
-
-
-(use-package tree-sitter-langs
-  :straight t
-  :after tree-sitter)
-
-
-(use-package ts-fold
-  :straight (:type git :host github :repo "emacs-tree-sitter/ts-fold")
-  :hook (prog-mode . +ts-fold-ensure)
-  :init
-  (defun +ts-fold-ensure ()
-    (if (derived-mode-p 'emacs-lisp-mode)
-        (hs-minor-mode) ;; Fall back to builtin
-      (ts-fold-mode))))
-
-
-;; Needed by `ts-fold'
-(use-package fringe-helper
-  :straight t)
+(if feat/treesitter
+    (use-package treesit-langs
+      :straight (:host github :repo "kiennq/tree-sitter-langs" :files ("tree-sitter-langs-build.el" "treesit-*.el" "queries"))
+      :after treesit
+      :config
+      (tree-sitter-langs-install-grammars t "0.12.63")))
 
 
 ;;; Eglot + LSP
