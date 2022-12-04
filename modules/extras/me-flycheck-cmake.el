df0df313c076aa4c8ec74047eb26f634fae847e5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-04 18:17:04 +0100

    refactor(flycheck): better integration for CMake

--- modules/extras/me-flycheck-cmake.el
+++ modules/extras/me-flycheck-cmake.el
@@ -1,4 +1,8 @@
-;;; me-flycheck-cmake.el --- flycheck checker based on cmake
+;;; me-flycheck-cmake.el --- flycheck integration for CMake-based projects
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'json)
 
@@ -6,20 +10,27 @@
 (defvar-local +flycheck-cmake-compiler nil)
 
 (defun +flycheck-cmake-clear-cache ()
+  "Clear cached compiler and \"compile_commands.json\" data."
+  (interactive)
   (setq-local +flycheck-cmake-json nil
               +flycheck-cmake-compiler nil))
 
-(defun +flycheck-cmake-extract-args ()
-  (let ((file (file-truename (buffer-file-name))))
-    (setq file (replace-regexp-in-string "\.h$" ".cpp" file))
+(defun +flycheck-cmake-extract-args (&optinal filename)
+  "Extract compiler arguments for FILENAME.
+When FILENAME is nil, use the file name of the current buffer."
+  (let ((file (replace-regexp-in-string
+               ;; TODO: Check if the file exists, if not try with ".c" instead of ".cpp"
+               (rx (seq "." (or "h" "hpp" "hh") eol)) ".cpp"
+               (file-truename (or filename (buffer-file-name))))))
     (when (and (derived-mode-p 'c++-mode)
-               (string-match (file-truename (projectile-project-root)) file))
+               (string-match
+                (file-truename (project-root (project-current))) file))
       (unless +flycheck-cmake-json
         (setq-local +flycheck-cmake-json
                     (json-read-file
                      (expand-file-name
                       "compile_commands.json"
-                      (projectile-project-root)))))
+                      (project-root (project-current))))))
       (let* ((js +flycheck-cmake-json)
              (matched-entry (cl-find-if
                              (lambda (entry)
@@ -29,15 +40,18 @@
              (cmake-args ""))
         (when cmd
           (let ((lst (split-string (replace-regexp-in-string " +-o .*\\|(\\|)" "" cmd))))
-            (setq-local +flycheck-cmake-compiler (car lst))
+            (unless +flycheck-cmake-compiler
+              (setq-local +flycheck-cmake-compiler (car lst)))
             (setq cmake-args (cdr lst))))))))
 
 (defun +flycheck-cmake--setup (&rest _)
+  "Setup GCC and Clang checkers with the right arguments."
   (let ((args (+flycheck-cmake-extract-args)))
-    (setq flycheck-gcc-args args
-          flycheck-clang-args args)))
+    (setq-local flycheck-gcc-args args
+                flycheck-clang-args args)))
 
 (advice-add 'flycheck-mode :before #'+flycheck-cmake--setup)
 
 (provide 'me-flycheck-cmake)
-;;; flycheck-cmake.el ends here
+
+;;; me-flycheck-cmake.el ends here
