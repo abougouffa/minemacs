3e855b9b8022bd73cf6e15c87213c7761a029d9c
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-14 00:26:46 +0100

    feat(gnus): WIP initial module

---
+++ modules/me-gnus.el
@@ -0,0 +1,11 @@
+;; -*- lexical-binding: t; -*-
+
+
+;; See
+;; https://www.emacswiki.org/emacs/GnusTutorial
+;; https://github.com/redguardtoo/mastering-emacs-in-one-year-guide/blob/master/gnus-guide-en.org
+
+(setq gnus-init-file (+expand 'config "gnus.el"))
+
+
+(provide me-gnus)
