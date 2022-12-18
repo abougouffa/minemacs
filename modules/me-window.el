060262262ec4b597bbd556928712239f5dbd9a99
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-18 01:27:00 +0100

    feat: popwin replacement with builtin buffer settings

--- modules/me-window.el
+++ modules/me-window.el
@@ -5,40 +5,44 @@
 ;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
 
 
+;; Window configuration for special windows.
+;; This section inspired by the article "Demystifying Emacs’s Window Manager" found here:
+;; https://www.masteringemacs.org/article/demystifying-emacs-window-manager
+(add-to-list
+ 'display-buffer-alist
+ `(,(rx (seq "*" (or "Help" (seq "helpful" (zero-or-more not-newline))) "*"))
+   (display-buffer-reuse-window display-buffer-pop-up-window)
+   (inhibit-same-window . t)))
 
+(add-to-list
+ 'display-buffer-alist
+ `(,(rx (seq "*" (or "Completions" "Warnings") "*"))
+   (display-buffer-reuse-window display-buffer-pop-up-window)
+   (inhibit-same-window . t)
+   (window-height . 10)))
 
-(when nil
-  ;; Window configuration for special windows.
-  ;; This section inspired by the article "Demystifying Emacs’s Window
-  ;; Manager" found here:
-  ;; https://www.masteringemacs.org/article/demystifying-emacs-window-manager
-  (add-to-list
-   'display-buffer-alist
-   '("\\*Help\\*"
-     (display-buffer-reuse-window display-buffer-pop-up-window)
-     (inhibit-same-window . t)))
+;; Show dictionary definition on the left
+(add-to-list
+ 'display-buffer-alist
+ `(,(rx (seq "*" (or "Dictionary" "lexic") "*"))
+   (display-buffer-in-side-window)
+   (side . right)
+   (window-width . 70)))
 
-  (add-to-list
-   'display-buffer-alist
-   '("\\*Completions\\*"
-     (display-buffer-reuse-window display-buffer-pop-up-window)
-     (inhibit-same-window . t)
-     (window-height . 10)))
-
-  (add-to-list
-   'display-buffer-alist
-   '("\\*Warnings\\*"
-     (display-buffer-reuse-window display-buffer-pop-up-window)
-     (inhibit-same-window . t)
-     (window-height . 10)))
-
-  ;; Show dictionary definition on the left
-  (add-to-list
-   'display-buffer-alist
-   '("^\\*Dictionary\\*"
-     (display-buffer-in-side-window)
-     (side . left)
-     (window-width . 70))))
+(add-to-list
+ 'display-buffer-alist
+ '((lambda (buffer-or-name _)
+     (let ((buffer (get-buffer buffer-or-name)))
+      (with-current-buffer buffer
+       (or (memq major-mode '(vterm-mode eshell-mode))
+        (string-prefix-p vterm-buffer-name (buffer-name buffer))
+        (string-prefix-p eshell-buffer-name (buffer-name buffer))))))
+   ;; (display-buffer-reuse-window display-buffer-at-bottom)
+   (display-buffer-reuse-window display-buffer-in-direction)
+   (direction . bottom) ;; bottom (above below...)
+   (dedicated . t) ;; Close when finished
+   (reusable-frames . visible) ;;
+   (window-height . 0.3)))
 
 (setq frame-title-format
       '(""
@@ -63,7 +67,7 @@
                      (file-name-nondirectory
                       (string-trim-right (expand-file-name (vc-root-dir)) "/"))))))))
           (concat
-           (format " in %s mode" (car (ensure-list mode-name)))
+           (format " {%s}" (symbol-name major-mode))
            (if (buffer-modified-p) " ○" " ●")
            (when (and proj (not (string= proj "-")))
             (format " %s" proj)))))))
