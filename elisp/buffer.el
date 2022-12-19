f9210be7b4ef2243ae39b786236cc96aa64d2ae8
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-19 22:45:57 +0100

    feat(buffer): add +region-to-file, +region-to-buffer

--- elisp/buffer.el
+++ elisp/buffer.el
@@ -20,3 +20,63 @@ BUFFER may be either a buffer or its name (a string)."
               ;; "Attempt to delete the sole visible or iconified frame".
               (condition-case nil (delete-window win) (error nil))))))
     (when msgp (user-error "Cannot kill buffer.  Not a live buffer: `%s'" buffer))))
+
+;;;###autoload
+(defun +region-to-buffer (start end buffer arg)
+  "Copy region to BUFFER: At beginning (prefix >= 0), end (< 0), or replace.
+START and END are the region boundaries.
+BUFFER is a buffer or its name (a string).
+With prefix ARG >= 0: `append-to-buffer':
+  Append contents of region to end of BUFFER.
+  (Point is moved to end of BUFFER first.)
+With prefix ARG < 0:  `prepend-to-buffer':
+  Prepend contents of region to beginning of BUFFER.
+  (Point is moved to beginning of BUFFER first.)
+With no prefix ARG (nil): `copy-to-buffer'.
+  Write region to BUFFER, replacing any previous contents."
+  (interactive
+   (let ((arg (and current-prefix-arg (prefix-numeric-value current-prefix-arg))))
+     (list (region-beginning)
+           (region-end)
+           (read-buffer
+            (concat (if arg
+                        (if (natnump arg) "Append" "Prepend")
+                      "Write")
+                    " region to buffer: ")
+            (if (fboundp 'another-buffer) ; Defined in `misc-fns.el'.
+                (another-buffer nil t)
+              (other-buffer (current-buffer))))
+           arg)))
+  (setq buffer (get-buffer-create buffer)) ; Convert to buffer.
+  (when (eq buffer (current-buffer)) (error "Cannot copy region to its own buffer"))
+  (cond ((natnump arg)
+         (with-current-buffer buffer (goto-char (point-max)))
+         (append-to-buffer buffer start end))
+        (arg
+         (with-current-buffer buffer (goto-char (point-min)))
+         (prepend-to-buffer buffer start end))
+        (t (copy-to-buffer buffer start end))))
+
+;;;###autoload
+(defun +region-to-file (start end filename arg)
+  "With prefix arg, this is `append-to-file'.  Without, it is `write-region'.
+START and END are the region boundaries.
+Prefix ARG non-nil means append region to end of file FILENAME.
+Prefix ARG nil means write region to FILENAME, replacing contents."
+  (interactive
+   (list (region-beginning)
+         (region-end)
+         (read-file-name (concat (if current-prefix-arg "Append" "Write")
+                                 " region to file: "))
+         current-prefix-arg))
+  (let* ((curr-file (buffer-file-name))
+         (same-file-p (and curr-file (string= curr-file filename))))
+    (cond ((or (not same-file-p)
+               (progn (when (fboundp 'flash-ding) (flash-ding))
+                      (yes-or-no-p
+                       (format
+                        "Do you really want to REPLACE the contents of `%s' by just the REGION? "
+                        (file-name-nondirectory curr-file)))))
+           (write-region start end filename arg)
+           (when same-file-p (revert-buffer t t)))
+          (t (message "OK.  Not written.")))))
