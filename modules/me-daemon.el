58da3455ea63634f09133e4fa86c64379cfb1a3f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-23 00:49:40 +0100

    tweak(daemon): check if modules enabled before launching background apps

--- modules/me-daemon.el
+++ modules/me-daemon.el
@@ -8,29 +8,31 @@
 (defun +daemon--setup-background-apps ()
   (with-eval-after-load 'minemacs-loaded
     (+eval-when-idle!
-     ;; mu4e
-     (when (require 'mu4e nil t)
-       (unless (mu4e-running-p)
-         (let ((inhibit-message t))
-           (mu4e t)
-           (+info! "Started `mu4e' in background.")))))
+      ;; mu4e
+      (when (require 'mu4e nil t)
+        (unless (mu4e-running-p)
+          (let ((inhibit-message t))
+            (mu4e t)
+            (+info! "Started `mu4e' in background.")))))
 
-    ;; RSS
-    (+eval-when-idle!
-     (run-at-time
-      (* 60 5)
-      (* 60 60 3)
-      (lambda ()
-        (let ((inhibit-message t))
-          (+info! "Updating RSS feed.")
-          (elfeed-update)))))
+    (when (featurep 'me-rss)
+      ;; RSS
+      (+eval-when-idle!
+        (run-at-time
+         (* 60 5)
+         (* 60 60 3)
+         (lambda ()
+           (let ((inhibit-message t))
+             (+info! "Updating RSS feed.")
+             (elfeed-update))))))
 
-    (+eval-when-idle!
-     (require 'server)
-     (unless (or (daemonp) (server-running-p))
-       (let ((inhibit-message t))
-         (+info! "Starting Emacs daemon in background.")
-         (server-start nil t))))))
+    (when (featurep 'me-email)
+      (+eval-when-idle!
+        (require 'server)
+        (unless (or (daemonp) (server-running-p))
+          (let ((inhibit-message t))
+            (+info! "Starting Emacs daemon in background.")
+            (server-start nil t)))))))
 
 ;; At daemon startup
 (add-hook 'emacs-startup-hook #'+daemon--setup-background-apps)
