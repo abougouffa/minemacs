1d6878371c8b8fa6031327ea3c5614c8a134b14b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-22 01:45:19 +0100

    fix(ros): use `ros2 bag`, `rosbag` or `mcap`

--- modules/me-ros.el
+++ modules/me-ros.el
@@ -25,23 +25,27 @@
     (setq-local buffer-read-only t
                 truncate-lines t))
 
-  (defvar rosbag-mcap-command
-    (or (executable-find "mcap") (executable-find "mcap-cli")))
-
   (defun rosbag-info-mode-open-file (file)
     "Browse the contents of an ROS bag (v1, SQLite, or MCAP) file."
     (interactive "fROS/ROS2/MCAP bag file name: ")
-    (if (not (and rosbag-mcap-command (executable-find rosbag-mcap-command)))
-        (user-error "Cannot find the `mcap-cli' command.")
-      (let ((bag-format (file-name-extension file)))
-        (if (not (member bag-format '("bag" "db3" "mcap")))
-            (user-error "File \"%s\" doesn't seem to be a ROS/ROS2 bag file."
-                        (file-name-nondirectory file))
-          (let ((buffer-read-only nil)
-                (buff (get-buffer-create
-                       (format "*ROS (%s) %s*" (upcase bag-format) (file-name-nondirectory file)))))
-            (pop-to-buffer buff)
-            (call-process rosbag-mcap-command nil buff nil "info" (expand-file-name file))
+    (let ((bag-format (file-name-extension file)))
+      (if (not (member bag-format '("bag" "db3" "mcap")))
+          (user-error "File \"%s\" doesn't seem to be a ROS/ROS2 bag file."
+                      (file-name-nondirectory file))
+        (let ((buffer-read-only nil)
+              (buff (get-buffer-create
+                     (format "*ROS (%s) %s*" (upcase bag-format) (file-name-nondirectory file)))))
+          (pop-to-buffer buff)
+          (pcase bag-format
+            ("bag"
+             (call-process (executable-find "rosbag")
+                           nil buff nil "info" (expand-file-name file)))
+            ("db3"
+             (call-process (executable-find "ros2")
+                           nil buff nil "bag" "info" (expand-file-name file)))
+            ("mcap"
+             (call-process (or (executable-find "mcap") (executable-find "mcap-cli"))
+                           nil buff nil "info" (expand-file-name file)))
             (rosbag-info-mode)))))))
 
 ;; Needed by ros.el
