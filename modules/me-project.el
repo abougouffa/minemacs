36ba600e45c5dded4f8f8a67f80bd875f98a07bd
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-24 01:02:04 +0100

    refactor(projectile): minor changes

--- modules/me-project.el
+++ modules/me-project.el
@@ -11,24 +11,25 @@
   :general
   (+map
     ;; Project
-    "pa"  '(projectile-add-known-project :wk "Add")
-    "pD"  '(projectile-edit-dir-locals :wk "Edit dir-locals")
-    ;;
-    "pc"  '(nil :wk "compile/test")
-    "pcc"  #'projectile-compile-project
-    "pcg"  #'projectile-configure-project
-    "pct"  #'projectile-test-project
-    "pci"  #'projectile-install-project
+    :infix "p"
+    "a"  '(projectile-add-known-project :wk "Add")
+    "D"  '(projectile-edit-dir-locals :wk "Edit dir-locals")
+    ;; Compile/test
+    "c"  '(nil :wk "compile/test")
+    "cc"  #'projectile-compile-project
+    "cg"  #'projectile-configure-project
+    "ct"  #'projectile-test-project
+    "ci"  #'projectile-install-project
     ;; Forget
-    "pF"  '(nil :wk "forget")
-    "pFz" '(projectile-cleanup-known-projects :wk "Zombie projects")
-    "pFp" '(projectile-remove-known-project :wk "Project")
-    "pFP" '(projectile-remove-current-project-from-known-projects :wk "Current project")
+    "F"  '(nil :wk "forget")
+    "Fz" '(projectile-cleanup-known-projects :wk "Zombie projects")
+    "Fp" '(projectile-remove-known-project :wk "Project")
+    "FP" '(projectile-remove-current-project-from-known-projects :wk "Current project")
     ;; Search/replace
-    "ps"  '(nil :wk "search/replace")
-    "pss" 'projectile-grep
-    "psn" '(fileloop-continue :wk "Next match")
-    "psr" #'projectile-replace-regexp)
+    "s"  '(nil :wk "search/replace")
+    "ss" 'projectile-grep
+    "sn" '(fileloop-continue :wk "Next match")
+    "sr" #'projectile-replace-regexp)
   :custom
   (projectile-cache-file (concat minemacs-cache-dir "projectile/cache.el"))
   (projectile-known-projects-file (concat minemacs-local-dir "projectile/known-projects.el"))
@@ -41,7 +42,7 @@
   (projectile-kill-buffers-filter 'kill-only-files)
   :init
   (global-set-key [remap evil-jump-to-tag] #'projectile-find-tag)
-  (global-set-key [remap find-tag]         #'projectile-find-tag)
+  (global-set-key [remap find-tag] #'projectile-find-tag)
   :config
   ;; HACK: from Doom Emacs
   ;; Projectile cleans up the known projects list at startup. If this list
@@ -58,13 +59,15 @@
   (+map
     ":"  '(consult-projectile-find-file :wk "Find file in project")
     ;; Buffer
-    "bp" #'consult-projectile-switch-to-buffer
+    "bp" #'consult-projectile-switch-to-buffer)
+  (+map
+    :infix "p"
     ;; Project
-    "pp" #'consult-projectile
-    "pP" '(consult-projectile-switch-project :wk "Switch")
-    "pr" #'consult-projectile-recentf
-    "pd" '(consult-projectile-find-dir :wk "Find directory")
-    "pf" '(consult-projectile-find-file :wk "Find file")))
+    "p" #'consult-projectile
+    "P" '(consult-projectile-switch-project :wk "Switch")
+    "r" #'consult-projectile-recentf
+    "d" '(consult-projectile-find-dir :wk "Find directory")
+    "f" '(consult-projectile-find-file :wk "Find file")))
 
 
 (provide 'me-project)
