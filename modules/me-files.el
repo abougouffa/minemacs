3a6ef1e24d0583a14825ae73cf481ad822abffe1
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-28 01:04:24 +0100

    tweak(dirvish): better config

--- modules/me-files.el
+++ modules/me-files.el
@@ -9,26 +9,26 @@
   :straight t
   :hook (minemacs-after-startup . dirvish-override-dired-mode)
   :custom
-  (dirvish-attributes '(file-size vc-state git-msg all-the-icons))
-  (dirvish-cache-dir (concat minemacs-cache-dir "dirvish/"))
+  (dirvish-attributes '(subtree-state all-the-icons file-size vc-state git-msg))
+  (dirvish-cache-dir (+directory-ensure (concat minemacs-cache-dir "dirvish/")))
   (dirvish-mode-line-format '(:left (sort file-time symlink) :right (omit yank index)))
   (dirvish-side-width 30)
   (dirvish-fd-default-dir "~/")
+  (dirvish-use-header-line t) ; 'global make header line span all panes
+  (dirvish-use-mode-line t)
   :general
   (+map
     ;; Open
-    "o-"  '(dirvish :wk "Dirvish")
-    "op"  '(dirvish-side :wk "Side panel")
-    "oq"  '(dirvish-quick-access :wk "Dirvish quick access")
+    "o-" '(dirvish :wk "Dirvish")
+    "op" '(dirvish-side :wk "Side panel")
+    "oq" '(dirvish-quick-access :wk "Dirvish quick access")
     ;; Search
-    "sd"  '(dirvish-fd :wk "Dirvish fd"))
-  (+map-key :keymaps 'dirvish-mode-map
-    "q" #'dirvish-quit)
+    "sd" '(dirvish-fd :wk "Dirvish fd"))
   :config
-  (require 'dirvish-fd)
-  (require 'dirvish-yank)
-  (require 'dirvish-icons)
-  (require 'dirvish-emerge)
+  (+map-key :keymaps 'dirvish-mode-map
+    "q" #'dirvish-quit
+    "s" #'dirvish-subtree-toggle
+    "y" #'dirvish-yank-menu))
 
 (use-package vlf-setup
   :straight vlf
