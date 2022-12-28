5f955f1f9606c68bfb87dbf3fa69077d2a220018
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-29 00:37:19 +0100

    tweak(flymake): nicer fringe bitmaps, remove flycheck (obsoleted)

--- modules/me-checkers.el
+++ modules/me-checkers.el
@@ -9,50 +9,61 @@
   :straight (:type built-in)
   :general
   (+map "tf" #'flymake-mode)
+  :custom
+  (flymake-fringe-indicator-position 'right-fringe)
+  (flymake-error-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-error))
+  (flymake-warning-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-warning))
+  (flymake-note-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-info))
   :config
   (+map-local :keymaps 'flymake-mode-map
     "f"  '(nil :wk "flymake")
     "fn" #'flymake-goto-next-error
     "fN" #'flymake-goto-prev-error
-    "fs" #'flymake-start))
+    "fs" #'flymake-start
+    "fb" #'flymake-show-buffer-diagnostics
+    "fp" #'flymake-show-project-diagnostics)
 
-(use-package flycheck
-  :straight t
-  :custom
-  (flycheck-idle-change-delay 1.0)
-  (flycheck-check-syntax-automatically '(save idle-change mode-enabled))
-  (flycheck-buffer-switch-check-intermediate-buffers t)
-  (flycheck-display-errors-delay 0.2)
-  :general
-  (+map "tc" #'flycheck-mode)
-  :config
-  (+map
-    :keymaps 'flycheck-error-list-mode-map
-    "j"   #'flycheck-error-list-next-error
-    "k"   #'flycheck-error-list-previous-error
-    "RET" #'flycheck-error-list-goto-error)
+  ;; Larger right frings
+  (set-fringe-style '(8 . 13))
 
-  ;; Use the current session load path when checking
-  (setq flycheck-emacs-lisp-load-path 'inherit)
+  ;; Better fringe bitmaps
+  (when (fboundp 'define-fringe-bitmap)
+    (define-fringe-bitmap '+flymake-bitmap-arrow
+      [#b11111000
+       #b01111100
+       #b00111110
+       #b00011111
+       #b00111110
+       #b01111100
+       #b11111000])
+    (define-fringe-bitmap '+flymake-bitmap-arrow-hi-res
+      [#b01111000000
+       #b00111100000
+       #b00011110000
+       #b00001111000
+       #b00000111100
+       #b00000011110
+       #b00000011110
+       #b00000111100
+       #b00001111000
+       #b00011110000
+       #b00111100000
+       #b01111000000]
+      nil 13)
+    (define-fringe-bitmap '+flymake-bitmap-left-arrow-hi-res
+      [#b00000011110
+       #b00000111100
+       #b00001111000
+       #b00011110000
+       #b00111100000
+       #b01111000000
+       #b01111000000
+       #b00111100000
+       #b00011110000
+       #b00001111000
+       #b00000111100
+       #b00000011110]
+      nil 13)))
 
-  ;; Integrate Flycheck with eldoc (not working with clangd!)
-  (with-eval-after-load 'eldoc
-    (add-hook 'eldoc-documentation-functions '+flycheck-eldoc-function t t)
-    (setq eldoc-documentation-strategy #'eldoc-documentation-compose)
-
-    (setq flycheck-display-errors-function nil) ; do not show
-
-    (defun +flycheck-eldoc-function (report-doc &rest _)
-      "Document diagnostics at point, intended for `eldoc-documentation-functions'."
-      (when-let ((diags (flycheck-overlay-errors-at (point))))
-        (propertize
-         (funcall flycheck-help-echo-function diags)
-         'help-echo-inhibit-substitution t)))))
-
-(use-package me-flycheck-eglot
-  :after flycheck eglot)
-
-(use-package me-flycheck-cmake
-  :after flycheck)
 
 (provide 'me-checkers)
