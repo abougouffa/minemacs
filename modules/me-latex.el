a91dd34d42487bf7bb028b0c62b6a3a2c77ddfac
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-28 01:03:33 +0100

    refactor(latex): better style

--- modules/me-latex.el
+++ modules/me-latex.el
@@ -6,57 +6,59 @@
 
 
 ;; Adapted from Doom Emacs
-(use-package tex
-  :straight auctex ; (auctex :files ("*.el" "*.info" "dir" "doc" "etc" "images" "latex" "style"))
-  :general
+(use-package auctex
+  :straight t
+  :hook ((tex-mode TeX-mode latex-mode LaTeX-mode) . TeX-source-correlate-mode)
+  :custom
+  (TeX-parse-self t) ; parse on load
+  (TeX-auto-save t)  ; parse on save
+  (TeX-auto-local ".auctex-auto") ; use hidden directories for AUCTeX files.
+  (TeX-style-local ".auctex-style")
+  (TeX-source-correlate-method 'synctex)
+  (TeX-source-correlate-start-server nil) ; don't start the Emacs server when correlating sources.
+  (TeX-electric-sub-and-superscript t) ; automatically insert braces after sub/superscript in `LaTeX-math-mode'.
+  (TeX-save-query nil) ; just save, don't ask before each compilation.
+  :config
   (+map-local :keymaps '(tex-mode-map TeX-mode-map latex-mode-map LaTeX-mode-map)
     "c" #'TeX-command-run-all
     "m" #'TeX-command-master
     "v" #'TeX-view)
-  :config
-  (setq TeX-parse-self t ; parse on load
-        TeX-auto-save t  ; parse on save
-        ;; Use hidden directories for AUCTeX files.
-        TeX-auto-local ".auctex-auto"
-        TeX-style-local ".auctex-style"
-        TeX-source-correlate-mode t
-        TeX-source-correlate-method 'synctex
-        ;; Don't start the Emacs server when correlating sources.
-        TeX-source-correlate-start-server nil
-        ;; Automatically insert braces after sub/superscript in `LaTeX-math-mode'.
-        TeX-electric-sub-and-superscript t
-        ;; Just save, don't ask before each compilation.
-        TeX-save-query nil)
-  (when (featurep 'pdf-tools)
-    (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools")))
-  (setq-default TeX-master nil))
+  (with-eval-after-load 'pdf-tools
+    (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools"))))
 
 ;; Adapted from Doom Emacs
 (use-package auctex-latexmk
   :straight t
   :after latex
-  :init
-  (setq auctex-latexmk-inherit-TeX-PDF-mode t)
-  ;; Set LatexMk as the default.
-  (add-hook
-   'LaTeX-mode
-   (defun +tex--set-latexmk-as-default-cmd-h ()
-     (setq TeX-command-default "LatexMk")))
+  :hook (LaTeX-mode . +tex--set-latexmk-as-default-cmd-h)
+  :defines +tex--set-latexmk-as-default-cmd-h
+  :custom
+  (auctex-latexmk-inherit-TeX-PDF-mode t)
   :config
+  (defun +tex--set-latexmk-as-default-cmd-h ()
+    (setq TeX-command-default "LatexMk"))
   ;; Add LatexMk as a TeX target.
   (auctex-latexmk-setup))
 
-;; Set up mode for bib files.
-(with-eval-after-load 'bibtex
-  (setq bibtex-dialect 'biblatex
-        bibtex-align-at-equal-sign t
-        bibtex-text-indentation 20)
-  (define-key bibtex-mode-map (kbd "C-c \\") #'bibtex-fill-entry))
+(use-package bibtex
+  :straight (:type built-in)
+  :defer t
+  :hook (bibtex-mode . display-line-numbers-mode)
+  :custom
+  (bibtex-dialect 'biblatex)
+  (bibtex-align-at-equal-sign t)
+  (bibtex-text-indentation 20)
+  :config
+  (+map-local
+    "f" #'bibtex-fill-entry
+    "r" #'bibtex-reformat))
 
 ;; Adapted from Doom Emacs
 (use-package reftex
   :straight (:type built-in)
   :hook (LaTeX-mode . turn-on-reftex)
+  :hook (reftex-toc-mode . +reftex--toc-tweaks-h)
+  :defines +reftex--toc-tweaks-h
   :config
   ;; Set up completion for citations and references.
   ;; (set-company-backend! 'reftex-mode 'company-reftex-labels 'company-reftex-citations)
@@ -76,19 +78,17 @@
         ;; This is needed when `reftex-cite-format' is set. See
         ;; https://superuser.com/a/1386206
         LaTeX-reftex-cite-format-auto-activate nil)
-  (when (featurep 'evil)
+  (with-eval-after-load 'evil
     (add-hook 'reftex-mode-hook #'evil-normalize-keymaps))
   (+map-local :keymaps 'reftex-mode-map
     ";" 'reftex-toc)
-  (add-hook
-   'reftex-toc-mode-hook
-   (defun +reftex--toc-tweaks-h ()
-     (reftex-toc-rescan)
-     (+map-key :keymaps 'reflex-toc-mode-map
-       "j"   #'next-line
-       "k"   #'previous-line
-       "q"   #'kill-buffer-and-window
-       "ESC" #'kill-buffer-and-window))))
+  (defun +reftex--toc-tweaks-h ()
+    (reftex-toc-rescan)
+    (+map-key :keymaps 'reflex-toc-mode-map
+      "j"   #'next-line
+      "k"   #'previous-line
+      "q"   #'kill-buffer-and-window
+      "ESC" #'kill-buffer-and-window)))
 
 
 (provide 'me-latex)
