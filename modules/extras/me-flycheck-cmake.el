27309133eae207627724877eb4095cf758d8fdcc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-28 12:58:08 +0100

    revert+refactor: restore flycheck-cmake, move fly* to new module

---
+++ modules/extras/me-flycheck-cmake.el
@@ -0,0 +1,65 @@
+;;; me-flycheck-cmake.el --- flycheck integration for CMake-based projects
+
+
+(require 'json)
+
+(defvar-local +flycheck-cmake-json nil)
+(defvar-local +flycheck-cmake-compiler nil)
+
+(defun +flycheck-cmake-clear-cache ()
+  "Clear cached compiler and \"compile_commands.json\" data."
+  (interactive)
+  (setq-local +flycheck-cmake-json nil
+              +flycheck-cmake-compiler nil))
+
+(defun +flycheck-cmake-get-compile-db-file (&optional root)
+  "Get the \"compile_commands.json\" file starting from ROOT path.
+When ROOT is nil, use the project root."
+  (catch 'file-found
+    (let ((proj-root (or root (project-root (project-current)))))
+      (dolist (file '("compile_commands.json"
+                      "build/compile_commands.json"
+                      "build/release/compile_commands.json"
+                      "build/debug/compile_commands.json"))
+        (let ((full-file-name (expand-file-name file proj-root)))
+          (when (file-exists-p full-file-name)
+            (throw 'file-found full-file-name)))))))
+
+(defun +flycheck-cmake-extract-args (&optional filename)
+  "Extract compiler arguments for FILENAME.
+When FILENAME is nil, use the file name of the current buffer."
+  (let ((file (replace-regexp-in-string
+               ;; TODO: Check if the file exists, if not try with ".cc" or ".c" instead of ".cpp"
+               (rx (seq "." (or "h" "hpp" "hh") eol)) ".cpp"
+               (file-truename (or filename (buffer-file-name))))))
+    (when (and (derived-mode-p 'c++-mode)
+               (string-match (file-truename (project-root (project-current))) file))
+      (unless +flycheck-cmake-json
+        (when-let ((db-file (+flycheck-cmake-get-compile-db-file)))
+          (setq-local +flycheck-cmake-json (json-read-file db-file))))
+      (let* ((matched-entry (cl-find-if
+                             (lambda (entry)
+                               (equal (file-truename (cdr (assq 'file entry))) file))
+                             +flycheck-cmake-json))
+             (cmd (cdr (assq 'command matched-entry)))
+             (cmake-args ""))
+        (when cmd
+          (let ((lst (split-string (replace-regexp-in-string " +-o .*\\|(\\|)" "" cmd))))
+            (unless +flycheck-cmake-compiler
+              (setq-local +flycheck-cmake-compiler (car lst)))
+            (setq cmake-args (cdr lst))))))))
+
+;;;###autoload
+(defun +flycheck-cmake-setup (&rest _)
+  "Setup GCC and Clang checkers with the right arguments."
+  (interactive)
+  (when (derived-mode-p 'c-mode 'c-ts-mode 'c++-mode 'c++-ts-mode)
+    (ignore-errors
+      (let ((args (+flycheck-cmake-extract-args)))
+        (setq-local flycheck-gcc-args args
+                    flycheck-clang-args args)))))
+
+(advice-add 'flycheck-mode :before #'+flycheck-cmake-setup)
+
+
+(provide 'me-flycheck-cmake)
---
+++ modules/me-checkers.el
@@ -0,0 +1,58 @@
+;;; me-prog.el --- Programming stuff -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
+
+
+(use-package flymake
+  :straight (:type built-in)
+  :general
+  (+map "tf" #'flymake-mode)
+  :config
+  (+map-local :keymaps 'flymake-mode-map
+    "f"  '(nil :wk "flymake")
+    "fn" #'flymake-goto-next-error
+    "fN" #'flymake-goto-prev-error
+    "fs" #'flymake-start))
+
+(use-package flycheck
+  :straight t
+  :custom
+  (flycheck-idle-change-delay 1.0)
+  (flycheck-check-syntax-automatically '(save idle-change mode-enabled))
+  (flycheck-buffer-switch-check-intermediate-buffers t)
+  (flycheck-display-errors-delay 0.2)
+  :general
+  (+map "tc" #'flycheck-mode)
+  :config
+  (+map
+    :keymaps 'flycheck-error-list-mode-map
+    "j"   #'flycheck-error-list-next-error
+    "k"   #'flycheck-error-list-previous-error
+    "RET" #'flycheck-error-list-goto-error)
+
+  ;; Use the current session load path when checking
+  (setq flycheck-emacs-lisp-load-path 'inherit)
+
+  ;; Integrate Flycheck with eldoc (not working with clangd!)
+  (with-eval-after-load 'eldoc
+    (add-hook 'eldoc-documentation-functions '+flycheck-eldoc-function t t)
+    (setq eldoc-documentation-strategy #'eldoc-documentation-compose)
+
+    (setq flycheck-display-errors-function nil) ; do not show
+
+    (defun +flycheck-eldoc-function (report-doc &rest _)
+      "Document diagnostics at point, intended for `eldoc-documentation-functions'."
+      (when-let ((diags (flycheck-overlay-errors-at (point))))
+        (propertize
+         (funcall flycheck-help-echo-function diags)
+         'help-echo-inhibit-substitution t)))))
+
+(use-package me-flycheck-eglot
+  :after flycheck eglot)
+
+(use-package me-flycheck-cmake
+  :after flycheck)
+
+(provide 'me-checkers)
