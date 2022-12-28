41b76a821c598c62897f470073814594eb285ad9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-28 01:00:17 +0100

    doc(skel): add early-config.el example

---
+++ skel/early-config.el
@@ -0,0 +1,8 @@
+;;; early-config.el -*- coding: utf-8-unix; lexical-binding: t; -*-
+
+;; This file will be loaded at the end of `early-init.el', it can be used to set
+;; some early initialization stuff, or to set some MinEmacs variables, specially
+;; these used in macros.
+
+;; Set log level to `info' rather than `error'
+(setq minemacs-msg-level 2)
