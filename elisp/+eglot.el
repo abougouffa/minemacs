c2f64d0e9391990a3a30264c1e40255ab7e45263
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-29 15:22:36 +0100

    fix(eglot): correct handling of the case of single server

--- elisp/+eglot.el
+++ elisp/+eglot.el
@@ -8,11 +8,11 @@ Examples:
   (+eglot-register 'lua-mode \"lua-language-server\" \"lua-lsp\")
   (+eglot-register '(c-mode c++-mode) '(\"clangd\" \"--clang-tidy\" \"-j=12\") \"ccls\")"
   (declare (indent 0))
-  (let* ((multi-p (> (length servers) 1))
+  (let* ((multi-p (length> servers 1))
          (first-server (car servers))
          (first-server (if (listp first-server) (car first-server) first-server)))
     (with-eval-after-load 'eglot
       (when (executable-find first-server)
         (add-to-list
          'eglot-server-programs
-         `(,modes . ,(if multi-p (eglot-alternatives (ensure-list servers)) servers)))))))
+         `(,modes . ,(if multi-p (eglot-alternatives (ensure-list servers)) (car servers))))))))
