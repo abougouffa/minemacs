de76442bef90162c29e804fbc1941fedba757f69
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-29 22:45:58 +0100

    fix(eglot): the case of single server with no args!

--- elisp/+eglot.el
+++ elisp/+eglot.el
@@ -8,11 +8,13 @@ Examples:
   (+eglot-register 'lua-mode \"lua-language-server\" \"lua-lsp\")
   (+eglot-register '(c-mode c++-mode) '(\"clangd\" \"--clang-tidy\" \"-j=12\") \"ccls\")"
   (declare (indent 0))
-  (let* ((multi-p (length> servers 1))
+  (let* ((alternatives-p (length> servers 1))
          (first-server (car servers))
          (first-server (if (listp first-server) (car first-server) first-server)))
     (with-eval-after-load 'eglot
       (when (executable-find first-server)
         (add-to-list
          'eglot-server-programs
-         `(,modes . ,(if multi-p (eglot-alternatives (ensure-list servers)) (car servers))))))))
+         (cons modes (if alternatives-p
+                         (eglot-alternatives (ensure-list servers))
+                       (ensure-list (car servers)))))))))
