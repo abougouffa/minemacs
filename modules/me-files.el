2691ac7b27b717eb25459dd7f8d4ef3f1d5fcb97
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-12-31 01:31:33 +0100

    feat(treemacs): initial support

--- modules/me-files.el
+++ modules/me-files.el
@@ -20,7 +20,6 @@
   (+map
     ;; Open
     "o-" '(dirvish :wk "Dirvish")
-    "op" '(dirvish-side :wk "Side panel")
     "oq" '(dirvish-quick-access :wk "Dirvish quick access")
     ;; Search
     "sd" '(dirvish-fd :wk "Dirvish fd"))
@@ -32,6 +31,23 @@
 (use-package vlf-setup
   :straight vlf
   :after minemacs-loaded)
+
+(use-package treemacs
+  :straight t
+  :defer t
+  :general
+  (+map
+    "op" '(treemacs :wk "Side panel"))
+  :custom
+  (treemacs-persist-file (concat minemacs-local-dir "treemacs/persist.el"))
+  (treemacs-last-error-persist-file (concat minemacs-local-dir "treemacs/last-error-persist.el"))
+  (treemacs-width 30))
+
+(use-package treemacs-all-the-icons
+  :straight t
+  :after treemacs all-the-icons
+  :config
+  (treemacs-load-theme "all-the-icons"))
 
 
 (provide 'me-files)
