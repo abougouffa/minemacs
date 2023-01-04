8405e58e574210502f04e5e947a16eecd90eeb91
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-01-05 00:07:12 +0100

    feat(workspaces): WIP integration for tabspaces and tab-bar

--- modules/me-workspaces.el
+++ modules/me-workspaces.el
@@ -4,8 +4,39 @@
 
 ;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
 
-;; TODO Customization for windows, popups, workspaces (via tab-bar-mode and tab-line-mode)
-;; https://www.masteringemacs.org/article/demystifying-emacs-window-manager
+(use-package tabspaces
+  :straight t
+  :hook (minemacs-after-startup . tabspaces-mode)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  ;; sessions
+  (tabspaces-session t)
+  (tabspaces-session-auto-restore t))
+
+(use-package tab-bar
+  :config
+  (setq tab-bar-format '(tab-bar-format-history
+                         tab-bar-format-tabs
+                         tab-bar-separator)
+        tab-bar-tab-hints t
+        tab-bar-tab-name-format-function #'+tab-bar-tab-spaced-name-format
+        tab-bar-close-button-show t)
+
+  (defun +tab-bar-tab-spaced-name-format (tab i)
+    (let ((current-p (eq (car tab) 'current-tab)))
+      (propertize
+       (concat (if tab-bar-tab-hints (format " %c " (+ ?① (1- i)) " "))
+               (alist-get 'name tab)
+               (or (and tab-bar-close-button-show
+                        (not (eq tab-bar-close-button-show
+                                 (if current-p 'non-selected 'selected)))
+                        tab-bar-close-button)
+                   ""))
+       'face (funcall tab-bar-tab-face-function tab)))))
+
 
 
 (provide 'me-workspaces)
