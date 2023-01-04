bc7e9b0cf4030084d0e17e22634ddc39250b1622
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-01-04 01:09:05 +0100

    refactor(early-init): minor changes, additional comments

--- early-init.el
+++ early-init.el
@@ -1,21 +1,22 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
 (setq
- ;; Garbage collection
- ;; Increase the GC threshold for faster startup
- ;; This will be overriten when `gcmh' is loaded
+ ;; Garbage collection tweaks: Increase the GC threshold for faster startup This
+ ;; will be overriten when `gcmh' (Garbage Collector Magic Hack) is loaded.
  gc-cons-threshold most-positive-fixnum
  ;; Prefer loading newest compiled .el file
- load-prefer-newer t)
-
-;; Remove some unneeded UI elements
-(setq default-frame-alist
-      '((tool-bar-lines . 0)
-        (menu-bar-lines . 0)
-        (vertical-scroll-bars)
-        (mouse-color . "blue")
-        (left-fringe . 8)
-        (right-fringe . 13)))
+ load-prefer-newer t
+ ;; Remove some unneeded UI elements
+ default-frame-alist '((tool-bar-lines . 0)
+                       (menu-bar-lines . 0)
+                       (vertical-scroll-bars)
+                       (mouse-color . "blue")
+                       (left-fringe . 8)
+                       (right-fringe . 13))
+ ;; Set explicitly disabled modes
+ tool-bar-mode nil
+ menu-bar-mode nil
+ scroll-bar-mode nil)
 
 ;; You can set the MINEMACS_ALPHA environment variable to an alpha percentage
 (when (>= emacs-major-version 29)
@@ -23,10 +24,6 @@
               (alpha (string-to-number alpha)))
     (push (cons 'alpha-background (if (zerop alpha) 93 alpha))
           default-frame-alist)))
-
-(setq tool-bar-mode nil
-      menu-bar-mode nil
-      scroll-bar-mode nil)
 
 ;; For `lsp-mode' performance, set it here so we don't need to add it to the
 ;; system's environment variables.
