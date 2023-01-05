1246ff5e3ddc87c55be099101043897db2abf87a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-01-06 00:52:03 +0100

    tweak(eaf): split the config

--- modules/me-eaf.el
+++ modules/me-eaf.el
@@ -8,20 +8,12 @@
 (use-package eaf
   :straight (:host github :repo "emacs-eaf/emacs-application-framework" :files (:defaults "*"))
   :unless (+emacs-features-p 'lucid)
-  :commands
-  eaf-install-and-update eaf-open eaf-open-jupyter eaf-open-mindmap
-  eaf-file-sender-qrcode eaf-file-sender-qrcode-in-dired eaf-open-browser
-  +eaf-open-mail-as-html +browse-url-eaf
   :general
   (+map
     "oo" '(eaf-open :wk "Open with EAF"))
-  (+map-local
-    :keymaps '(mu4e-headers-mode-map mu4e-view-mode-map)
-    "h" '(+eaf-open-mail-as-html :wk "Open mail as HTML")
-    "o" '(eaf-open-browser :wy "Open URL (EAF)"))
   :custom
   ;; Generic
-  (eaf-apps-to-install '(browser mindmap jupyter org-previewer
+  (eaf-apps-to-install '(browser mindmap jupyter org-previewer pdf-viewer
                          markdown-previewer file-sender video-player))
   (eaf-start-python-process-when-require t)
   (eaf-kill-process-after-last-buffer-closed t)
@@ -42,21 +34,18 @@
   (eaf-webengine-download-path "~/Downloads")
   (eaf-webengine-enable-plugin t)
   (eaf-webengine-enable-javascript t)
-  (eaf-webengine-enable-javascript-access-clipboard t)
-  ;; Jupyter
-  (eaf-jupyter-font-family (plist-get minemacs-fonts :font-family))
-  (eaf-jupyter-font-size 14)
-  ;; Video player
-  (eaf-video-player-keybinding
-   '(("p" . "toggle_play")
-     ("q" . "close_buffer")
-     ("h" . "play_backward")
-     ("l" . "play_forward")
-     ("j" . "decrease_volume")
-     ("k" . "increase_volume")
-     ("f" . "toggle_fullscreen")
-     ("R" . "restart")))
-  ;; Browser
+  (eaf-webengine-enable-javascript-access-clipboard t))
+
+(use-package eaf-browser
+  :after eaf
+  :demand t
+  :commands +eaf-open-mail-as-html +browse-url-eaf eaf-open-browser
+  :general
+  (+map-local
+    :keymaps '(mu4e-headers-mode-map mu4e-view-mode-map)
+    "h" '(+eaf-open-mail-as-html :wk "Open mail as HTML")
+    "o" '(eaf-open-browser :wy "Open URL (EAF)"))
+  :custom
   (eaf-browser-continue-where-left-off t)
   (eaf-browser-enable-adblocker t)
   (eaf-browser-ignore-history-list '("google.com/search" "file://"))
@@ -67,31 +56,6 @@
   (eaf-browser-continue-where-left-off t)
   (eaf-browser-aria2-auto-file-renaming t)
   :config
-  ;; Apps
-  (require 'eaf-browser nil t)
-  (require 'eaf-jupyter nil t)
-  (require 'eaf-mindmap nil t)
-  (require 'eaf-file-sender nil t)
-  (require 'eaf-video-player nil t)
-
-  (defun +eaf-all-the-icons--setup ()
-    (require 'eaf-all-the-icons nil t)
-    (mapc (lambda (v) (eaf-all-the-icons-icon (car v)))
-          eaf-all-the-icons-alist))
-
-  (cond
-   ((display-graphic-p)
-    (+eaf-all-the-icons--setup))
-   ((daemonp)
-    (add-hook
-     'server-after-make-frame-hook
-     (defun +eaf-all-the-icons--setup-once-h ()
-       (when (display-graphic-p)
-         (+eaf-all-the-icons--setup)
-         (remove-hook
-          'server-after-make-frame-hook
-          #'+eaf-all-the-icons--setup-once-h))))))
-
   (defun +browse-url-eaf (url &rest args)
     "Open URL in EAF Browser."
     (interactive (browse-url-interactive-arg "URL: "))
@@ -107,6 +71,49 @@
              (browse-url-browser-function #'eaf-open-browser))
         (mu4e-action-view-in-browser msg)
       (message "No message at point."))))
+
+(use-package eaf-jupyter
+  :after eaf
+  :commands eaf-open-jupyter
+  :custom
+  (eaf-jupyter-font-family (plist-get minemacs-fonts :font-family))
+  (eaf-jupyter-font-size 14))
+
+(use-package eaf-pdf-viewer
+  :after eaf
+  :commands eaf-open-office
+  :custom
+  (eaf-pdf-outline-buffer-indent 2))
+
+(use-package eaf-video-player
+  :after eaf
+  :custom
+  (eaf-video-player-keybinding
+   '(("p" . "toggle_play")
+     ("q" . "close_buffer")
+     ("h" . "play_backward")
+     ("l" . "play_forward")
+     ("j" . "decrease_volume")
+     ("k" . "increase_volume")
+     ("f" . "toggle_fullscreen")
+     ("R" . "restart"))))
+
+(use-package eaf-file-sender
+  :after eaf
+  :commands eaf-file-sender-qrcode eaf-file-sender-qrcode-in-dired)
+
+(use-package eaf-mindmap
+  :after eaf
+  :commands eaf-create-mindmap eaf-open-mindmap)
+
+(use-package eaf-all-the-icons
+  :after eaf all-the-icons)
+
+(use-package eaf-markdown-previewer
+  :after eaf markdown-mode)
+
+(use-package eaf-org-previewer
+  :after eaf org)
 
 
 (provide 'me-eaf)
