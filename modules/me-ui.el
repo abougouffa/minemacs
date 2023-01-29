e058a7617f1ef9333b9e0a30a22a6f21d214f3ba
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-01-29 02:40:57 +0100

    fix(writeroom): org-latex scaling

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -75,16 +75,16 @@
      (defun +writeroom--scale-up-latex-h ()
        (setq-local +writeroom-org-format-latex-scale
                    (plist-get org-format-latex-options :scale))
-       (set (make-local-variable 'org-format-latex-options)
-            (plist-put org-format-latex-options
-                       :scale (if (+emacs-features-p 'pgtk) 1.4 2.1)))))
+       (setq org-format-latex-options
+             (plist-put org-format-latex-options
+                        :scale (if (+emacs-features-p 'pgtk) 1.4 2.1)))))
 
     (add-hook
      'writeroom-mode-disable-hook
      (defun +writeroom--scale-down-latex-h ()
-       (set (make-local-variable 'org-format-latex-options)
-            (plist-put org-format-latex-options
-                       :scale (or +writeroom-org-format-latex-scale 1.0)))))))
+       (setq org-format-latex-options
+             (plist-put org-format-latex-options
+                        :scale (or +writeroom-org-format-latex-scale 1.0)))))))
 
 (use-package mixed-pitch
   :straight t
