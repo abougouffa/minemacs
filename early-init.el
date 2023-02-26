4bc7caf4e9462149b9785de5cb4c9dae68db7772
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-02-27 00:06:26 +0100

    doc(early-init): extensive documentation for MinEmacs

--- early-init.el
+++ early-init.el
@@ -1,9 +1,9 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
 (setq
- ;; Garbage collection tweaks: Increase the GC threshold for faster startup.
- ;; This will be overriten when `gcmh' (Garbage Collector Magic Hack) gets
- ;; loaded in `me-gc'.
+ ;; HACK: Increase the garbage collection (GC) threshold for faster startup.
+ ;; This will be overwritten when `gcmh-mode' (a.k.a. the Garbage Collector
+ ;; Magic Hack) gets loaded in the `me-gc' module (see "init.el").
  gc-cons-threshold most-positive-fixnum
  ;; Prefer loading newest compiled .el file
  load-prefer-newer t
@@ -15,26 +15,36 @@
                        (left-fringe . 8)
                        (right-fringe . 13)
                        (fullscreen . maximized))
- ;; Set explicitly disabled modes
+ ;; Explicitly set modes disabled in `default-frame-alist' to nil
  tool-bar-mode nil
  menu-bar-mode nil
  scroll-bar-mode nil)
 
-;; You can set the MINEMACS_ALPHA environment variable to an alpha percentage
+;; NOTE: In Emacs29+, frames can have a transparent background via the
+;; `alpha-background' parameter. For a better experience, this value should be
+;; set early before any frame gets created (i.e. in "early-init.el"). MinEmacs
+;; uses the "$MINEMACS_ALPHA" environment variable that can be set to an integer
+;; value in the [1-100] range (the alpha percentage). When this variable is not
+;; set, Emacs will load the default GUI (without background alpha), and when it
+;; is set but the value is not valid, MinEmacs will fallback to the default
+;; alpha of 93%.
 (when (>= emacs-major-version 29)
   (when-let* ((alpha (getenv "MINEMACS_ALPHA"))
               (alpha (string-to-number alpha)))
     (push (cons 'alpha-background (if (zerop alpha) 93 alpha))
           default-frame-alist)))
 
-;; For `lsp-mode' performance, set it here so we don't need to add it to the
-;; system's environment variables.
+;; HACK: In `lsp-mode' (see the `me-lsp' module), the user can define the
+;; "$LSP_USE_PLISTS=true" to improve `lsp-mode' performances. We set this
+;; environment variable here so we don't need to add it to the system's
+;; environment variables.
 (setenv "LSP_USE_PLISTS" "true")
 
-;; Load MinEmacs variables first
+;; Load MinEmacs variables from the `me-vars' core module.
 (load (expand-file-name "core/me-vars.el" (file-name-directory (file-truename load-file-name))) nil t)
 
-;;; Load the early config file if it exists
+;; Load the user early configuration file from "$MINEMACSDIR/early-config.el"
+;; if it exists.
 (let ((early-config-path (concat minemacs-config-dir "early-config.el")))
   (when (file-exists-p early-config-path)
     (load early-config-path nil (not minemacs-verbose))))
