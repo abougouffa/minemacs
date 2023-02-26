0b5f3325333ef12c49fe126966ed5a9b625fb85f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-02-27 00:02:47 +0100

    doc(init): extensive documentation of MinEmacs

--- init.el
+++ init.el
@@ -16,44 +16,63 @@ You are running Emacs v%s, this version should work BUT IT IS NOT TESTED."
       (error "Emacs v%s is not supported, MinEmacs requires v%s or higher"
              emacs-version supported-version))))
 
-;; Load vars if they have not been loaded (if Emacs has been loaded directly
-;; from "init.el" without passing by "early-init.el")
+;; HACK: At this point, MinEmacs variables defined in `me-vars' should be
+;; already loaded (in "early-init.el"). However, we double-check here and load
+;; them if necessary in case Emacs has been loaded directly from "init.el"
+;; without passing by "early-init.el". This can happen when we are running in a
+;; `me-org-export-async-init' context, or if we use some bootstrapping mechanism
+;; like Chemacs2.
 (unless (featurep 'me-vars)
-  ;; Load MinEmacs variables first
+  ;; Load MinEmacs variables from `me-vars'
   (load (expand-file-name "core/me-vars.el" (file-name-directory (file-truename load-file-name))) nil t))
 
-;; Overwrite `user-emacs-directory' with `minemacs-local-dir'
+;; HACK: Most core and third-party packages depends on the
+;; `user-emacs-directory' variable to store some cache information and generated
+;; configuration files. However, this will mess with MinEmacs' directory (which
+;; defaults to `user-emacs-directory'). To keep the "~/.emacs.d/" directory
+;; clean, we overwrite the `user-emacs-directory' with `minemacs-local-dir' so
+;; all generated files gets stored in "~/.emacs.d/local/".
+;; BUG: It is important to set this here and not in `me-vars' nor in
+;; "early-init.el", otherwise, it won't work with Chemacs2-based installations.
 (setq user-emacs-directory minemacs-local-dir)
 
-;; Load Emacs 29 backports for earlier Emacs versions
+;; HACK: Load Emacs 29 back ports for earlier Emacs versions. Note that I do
+;; only back port a very small number of the functions/variables that I use from
+;; Emacs29+ to be compatible with Emacs 28.2. If you need a complete forward
+;; compatibility, you can install and use the `compat' library.
 (when (< emacs-major-version 29)
   (load (concat minemacs-modules-dir "me-backports-29.el") nil (not minemacs-verbose)))
 
 (setq
- ;; Enable debugging on error when env variable "MINEMACS_DEBUG" is defined
+ ;; Enable debugging on error when Emacs is launched with the "--debug-init"
+ ;; option or when the environment variable "$MINEMACS_DEBUG" is defined (see
+ ;; `me-vars').
  debug-on-error minemacs-debug
- ;; Make byte compilation less verbose
+ ;; Make byte compilation less noisy
  byte-compile-warnings minemacs-verbose
  byte-compile-verbose minemacs-verbose)
 
 ;; Native compilation settings
 (when (featurep 'native-compile)
   (setq
-   ;; Silence compiler warnings as they can be pretty disruptive
+   ;; Silence compiler warnings as they can be pretty disruptive, unless we are
+   ;; running in `minemacs-verbose' mode.
    native-comp-async-report-warnings-errors (when minemacs-verbose 'silent)
    native-comp-verbose (if minemacs-verbose 3 0))
 
-  ;; Make native compilation happens asynchronously
-  ;; HACK Temporary support deprecated variable `native-comp-deferred-compilation'
+  ;; Make native compilation happens asynchronously.
+  ;; HACK: Temporary support the `native-comp-deferred-compilation' variable
+  ;; deprecated late in Emacs29.
   (set (if (boundp 'native-comp-deferred-compilation)
-           'native-comp-deferred-compilation ; DEPECATED
+           'native-comp-deferred-compilation ; Deprecated in Emacs29
          'native-comp-jit-compilation)
        t)
 
-  ;; Set the right directory to store the native compilation cache
+  ;; Set the right directory to store the native compilation cache to avoid
+  ;; messing with "~/.emacs.d/".
   (startup-redirect-eln-cache (concat minemacs-cache-dir "eln/")))
 
-;; Add direcotries to `load-path'
+;; Add some of MinEmacs' directories to `load-path'.
 (add-to-list 'load-path minemacs-core-dir)
 (add-to-list 'load-path minemacs-elisp-dir)
 (add-to-list 'load-path minemacs-extras-dir)
@@ -82,11 +101,32 @@ You are running Emacs v%s, this version should work BUT IT IS NOT TESTED."
 ;; Load autoloads file
 (load minemacs-autoloads-file nil (not minemacs-verbose))
 
-;; Load environment variables when available
+;; Load environment variables when available.
+;; HACK: When Emacs is launched from the terminal (in GNU/Linux), it inherits
+;; the terminal's environment variables, which can be useful specially for
+;; running commands under a custom "$PATH" directory. But when Emacs is launched
+;; from the desktop manager (KDE, Gnome, etc.), it can omit the terminal's
+;; environment variables. The way I solve this is by launching Emacs from
+;; terminal, which gives Emacs the full environment variables of the invoking
+;; terminal. Then I call the `+env-save' command, which reads the environment
+;; variables defined in `+env-save-vars' and stores them in
+;; "~/.emacs.d/local/system-env.el". This file is then loaded in the future
+;; Emacs sessions (launched either from terminal or from GUI) by calling the
+;; `+env-load' command.
 (+env-load)
 
-;; Syncronization point!
-;; Profile emacs startup and trigger `minemacs-loaded' 5s after loading Emacs
+;; NOTE: This is MinEmacs' synchronization point. To get a fast Emacs startup,
+;; MinEmacs tries to defer loading packages until this hook is executed. This is
+;; managed by the `minemacs-loaded' and `minemacs-lazy' pseudo-packages. After
+;; loading Emacs, the `emacs-startup-hook' gets executed, we use this hook to
+;; profile the startup time, load the fonts and the theme, and setup the
+;; *scratch* buffer content. Lastly we trigger the `minemacs-loaded'
+;; synchronization module, which runs the `minemacs-after-startup-hook' hooks
+;; and provide `minemacs-loaded' so the packages loaded with `:after
+;; minemacs-loaded' can be loaded. The `minemacs-loaded' will trigger
+;; `minemacs-lazy', which provides `minemacs-lazy' so the packages loaded with
+;; `:after minemacs-lazy' can be loaded and incrementally run the hooks in
+;; `minemacs-lazy-hook' when Emacs gets idle.
 (add-hook
  'emacs-startup-hook
  (defun +minemacs--loaded-h ()
@@ -156,10 +196,18 @@ You are running Emacs v%s, this version should work BUT IT IS NOT TESTED."
 ;; Load fonts early (they are read from the default `minemacs-default-fonts').
 (+set-fonts)
 
-;; Ensure me-gc is in the list
+;; NOTE: Ensure the `me-gc' module is in the core modules list. This module
+;; enables the `gcmh-mode' package (a.k.a. the Garbage Collector Magic Hack).
+;; This GCMH minimizes GC interference with the activity by using a high GC
+;; threshold during normal use, then when Emacs is idling, GC is triggered and a
+;; low threshold is set. In MinEmacs, we set the threshold (`gc-cons-threshold'
+;; variable) to an unlimited size in "early-init.el", this helps improving the
+;; startup time, but needs to be set down to a more reasonable value after Emacs
+;; gets loaded. The use of `gcmh-mode' ensures reverting this value so we don't
+;; need to do it manually.
 (add-to-list 'minemacs-core-modules 'me-gc t)
 
-;; Core modules
+;; Load MinEmacs core modules
 (dolist (module minemacs-core-modules)
   (+log! "Loading core module \"%s\"" module)
   (let ((filename (concat minemacs-core-dir (format "%s.el" module))))
@@ -167,7 +215,7 @@ You are running Emacs v%s, this version should work BUT IT IS NOT TESTED."
         (load filename nil (not minemacs-verbose))
       (+error! "Core module \"%s\" not found!" module))))
 
-;; Modules
+;; Load MinEmacs modules
 (dolist (module minemacs-modules)
   (+log! "Loading module \"%s\"" module)
   (let ((filename (concat minemacs-modules-dir (format "%s.el" module))))
@@ -175,24 +223,26 @@ You are running Emacs v%s, this version should work BUT IT IS NOT TESTED."
         (load filename nil (not minemacs-verbose))
       (+error! "Module \"%s\" not found!" module))))
 
-;; Run hooks before loading user config
+;; Run the hooks in `minemacs-before-user-config-hook' before loading the user
+;; configuration and custom variables
 (require 'minemacs-before-user-config)
 
-;; Write user custom variables to separate file instead of init.el
+;; Write user custom variables to separate file instead of "init.el"
 (setq custom-file (concat minemacs-config-dir "custom-vars.el"))
 
+;; Load the custom variables file if it exists
 (when (and custom-file (file-exists-p custom-file))
   (+log! "Loafing user custom file from \"%s\"" custom-file)
   (load custom-file nil (not minemacs-verbose)))
 
-;; Load user config when available
+;; Load user configuration from "$MINEMACSDIR/config.el" when available
 (let ((user-config (concat minemacs-config-dir "config.el")))
   (when (file-exists-p user-config)
     (+log! "Loading user config file from \"%s\"" user-config)
     (load user-config nil (not minemacs-verbose))))
 
 (with-eval-after-load 'minemacs-loaded
-  ;; Delete outdated natively compiled files
+  ;; Delete outdated natively compiled files when Emacs become idle
   (when (featurep 'native-compile)
     (+eval-when-idle!
       (+info! "Trying to clean outdated native compile cache")
