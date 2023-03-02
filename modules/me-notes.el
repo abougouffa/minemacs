a4574e707abeaa9dc33a852e442c0d0b001339b9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-02 16:49:05 +0100

    clean(notes): remove deft

--- modules/me-notes.el
+++ modules/me-notes.el
@@ -39,37 +39,5 @@
    consult-org-roam-forward-links
    :preview-key (kbd "M-.")))
 
-;; From https://org-roam.discourse.group/t/configure-deft-title-stripping-to-hide-org-roam-template-headers/478/10
-(use-package deft
-  :straight t
-  :general
-  (+map
-    "nd" '(deft :wk "Deft"))
-  :init
-  (setq deft-default-extension "org")
-  :custom
-  (deft-directory org-roam-directory)
-  (deft-recursive t)
-  (deft-use-filter-string-for-filename t)
-  (deft-strip-summary-regexp
-   (rx (group
-        (or (any ?\n ?\t) ;; blanks
-            (seq bol "#+" (one-or-more (any alpha ?_)) ":" (zero-or-more not-newline) eol) ;; org-mode metadata
-            (seq bol ":PROPERTIES:" ?\n (one-or-more (group (one-or-more not-newline) ?\n)) ":END:" ?\n) ;; org-roam ID
-            (seq bol ":properties:" ?\n (one-or-more (group (one-or-more not-newline) ?\n)) ":end:" ?\n) ;; org-roam ID
-            (seq "[[" (group (zero-or-more not-newline) "]")))))) ;; any link
-  :config
-  (defun +deft-parse-title (file contents)
-    "Parse the given FILE and CONTENTS and determine the title.
-     If `deft-use-filename-as-title' is nil, the title is taken to
-     be the first non-empty line of the FILE.  Else the base name of the FILE is
-     used as title."
-    (let ((begin (string-match "^#\\+[tT][iI][tT][lL][eE]: .*$" contents)))
-      (if begin
-          (string-trim (substring contents begin (match-end 0)) "#\\+[tT][iI][tT][lL][eE]: *" "[\n\t ]+")
-        (deft-base-filename file))))
-
-  (advice-add 'deft-parse-title :override #'+deft-parse-title))
-
 
 (provide 'me-notes)
