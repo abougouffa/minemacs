d6d6b0a11132869a1803f9dcb8c1aedf9b16300d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-08 00:46:02 +0100

    doc(lazy): add documentation

--- core/minemacs-lazy.el
+++ core/minemacs-lazy.el
@@ -13,9 +13,14 @@
 
 ;; Run hooks
 (when minemacs-lazy-hook
+  ;; Reverse the order to follow the order in which modules are loaded. Make
+  ;; sure `gcmh-mode' is the last to be called. The `gc-cons-threshold' has been
+  ;; set in "early-init.el" to a ridiculously high value to reduce the number of
+  ;; garbage collections at startup, it will be overwritten by `gcmh-mode', so
+  ;; we defer loading it to the end to maximize the benefit.
   (setq minemacs-lazy-hook (append (delq 'gcmh-mode (reverse minemacs-lazy-hook)) '(gcmh-mode)))
   (if minemacs-not-lazy
-      (progn ;; If minemacs-no-lazy is bound and true, force loading lazy hooks immediately
+      (progn ; If `minemacs-no-lazy' is bound and true, force loading lazy hooks immediately
         (+log! "Loading %d lazy packages immediately."
                (length minemacs-lazy-hook))
         (run-hooks 'minemacs-lazy-hook))
