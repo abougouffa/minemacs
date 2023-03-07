6102d9b2f797de3d400bb1f2dd0756c7a1353d6f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-08 00:46:58 +0100

    tweak(gcmh): better defaults

--- core/me-gc.el
+++ core/me-gc.el
@@ -9,9 +9,14 @@
   :straight t
   :hook (minemacs-lazy . gcmh-mode)
   :custom
-  (gcmh-idle-delay 'auto)
-  (gcmh-auto-idle-delay-factor 10)
-  (gcmh-high-cons-threshold (* 16 1024 1024)))
+  ;; Set the delay to 20s instead of the default 15. I tried using `auto', but
+  ;; with the default 20 of `gcmh-auto-idle-delay-factor', it make trigger GC
+  ;; each 1s on my machine.
+  (gcmh-idle-delay 20)
+  ;; The default `gcmh's 1GB is probably too high. We set it to 256MB on 64bit
+  ;; systems, or 16MB on 32bit ones.
+  (gcmh-high-cons-threshold
+   (* 1024 1024 (if (memq sys/arch '(x86_64 amd64 aarch64)) 256 16))))
 
 
 (provide 'me-gc)
