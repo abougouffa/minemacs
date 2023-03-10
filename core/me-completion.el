0ac91dbd304e501641f803238db560f42950a51d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-10 11:37:34 +0100

    fix(vertico): revert to using the new `keymap-global-set`

--- core/me-completion.el
+++ core/me-completion.el
@@ -233,9 +233,7 @@
 (use-package vertico-repeat
   :hook (minibuffer-setup . vertico-repeat-save)
   :init
-  ;; TODO: Use `compat' to provide `keymap-global-set'
-  (when (>= emacs-major-version 29)
-    (keymap-global-set "M-R" #'vertico-repeat)))
+  (keymap-global-set "M-R" #'vertico-repeat))
 
 
 (provide 'me-completion)
