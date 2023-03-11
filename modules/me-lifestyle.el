51f78d197369567d0dc14a438db741a1e6b9269c
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-12 00:16:21 +0100

    doc(lifestyle): add a comment to `awqat`

--- modules/me-lifestyle.el
+++ modules/me-lifestyle.el
@@ -9,6 +9,11 @@
   :straight (:host github :repo "zkry/awqat")
   :commands awqat-times-for-day
   :custom
+  ;; In your "config.el", you need to set `calendar-latitude' and
+  ;; `calendar-longitude' (both defined in `solar'). Alongside with the
+  ;; calculation method, either by setting the right angles for Fajr and Isha,
+  ;; or by using one of the predefined presets (see `awqat' for more
+  ;; information).
   (awqat-mode-line-format " 🕌 ${prayer} (${hours}h${minutes}m) ")
   (awqat-update-interval 30.0))
 
