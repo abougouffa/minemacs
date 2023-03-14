9e6d6c2c63fee0cb3b3fb545bb2d67a7485e7625
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-15 00:18:55 +0100

    feat(tools): add docker-compose-mode

--- modules/me-tools.el
+++ modules/me-tools.el
@@ -69,6 +69,9 @@
   :init
   (+map! "ok" #'docker))
 
+(use-package docker-compose-mode
+  :straight t)
+
 (unless (+emacs-features-p 'tree-sitter)
   ;; Emacs 29 comes with `dockerfile-ts-mode'
   (use-package dockerfile-mode
