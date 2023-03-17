4de612cc07014229684e2b917b31a0c93ff6ac92
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-03-17 03:40:18 +0100

    fix(treesit-auto): temporary fix for upstream issue
    See https://github.com/renzmann/treesit-auto/issues/36

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -12,7 +12,9 @@
   (treesit-font-lock-level 4))
 
 (use-package treesit-auto
-  :straight (:host github :repo "renzmann/treesit-auto")
+  ;; TEMP: disable autoloads to fix the `treesit-auto-recipe-list' issue.
+  ;; github.com/renzmann/treesit-auto/issues/36
+  :straight (:host github :repo "renzmann/treesit-auto" :build (:not autoloads))
   :when (+emacs-features-p 'tree-sitter)
   :after treesit
   :demand t
