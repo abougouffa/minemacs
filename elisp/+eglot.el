51dd7b8f69548132ef9647cf915e0b9027170606
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-05-22 00:01:36 +0200

    tweak(eglot): do not assume servers to be executable

--- elisp/+eglot.el
+++ elisp/+eglot.el
@@ -16,15 +16,11 @@ Examples:
   (+eglot-register 'lua-mode \"lua-language-server\" \"lua-lsp\")
   (+eglot-register '(c-mode c++-mode) '(\"clangd\" \"--clang-tidy\" \"-j=12\") \"ccls\")"
   (declare (indent 0))
-  (let* ((alternatives-p (length> servers 1))
-         (first-server (car servers))
-         (first-server (if (listp first-server) (car first-server) first-server)))
-    (with-eval-after-load 'eglot
-      (when (executable-find first-server)
-        (add-to-list
-         'eglot-server-programs
-         (cons modes (if alternatives-p
-                         (eglot-alternatives (ensure-list servers))
-                       (ensure-list (car servers)))))))))
+  (with-eval-after-load 'eglot
+    (add-to-list
+     'eglot-server-programs
+     (cons modes (if (length> servers 1)
+                     (eglot-alternatives (ensure-list servers))
+                   (ensure-list (car servers)))))))
 
 ;;; +eglot.el ends here
