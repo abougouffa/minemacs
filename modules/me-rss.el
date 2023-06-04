8e0c5968962cbf63bd0234d60f08069d62a4f165
DarkBuffalo <65488203+DarkBuffalo@users.noreply.github.com>
2023-06-04 16:06:11 +0200

    tweak(elfeed): add tag selector & elfeed-org

--- modules/me-rss.el
+++ modules/me-rss.el
@@ -17,6 +17,7 @@
   :init
   (+map! "of" #'elfeed)
   (+nmap! :keymaps 'elfeed-search-mode-map
+    "<tab>" #'+elfeed-completing-filter
     "d" #'+elfeed-youtube-dl)
   (+nmap! :keymaps 'elfeed-show-mode-map
     "D" #'+elfeed-download-image)
@@ -29,6 +30,8 @@
   (defcustom +yt-dlp-command "yt-dlp"
     "The \"yt-dlp\" command."
     :group 'minemacs-tools)
+  (defvar +elfeed-file "elfeed.org"
+   "Name of org file.")
   :custom
   (elfeed-db-directory (concat minemacs-local-dir "elfeed/db/"))
   (elfeed-enclosure-default-dir (concat minemacs-local-dir "elfeed/enclosure/"))
@@ -60,7 +63,70 @@
                when (elfeed-entry-link entry)
                do (+yt-dl-it it))
       (mapc #'elfeed-search-update-entry entries)
-      (unless (use-region-p) (forward-line)))))
+      (unless (use-region-p) (forward-line))))
+
+  (defun +elfeed-filter-results-count (search-filter)
+   "Count results for SEARCH-FILTER."
+   (let* ((filter (elfeed-search-parse-filter search-filter))
+          (head (list nil))
+          (tail head)
+          (count 0))
+     (let ((lexical-binding t)
+           (func (byte-compile (elfeed-search-compile-filter filter))))
+       (with-elfeed-db-visit (entry feed)
+         (when (funcall func entry feed count)
+           (setf (cdr tail) (list entry)
+                 tail (cdr tail)
+                 count (1+ count)))))
+     count))
+
+  (defun +elfeed-view-filtered (filter)
+    "Filter the elfeed-search buffer to show feeds tagged with FILTER."
+    (interactive)
+    (elfeed)
+    (unwind-protect
+        (let ((elfeed-search-filter-active :live))
+          (setq elfeed-search-filter filter))
+      (elfeed-search-update :force)))
+
+  (defun +elfeed-get-tags ()
+   (let ((all-tags '()))
+     (with-temp-buffer (insert-file-contents (concat minemacs-config-dir +elfeed-file)
+                                             (delay-mode-hooks (org-mode)))
+        (org-map-entries
+         (lambda ()
+           (let ((tag-string (car (last (org-heading-components)))))
+             (when tag-string
+               (setq all-tags
+                     (append all-tags (split-string tag-string ":" t)))))))
+
+      ;; now get counts
+      (loop for tag in (seq-uniq all-tags)
+            collect  (cons tag (format "@6months-ago +unread +%s" tag))))))
+
+ (defun +elfeed-completing-filter ()
+   "Completing filter."
+   (interactive)
+   (let* ((tags (append '(("All" . "@6-months-ago +unread")) (+elfeed-get-tags) nil))
+          (categories (-filter
+                       (lambda (item)
+                         (> (+elfeed-filter-results-count (cdr item))
+                            0))
+                       tags)))
+
+     (if ( > (length categories) 0)
+         (progn
+           (+elfeed-view-filtered (cdr (assoc (completing-read "Categories: " categories)
+                                              categories)))
+           (goto-char (window-start)))
+       (message "All caught up \\o/")))))
+
+(use-package elfeed-org
+  :straight t
+  :after (elfeed)
+  :init
+  (setq rmh-elfeed-org-files (list (concat minemacs-config-dir +elfeed-file)))
+  (elfeed-org))
 
 
 (provide 'me-rss)
