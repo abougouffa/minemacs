6fa0fe3a931dfc0c231e24bc479d702d114f151a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-07 01:52:24 +0200

    refactor(flymake): small simplification

--- modules/me-checkers.el
+++ modules/me-checkers.el
@@ -14,6 +14,9 @@
   (+map! "tf" #'flymake-mode)
   :custom
   (flymake-fringe-indicator-position 'right-fringe)
+  (flymake-error-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-error))
+  (flymake-warning-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-warning))
+  (flymake-note-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-info))
   :config
   (+map-local! :keymaps 'flymake-mode-map
     "f"  '(nil :wk "flymake")
@@ -24,70 +27,49 @@
     "fp" #'flymake-show-project-diagnostics)
 
   ;; Use the session's load-path with flymake
-  (setq elisp-flymake-byte-compile-load-path load-path)
+  (setq elisp-flymake-byte-compile-load-path
+        (append elisp-flymake-byte-compile-load-path load-path))
+
   ;; Larger right frings
   (set-fringe-style '(8 . 13))
 
   ;; Better fringe bitmaps
-  (when (fboundp 'define-fringe-bitmap)
-    (define-fringe-bitmap '+flymake-bitmap-arrow
-      [#b11111000
-       #b01111100
-       #b00111110
-       #b00011111
-       #b00111110
-       #b01111100
-       #b11111000])
-
-    (define-fringe-bitmap '+flymake-bitmap-arrow-hi-res
-      [#b01111000000
-       #b00111100000
-       #b00011110000
-       #b00001111000
-       #b00000111100
-       #b00000011110
-       #b00000011110
-       #b00000111100
-       #b00001111000
-       #b00011110000
-       #b00111100000
-       #b01111000000]
-      nil 13)
-
-    (define-fringe-bitmap '+flymake-bitmap-left-arrow-hi-res
-      [#b00000011110
-       #b00000111100
-       #b00001111000
-       #b00011110000
-       #b00111100000
-       #b01111000000
-       #b01111000000
-       #b00111100000
-       #b00011110000
-       #b00001111000
-       #b00000111100
-       #b00000011110]
-      nil 13)
-
-    (setq flymake-error-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-error)
-          flymake-warning-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-warning)
-          flymake-note-bitmap '(+flymake-bitmap-left-arrow-hi-res compilation-info))))
-
-;;   (defhydra +flycheck-main (:color blue :hint nil :foreign-keys warn)
-;;     "
-;; [Flymake]                                              [_q_] quit
-;;   ├──────────────────────────────────────────────────────────────────────╮
-;;   │  [_B_] Buffer diagnostics  [_P_] Project diagnostics  [_L_] Log buffer     │
-;;   │  [_n_] Next error          [_N_] Prev error           [_S_] Start          │
-;;   ╰──────────────────────────────────────────────────────────────────────╯
-;; "
-;;     ("B" flymake-show-buffer-diagnostics)
-;;     ("P" flymake-show-project-diagnostics)
-;;     ("L" flymake-switch-to-log-buffer)
-;;     ("n" flymake-goto-next-error)
-;;     ("N" flymake-goto-prev-error)
-;;     ("S" flymake-start)
-;;     ("q" nil :color blue)))
+  (define-fringe-bitmap '+flymake-bitmap-arrow
+    [#b11111000
+     #b01111100
+     #b00111110
+     #b00011111
+     #b00111110
+     #b01111100
+     #b11111000])
+  (define-fringe-bitmap '+flymake-bitmap-arrow-hi-res
+    [#b01111000000
+     #b00111100000
+     #b00011110000
+     #b00001111000
+     #b00000111100
+     #b00000011110
+     #b00000011110
+     #b00000111100
+     #b00001111000
+     #b00011110000
+     #b00111100000
+     #b01111000000]
+    nil 13)
+  (define-fringe-bitmap '+flymake-bitmap-left-arrow-hi-res
+    [#b00000011110
+     #b00000111100
+     #b00001111000
+     #b00011110000
+     #b00111100000
+     #b01111000000
+     #b01111000000
+     #b00111100000
+     #b00011110000
+     #b00001111000
+     #b00000111100
+     #b00000011110]
+    nil 13))
 
 (use-package flymake-easy
   :straight t)
