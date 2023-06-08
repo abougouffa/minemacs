6d7ed85493762c2802dd251ae1f07cd982c8647d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-08 02:08:36 +0200

    nit(mu4e): minor formatting

--- modules/me-email.el
+++ modules/me-email.el
@@ -36,31 +36,30 @@
   (mu4e-search-results-limit 1000)
   (mu4e-index-cleanup t)
   (mu4e-attachment-dir "~/Downloads/mu4e-attachements/")
-  (mu4e-update-interval (* 1 60)) ;; Every 1 min
-  (mu4e-context-policy 'pick-first) ;; Start with the first context
-  (mu4e-compose-context-policy 'ask) ;; Always ask which context to use when composing a new mail
-  (mu4e-sent-messages-behavior 'sent) ;; Will be overwritten for Gmail accounts
-  (mu4e-get-mail-command "mbsync -a") ;; Use mbsync to get mails
-  (mu4e-index-update-error-warning nil) ;; Do not show warning after update
-  (mu4e-hide-index-messages t) ;; Hide minibuffer messages after indexing
+  (mu4e-update-interval (* 1 60)) ; Every 1 min
+  (mu4e-context-policy 'pick-first) ; Start with the first context
+  (mu4e-compose-context-policy 'ask) ; Always ask which context to use when composing a new mail
   (mu4e-compose-dont-reply-to-self t) ; When I reply to my own message, don't include me in the "To" field
+  (mu4e-sent-messages-behavior 'sent) ; Will be overwritten for Gmail accounts
+  (mu4e-get-mail-command "mbsync -a") ; Use mbsync to get mails
+  (mu4e-index-update-error-warning nil) ; Do not show warning after update
+  (mu4e-hide-index-messages t) ; Hide minibuffer messages after indexing
   (mu4e-change-filenames-when-moving t)
-  (mu4e-completing-read-function #'completing-read) ;; Use `vertico' instead of `ido'
-  (mu4e-main-hide-personal-addresses t) ;; Don't display a list of my own addresses!
-  (mu4e-modeline-support nil) ;; `mu4e-alert' is much nicer.
-  (mu4e-eldoc-support t)
-  (sendmail-program (executable-find "msmtp")) ;; Use msmtp to send mails
+  (mu4e-completing-read-function #'completing-read) ; Use `vertico' instead of `ido'
+  (mu4e-main-hide-personal-addresses t) ; Don't display a list of my own addresses!
+  (mu4e-modeline-support nil) ; `mu4e-alert' is much nicer.
+  (read-mail-command 'mu4e)
+  (message-kill-buffer-on-exit t) ; Close after sending
+  (message-mail-user-agent 'mu4e-user-agent)
+  (sendmail-program (executable-find "msmtp")) ; Use msmtp to send mails
   (send-mail-function #'smtpmail-send-it)
   (message-sendmail-f-is-evil t)
   (message-sendmail-extra-arguments '("--read-envelope-from"))
   (message-send-mail-function #'message-send-mail-with-sendmail)
   (message-sendmail-envelope-from 'obey-mail-envelope-from)
-  (message-mail-user-agent 'mu4e-user-agent)
-  (message-kill-buffer-on-exit t) ;; Close after sending
   (mail-envelope-from 'header)
   (mail-specify-envelope-from t)
   (mail-user-agent 'mu4e-user-agent)
-  (read-mail-command 'mu4e)
   :config
   (+nvmap! :keymaps 'mu4e-view-mode-map
     "p" #'mu4e-view-save-attachments)
