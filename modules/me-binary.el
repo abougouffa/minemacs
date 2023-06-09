761337c4bd860415767f63f26f6d6b8a13fe0bc0
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-09 02:20:35 +0200

    tweak(binary): better deferring

--- modules/me-binary.el
+++ modules/me-binary.el
@@ -9,12 +9,11 @@
 ;;; Code:
 
 ;; +binary-* are autoloaded
-(+deferred!
- ;; BUG: Loading continuously on `dirvish'.
- ;; BUG: Showing up randomly on `tramp' files.
- (setq +binary-objdump-enable nil)
- (+binary-setup-modes))
-
+;; BUG: Loading continuously on `dirvish'.
+;; BUG: Showing up randomly on `tramp' files.
+(setq +binary-objdump-enable nil)
+(add-to-list 'magic-fallback-mode-alist '(+binary-objdump-buffer-p . objdump-disassemble-mode) t)
+(add-to-list 'magic-fallback-mode-alist '(+binary-hexl-buffer-p . +binary-hexl-mode-maybe) t)
 
 (provide 'me-binary)
 
