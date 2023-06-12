67c8d2dca10b979281ce03101c23b0ba4d8aefc6
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-13 01:51:10 +0200

    feat(ibuffer-project): group buffers by projects in ibuffer

--- modules/me-project.el
+++ modules/me-project.el
@@ -55,6 +55,17 @@
     "p" #'consult-project-extra-find
     "P" #'consult-project-extra-find-other-window))
 
+(use-package ibuffer-project
+  :straight t
+  :hook (ibuffer . +ibuffer-project-h)
+  :config
+  ;; From Crafted Emacs
+  (defun +ibuffer-project-h ()
+    (setq ibuffer-filter-groups (ibuffer-project-generate-filter-groups))
+    (unless (eq ibuffer-sorting-mode 'project-file-relative)
+      (ibuffer-do-sort-by-project-file-relative))))
+
+
 (provide 'me-project)
 
 ;;; me-project.el ends here
