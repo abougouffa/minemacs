3bd1f761ec4e5a8dad3cade65845cbace9816790
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-18 01:00:18 +0200

    tweak(daemon): simplify a condition

--- modules/me-daemon.el
+++ modules/me-daemon.el
@@ -9,8 +9,8 @@
 ;;; Code:
 
 ;; Email (mu4e)
-(+lazy-when! (memq 'me-email minemacs-modules)
-  (when (and +mu4e-auto-start +mu4e-available-p (require 'mu4e nil t))
+(+lazy-when! (and (memq 'me-email minemacs-modules) +mu4e-available-p)
+  (when (and +mu4e-auto-start (require 'mu4e nil t))
     (unless (mu4e-running-p)
       (+info! "Starting `mu4e' in background.")
       (let ((inhibit-message t))
