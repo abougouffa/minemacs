9edfb5c5330037da7286ce07d06765e2d185358f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-23 20:16:23 +0200

    fix(eglot): add `rust-ts-mode` to auto enable modes

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -76,7 +76,7 @@
   (defcustom +eglot-auto-enable-modes
     '(c++-mode c++-ts-mode c-mode c-ts-mode
       python-mode python-ts-mode
-      rust-mode cmake-mode
+      rust-mode rust-ts-mode cmake-mode
       js-mode js-ts-mode typescript-mode typescript-ts-mode
       json-mode json-ts-mode js-json-mode)
     "Modes for which Eglot can be automatically enabled by `+eglot-auto-enable'."
