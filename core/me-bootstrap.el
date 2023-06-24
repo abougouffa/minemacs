7e1dc871624378e1cc2a43e6c155c4919cff5722
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-06-25 01:00:52 +0200

    tweak(bootstrap): set `use-package-verbose` accordingly

--- core/me-bootstrap.el
+++ core/me-bootstrap.el
@@ -44,7 +44,7 @@
 
 (setq
  ;; Set `use-package' to verbose when MinEmacs is started in verbose mode
- use-package-verbose minemacs-verbose
+ use-package-verbose (cond (minemacs-debug 'debug) (minemacs-verbose t))
  ;; Defer loading packages by default, use `:demand' to force loading a package
  use-package-always-defer t)
 
