7841b94692009558c8ed3fd3b3b86916bf183c32
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-07-09 01:30:48 +0200

    tweak(undo-fu): set `evil-undo-system` with `setopt`

--- modules/me-undo.el
+++ modules/me-undo.el
@@ -24,8 +24,7 @@
   :demand t
   :config
   (with-eval-after-load 'evil
-    (setq evil-undo-system 'undo-fu)
-    (evil-set-undo-system 'undo-fu)))
+    (setopt evil-undo-system 'undo-fu)))
 
 (use-package undo-fu-session
   :straight t
