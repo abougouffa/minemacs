d2de9a2dcf68aacb07344e3b7881ff598f73cc5b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-09-05 01:07:49 +0200

    nit: clean file header

--- modules/obsolete/me-cov.el
+++ modules/obsolete/me-cov.el
@@ -1,21 +1,11 @@
 ;;; me-cov.el --- Code coverage -*- lexical-binding: t; -*-
-;;
-;; Filename: me-cov.el
-;; Description:
+
+;; Copyright (C) 2022-2023  Abdelhak Bougouffa
+
 ;; Author: Abdelhak Bougouffa (concat "abougouffa" "@" "fedora" "project" "." "org")
-;; Created: Sun Jul  9 23:26:43 2023 (+0200)
-;; Version:
-;; Last-Updated:
-;;     Update #: 0
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
+
 ;;; Commentary:
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
+
 ;;; Code:
 
 (use-package cov
