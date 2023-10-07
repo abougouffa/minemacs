5ea190d9bd3a86a91c6e8989e4af96c349edbfb7
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-10-07 16:06:16 +0200

    tweak(ui): add keybinding for `+writing-global-mode`

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -65,7 +65,8 @@
 
 (use-package me-writing-mode
   :init
-  (+map! "tw" #'+writing-mode))
+  (+map! "tw" #'+writing-mode
+         "tW" #'+writing-global-mode))
 
 (use-package page-break-lines
   :straight t
