7d54316323794e588944f6c95f232a35b6413ed5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-10-07 13:17:25 +0200

    tweak(defaults): add pulse line

--- core/me-defaults.el
+++ core/me-defaults.el
@@ -216,6 +216,15 @@
  ;; Small tab is enough!
  tab-width 2)
 
+;; Add visual pulse when changing focus, like beacon but built-in
+;; From https://karthinks.com/software/batteries-included-with-emacs/
+(defun +pulse-line (&rest _)
+  "Pulse the current line."
+  (pulse-momentary-highlight-one-line (point)))
+
+(dolist (command '(scroll-up-command scroll-down-command recenter-top-bottom other-window))
+  (advice-add command :after #'+pulse-line))
+
 ;; ====== Misc hooks and advices ======
 ;; Kill the minibuffer when switching by mouse to another window.
 ;; Adapted from: trey-jackson.blogspot.com/2010/04/emacs-tip-36-abort-minibuffer-when.html
