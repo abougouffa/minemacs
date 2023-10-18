e3166079b9cded34ee59ab5d0981d12d8ae273e0
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-10-18 14:56:36 +0200

    nit: add an example for `non-executable-script.sh`

---
+++ assets/examples/non-executable-script.sh
@@ -0,0 +1,6 @@
+#!/bin/sed 4,5!d;s/^#.//
+# -*- sh-basic-offset: 2; tab-width: 2; -*-
+
+# This script should be sourced from bash or from another script. It is not
+# intented to be executed directly.
+
