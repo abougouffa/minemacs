a1829615a0ad7bd1ab445355d090bcc86b691f8a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-10-29 00:06:34 +0200

    tweak(eaf): use `:when` to condition `use-package`

--- modules/obsolete/me-eaf.el
+++ modules/obsolete/me-eaf.el
@@ -8,12 +8,9 @@
 
 ;;; Code:
 
-(when (+emacs-features-p 'lucid)
-  ;; EAF don't work on LUCID, a.k.a. XEmacs.
-  (add-to-list 'minemacs-disabled-packages 'eaf))
-
 (use-package eaf
   :straight (:host github :repo "emacs-eaf/emacs-application-framework" :files (:defaults "*"))
+  :when (+emacs-features-p 'lucid) ; EAF don't work on LUCID, a.k.a. XEmacs.
   :hook (minemacs-build-functions . eaf-install-and-update)
   :init
   (+map! "oo" #'eaf-open)
