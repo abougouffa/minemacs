9e154013a89a5df202fe79555f126508327256a3
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-10-30 22:19:16 +0100

    tweak(lexic): make lexic obsolete

--- modules/me-natural-langs.el
+++ modules/me-natural-langs.el
@@ -164,36 +164,6 @@
 (use-package reverso
   :straight (:host github :repo "SqrtMinusOne/reverso.el"))
 
-(use-package lexic
-  :straight t
-  :when (executable-find "sdcv")
-  :init
-  (+map! :infix "s"
-    "l" #'lexic-search-word-at-point
-    "L" #'lexic-search)
-  :config
-  (+nvmap! :keymaps 'lexic-mode-map
-    "q" #'lexic-return-from-lexic
-    "RET" #'lexic-search-word-at-point
-    "a" #'outline-show-all
-    "h" `(,(+cmdfy! (outline-hide-sublevels 3)) :wk "Hide sublevels")
-    "o" #'lexic-toggle-entry
-    "n" #'lexic-next-entry
-    "N" `(,(+cmdfy! (lexic-next-entry t)) :wk "Last entry")
-    "p" #'lexic-previous-entry
-    "P" `(,(+cmdfy! (lexic-previous-entry t)) :wk "First entry")
-    "E" `(,(+cmdfy!
-            (lexic-return-from-lexic)
-            (switch-to-buffer (lexic-get-buffer)))
-          :wk "Expand")
-    "M" `(,(+cmdfy!
-            (lexic-return-from-lexic)
-            (lexic-goto-lexic))
-          :wk "Minimise")
-    "C-p" #'lexic-search-history-backwards
-    "C-n" #'lexic-search-history-forwards
-    "/" `(,(+cmdfy! (call-interactively #'lexic-search)) :wk "Search")))
-
 ;; Add this to .dir-locals.el
 ;; ((nil (eglot-workspace-configuration
 ;;        . ((ltex . ((language . "fr")
---
+++ modules/obsolete/me-lexic.el
@@ -0,0 +1,43 @@
+;;; me-lexic.el --- Offline dictionary -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2023  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa (concat "abougouffa" "@" "fedora" "project" "." "org")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package lexic
+  :straight t
+  :when (executable-find "sdcv")
+  :init
+  (+map! :infix "s"
+    "l" #'lexic-search-word-at-point
+    "L" #'lexic-search)
+  :config
+  (+nvmap! :keymaps 'lexic-mode-map
+    "q" #'lexic-return-from-lexic
+    "RET" #'lexic-search-word-at-point
+    "a" #'outline-show-all
+    "h" `(,(+cmdfy! (outline-hide-sublevels 3)) :wk "Hide sublevels")
+    "o" #'lexic-toggle-entry
+    "n" #'lexic-next-entry
+    "N" `(,(+cmdfy! (lexic-next-entry t)) :wk "Last entry")
+    "p" #'lexic-previous-entry
+    "P" `(,(+cmdfy! (lexic-previous-entry t)) :wk "First entry")
+    "E" `(,(+cmdfy!
+            (lexic-return-from-lexic)
+            (switch-to-buffer (lexic-get-buffer)))
+          :wk "Expand")
+    "M" `(,(+cmdfy!
+            (lexic-return-from-lexic)
+            (lexic-goto-lexic))
+          :wk "Minimise")
+    "C-p" #'lexic-search-history-backwards
+    "C-n" #'lexic-search-history-forwards
+    "/" `(,(+cmdfy! (call-interactively #'lexic-search)) :wk "Search")))
+
+(provide 'obsolete/me-lexic)
+
+;;; me-lexic.el ends here
