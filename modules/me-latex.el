22fe929108e4fbfbe954bd6e03fc3502fe9d6410
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-01 23:12:00 +0100

    refactor(latex): move fontification tweaks to `me-latex`

--- modules/me-latex.el
+++ modules/me-latex.el
@@ -66,7 +66,92 @@
   ;; Electric pairs in auctex
   (customize-set-variable 'TeX-electric-sub-and-superscript t)
   (customize-set-variable 'LaTeX-electric-left-right-brace t)
-  (customize-set-variable 'TeX-electric-math (cons "$" "$")))
+  (customize-set-variable 'TeX-electric-math (cons "$" "$"))
+
+  ;; Fontification taken from: tex.stackexchange.com/a/86119/81279
+  (setq font-latex-match-reference-keywords
+        '(;; BibLaTeX.
+          ("printbibliography" "[{")
+          ("addbibresource" "[{")
+          ;; Standard commands.
+          ("cite" "[{")
+          ("citep" "[{")
+          ("citet" "[{")
+          ("Cite" "[{")
+          ("parencite" "[{")
+          ("Parencite" "[{")
+          ("footcite" "[{")
+          ("footcitetext" "[{")
+          ;; Style-specific commands.
+          ("textcite" "[{")
+          ("Textcite" "[{")
+          ("smartcite" "[{")
+          ("Smartcite" "[{")
+          ("cite*" "[{")
+          ("parencite*" "[{")
+          ("supercite" "[{")
+          ;; Qualified citation lists.
+          ("cites" "[{")
+          ("Cites" "[{")
+          ("parencites" "[{")
+          ("Parencites" "[{")
+          ("footcites" "[{")
+          ("footcitetexts" "[{")
+          ("smartcites" "[{")
+          ("Smartcites" "[{")
+          ("textcites" "[{")
+          ("Textcites" "[{")
+          ("supercites" "[{")
+          ;; Style-independent commands.
+          ("autocite" "[{")
+          ("Autocite" "[{")
+          ("autocite*" "[{")
+          ("Autocite*" "[{")
+          ("autocites" "[{")
+          ("Autocites" "[{")
+          ;; Text commands.
+          ("citeauthor" "[{")
+          ("Citeauthor" "[{")
+          ("citetitle" "[{")
+          ("citetitle*" "[{")
+          ("citeyear" "[{")
+          ("citedate" "[{")
+          ("citeurl" "[{")
+          ;; Special commands.
+          ("fullcite" "[{")
+          ;; Cleveref.
+          ("cref" "{")
+          ("Cref" "{")
+          ("cpageref" "{")
+          ("Cpageref" "{")
+          ("cpagerefrange" "{")
+          ("Cpagerefrange" "{")
+          ("crefrange" "{")
+          ("Crefrange" "{")
+          ("labelcref" "{"))
+        font-latex-match-textual-keywords
+        '(;; BibLaTeX brackets.
+          ("parentext" "{")
+          ("brackettext" "{")
+          ("hybridblockquote" "[{")
+          ;; Auxiliary commands.
+          ("textelp" "{")
+          ("textelp*" "{")
+          ("textins" "{")
+          ("textins*" "{")
+          ;; Subcaption.
+          ("subcaption" "[{"))
+        font-latex-match-variable-keywords
+        '(;; Amsmath.
+          ("numberwithin" "{")
+          ;; Enumitem.
+          ("setlist" "[{")
+          ("setlist*" "[{")
+          ("newlist" "{")
+          ("renewlist" "{")
+          ("setlistdepth" "{")
+          ("restartlist" "{")
+          ("crefname" "{"))))
 
 ;; Adapted from Doom Emacs
 (use-package auctex-latexmk
