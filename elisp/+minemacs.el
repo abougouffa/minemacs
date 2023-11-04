6fb7628e7db6e0e5e443ffa488df74fd7c5d5ad4
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-04 15:36:44 +0100

    tweak(core): more robust `minemacs-first-*-file-hook`

--- elisp/+minemacs.el
+++ elisp/+minemacs.el
@@ -208,8 +208,8 @@ DEPTH and LOCAL are passed as is to `add-hook'."
   "Make a hook which runs on the first FILETYPE file which with an extension
 that matches EXT-REGEXP.
 
-This will creates a function named `+first-file--FILETYPE-h' which and adds it
-to `first-file-hook', this function will run on the first file that matches
+This will creates a function named `+first-file--FILETYPE-h' which gets executed
+before `after-find-file'. This function will run on the first file that matches
 EXT-REGEXP. When it runs, this function provides a feature named
 `minemacs-first-FILETYPE-file' and a run all hooks in
 `minemacs-first-FILETYPE-file-hook'."
@@ -217,21 +217,26 @@ EXT-REGEXP. When it runs, this function provides a feature named
          (fn-name (intern (format "+first-file-%s-h" (if filetype (format "-%s" filetype) ""))))
          (hook-name (intern (format "minemacs-first%s-file-hook" (if filetype (format "-%s" filetype) ""))))
          (feature-name (intern (format "minemacs-first%s-file" (if filetype (format "-%s" filetype) ""))))
-         (hook-docs (format "This hook will be run after opening the first %s file.\n\nExecuted inside `find-file-hook', it runs all hooks in `%s' and provide the `%s' feature."
-                            filetype hook-name feature-name)))
+         (hook-docs (format "This hook will be run after opening the first %s file (files that matches \"%s\").\n\nExecuted before `after-find-file', it runs all hooks in `%s' and provide the `%s' feature."
+                            filetype ext-regexp hook-name feature-name)))
     `(progn
        (+log! "Setting up hook `%s' -- function `%s' -- feature `%s'."
         ',hook-name ',fn-name ',feature-name)
        (defcustom ,hook-name nil ,hook-docs :group 'minemacs-core :type 'hook)
-       (add-hook 'find-file-hook
-        (defun ,fn-name ()
-         (+log! "Hook `find-file-hook' invoked, checking for `%s'." ',hook-name)
-         (when (and buffer-file-name (string-match-p ,ext-regexp buffer-file-name))
-          (+log! "Running %d `%s' hooks." (length ,hook-name) ',hook-name)
-          (remove-hook 'find-file-hook #',fn-name)
-          (provide ',feature-name)
-          (run-hooks ',hook-name)))
-        -101))))
+       (defun ,fn-name (&rest _)
+        (when (and
+               after-init-time ; after Emacs initialization
+               (featurep 'minemacs-loaded) ; after MinEmacs is loaded
+               (buffer-file-name) ; for named files
+               (string-match-p ,ext-regexp (buffer-file-name))) ; file name matches the regexp
+         (+log! "Running %d `%s' hooks." (length ,hook-name) ',hook-name)
+         (advice-remove 'after-find-file #',fn-name)
+         (provide ',feature-name)
+         (run-hooks ',hook-name)))
+       (if (daemonp)
+           ;; Load immediately after init when in daemon mode
+           (add-hook 'after-init-hook #',fn-name 90)
+         (advice-add 'after-find-file :before #',fn-name '((depth . -101)))))))
 
 ;; From Doom Emacs
 (defun +resolve-hook-forms (hooks)
