609781a4bf63c5abf718ea6a6265cc84bbfe94b1
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-12 22:33:31 +0100

    feat(tools): make Chezmoi obsolete (migrated the simpler GNU Stow)

--- modules/me-tools.el
+++ modules/me-tools.el
@@ -132,33 +132,6 @@
            (if (functionp pass) (funcall pass) pass))
        ""))))
 
-(use-package chezmoi
-  :straight (chezmoi :files (:defaults "extensions/*.el"))
-  :when (executable-find "chezmoi")
-  :commands
-  chezmoi-find chezmoi-write chezmoi-diff chezmoi-open-other
-  chezmoi-sync-files chezmoi-magit-status
-  :init
-  (+map! :infix "o"
-    "c" '(nil :wk "chezmoi")
-    "cf" #'chezmoi-find
-    "cw" #'chezmoi-write
-    "cd" #'chezmoi-diff
-    "co" #'chezmoi-open-other
-    "cs" #'chezmoi-sync-files))
-
-(use-package chezmoi-ediff
-  :when (executable-find "chezmoi")
-  :commands chezmoi-ediff
-  :init
-  (+map! "oce" #'chezmoi-ediff))
-
-(use-package chezmoi-magit
-  :when (executable-find "chezmoi")
-  :commands chezmoi-magit-status
-  :init
-  (+map! "ocg" #'chezmoi-magit-status))
-
 (use-package with-editor
   :straight t
   :hook ((shell-mode eshell-mode term-exec vterm-mode) . with-editor-export-editor)
---
+++ modules/obsolete/me-chezmoi.el
@@ -0,0 +1,42 @@
+;;; me-chezmoi.el --- Dotfiles management with chezmoi -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2023  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa (concat "abougouffa" "@" "fedora" "project" "." "org")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package chezmoi
+  :straight (chezmoi :files (:defaults "extensions/*.el"))
+  :when (executable-find "chezmoi")
+  :commands
+  chezmoi-find chezmoi-write chezmoi-diff chezmoi-open-other
+  chezmoi-sync-files chezmoi-magit-status
+  :init
+  (+map! :infix "o"
+    "c" '(nil :wk "chezmoi")
+    "cf" #'chezmoi-find
+    "cw" #'chezmoi-write
+    "cd" #'chezmoi-diff
+    "co" #'chezmoi-open-other
+    "cs" #'chezmoi-sync-files))
+
+(use-package chezmoi-ediff
+  :when (executable-find "chezmoi")
+  :commands chezmoi-ediff
+  :init
+  (+map! "oce" #'chezmoi-ediff))
+
+(use-package chezmoi-magit
+  :when (executable-find "chezmoi")
+  :commands chezmoi-magit-status
+  :init
+  (+map! "ocg" #'chezmoi-magit-status))
+
+
+(provide 'obsolete/me-chezmoi)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; me-chezmoi.el ends here
