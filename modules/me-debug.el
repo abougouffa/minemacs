9b7389184bd0bf8ce70f6f731bb39e9832636688
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-17 18:49:08 +0100

    tweak(dape): initial configuration for adapters

--- modules/me-debug.el
+++ modules/me-debug.el
@@ -10,8 +10,48 @@
 
 (use-package dape
   :straight (:host github :repo "svaante/dape")
+  :init
+  (defvar +dape-cpptools-command
+    (concat minemacs-local-dir "dape/microsoft.vscode-cpptools/extension/bin/cpptools"))
+  (defvar +dape-codelldb-command
+    (concat minemacs-local-dir "dape/vadimcn.codelldb/extension/adapter/codelldb"))
   :custom
-  (dape-inline-variables t))
+  (dape-inline-variables t)
+  (dape-configs `((debugpy
+                   modes (python-ts-mode python-mode)
+                   command "python3"
+                   command-args ("-m" "debugpy.adapter")
+                   :type "executable"
+                   :request "launch"
+                   :cwd dape-cwd-fn
+                   :program dape-find-file-buffer-default)
+                  (godot-launch
+                   modes (gdscript-mode)
+                   host "127.0.0.1"
+                   port 6006
+                   :type "server"
+                   :request "launch")
+                  (cpptools
+                   modes (c-mode c-ts-mode c++-mode c++-ts-mode)
+                   command-cwd ,(file-name-directory +dape-cpptools-command)
+                   command +dape-cpptools-command
+                   :type "cpptools"
+                   :request "launch"
+                   :cwd dape-cwd-fn
+                   :program dape-find-file
+                   :MIMode ,(cond
+                             ((executable-find "gdb") "gdb")
+                             ((executable-find "lldb") "lldb")))
+                  (codelldb
+                   modes (c-mode c-ts-mode c++-mode c++-ts-mode rust-mode rust-ts-mode)
+                   command ,+dape-codelldb-command
+                   host "localhost"
+                   port 5818
+                   command-args ("--port" "5818")
+                   :type "lldb"
+                   :request "launch"
+                   :cwd dape-cwd-fn
+                   :program dape-find-file))))
 
 (use-package realgud
   :straight (realgud :build (:not compile))
