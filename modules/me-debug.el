664c17ba73f23de96ddd4553b2a03c76dc64ac9a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-17 21:16:21 +0100

    tweak(dape): additional adapters for GO and JS

--- modules/me-debug.el
+++ modules/me-debug.el
@@ -12,12 +12,14 @@
   :straight (:host github :repo "svaante/dape")
   :init
   (defvar +dape-cpptools-command
-    (concat minemacs-local-dir "dape/microsoft.vscode-cpptools/extension/bin/cpptools"))
+    (concat minemacs-local-dir "dape/cpptools/extension/bin/cpptools"))
+  (defvar +dape-js-debug-path
+    (concat minemacs-local-dir "dape/js-debug/extension/"))
   (defvar +dape-codelldb-command
-    (concat minemacs-local-dir "dape/vadimcn.codelldb/extension/adapter/codelldb"))
+    (concat minemacs-local-dir "dape/codelldb/extension/adapter/codelldb"))
   :custom
   (dape-inline-variables t)
-  (dape-configs `((debugpy
+  (dape-configs `((debugpy ; https://github.com/microsoft/debugpy
                    modes (python-ts-mode python-mode)
                    command "python3"
                    command-args ("-m" "debugpy.adapter")
@@ -25,13 +27,7 @@
                    :request "launch"
                    :cwd dape-cwd-fn
                    :program dape-find-file-buffer-default)
-                  (godot-launch
-                   modes (gdscript-mode)
-                   host "127.0.0.1"
-                   port 6006
-                   :type "server"
-                   :request "launch")
-                  (cpptools
+                  (cpptools ; https://github.com/microsoft/vscode-cpptools
                    modes (c-mode c-ts-mode c++-mode c++-ts-mode)
                    command-cwd ,(file-name-directory +dape-cpptools-command)
                    command +dape-cpptools-command
@@ -42,7 +38,7 @@
                    :MIMode ,(cond
                              ((executable-find "gdb") "gdb")
                              ((executable-find "lldb") "lldb")))
-                  (codelldb
+                  (codelldb ; https://github.com/vadimcn/codelldb
                    modes (c-mode c-ts-mode c++-mode c++-ts-mode rust-mode rust-ts-mode)
                    command ,+dape-codelldb-command
                    host "localhost"
@@ -51,7 +47,36 @@
                    :type "lldb"
                    :request "launch"
                    :cwd dape-cwd-fn
-                   :program dape-find-file))))
+                   :program dape-find-file)
+                  (delve ; https://github.com/go-delve/delve
+                   modes (go-mode go-ts-mode)
+                   command "dlv"
+                   command-args ("dap" "--listen" "127.0.0.1:55878")
+                   command-cwd dape-cwd-fn
+                   host "127.0.0.1"
+                   port 55878
+                   :type "debug"
+                   :request "launch"
+                   :cwd dape-cwd-fn
+                   :program dape-cwd-fn)
+                  (js-debug ; https://github.com/microsoft/vscode-js-debug
+                   modes (js-mode js-ts-mode)
+                   host "localhost"
+                   port 8123
+                   command "node"
+                   command-cwd ,+dape-js-debug-path
+                   command-args ("src/dapDebugServer.js" "8123")
+                   :type "pwa-node"
+                   :request "launch"
+                   :cwd dape-cwd-fn
+                   :program dape-find-file-buffer-default
+                   :outputCapture "console"
+                   :sourceMapRenames t
+                   :pauseForSourceMap nil
+                   :enableContentValidation t
+                   :autoAttachChildProcesses t
+                   :console "internalConsole"
+                   :killBehavior "forceful"))))
 
 (use-package realgud
   :straight (realgud :build (:not compile))
