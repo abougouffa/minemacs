cf838c76cbbe6c83b3ba7ebdac825de8a89d1e65
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-18 23:25:57 +0100

    feat(checkers): add `flymake-ruff`

--- modules/me-checkers.el
+++ modules/me-checkers.el
@@ -12,6 +12,10 @@
   :straight t
   :autoload flymake-easy-load)
 
+(use-package flymake-ruff
+  :straight t
+  :hook ((python-mode python-ts-mode) . flymake-ruff-load))
+
 (use-package flymake-collection
   :straight t
   :hook (minemacs-after-startup . flymake-collection-hook-setup))
