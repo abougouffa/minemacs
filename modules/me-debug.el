28dea66c4b1227b939e9e9490667cca55cb101b5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-21 00:44:39 +0100

    tweak(dape): add keybindings

--- modules/me-debug.el
+++ modules/me-debug.el
@@ -10,9 +10,47 @@
 
 (use-package dape
   :straight (:host github :repo "svaante/dape")
+  :commands +dape-transient
   :custom
   (dape-inline-variables t)
-  (dape-adapter-dir (concat minemacs-local-dir "dape/")))
+  (dape-adapter-dir (concat minemacs-local-dir "dape/"))
+  :init
+  (+map! :infix "d"
+    "d" #'dape
+    "n" #'dape-next
+    "s" #'dape-step-in
+    "o" #'dape-step-out
+    "c" #'dape-continue
+    "r" #'dape-restart
+    "p" #'dape-pause
+    "b" #'dape-toggle-breakpoint
+    "r" #'dape-remove-breakpoint-at-point
+    "R" #'dape-remove-all-breakpoints
+    "t" #'+dape-transient
+    "q" #'dape-kill
+    "Q" #'dape-quit)
+  :config
+  (transient-define-prefix +dape-transient ()
+    "Transient for dape."
+    [["Stepping"
+      ("n"  "Next" dape-next :transient t)
+      ("s"  "Step in" dape-step-in :transient t)
+      ("o"  "Step out" dape-step-out :transient t)
+      ("c"  "Continue" dape-continue :transient t)
+      ("r"  "Restart" dape-restart :transient t)]
+     ["Breakpoints"
+      ("bb" "Toggle" dape-toggle-breakpoint :transient t)
+      ("bd" "Remove at pt" dape-remove-breakpoint-at-point :transient t)
+      ("bD" "Remove all" dape-remove-all-breakpoints :transient t)
+      ("bl" "Log" dape-log-breakpoint :transient t)]
+     ["Info"
+      ("ii" "Info" dape-info :transient t)
+      ("im" "Memory" dape-read-memory :transient t)
+      ("is" "Select Stack" dape-select-stack :transient t)
+      ("R"  "Repl" dape-repl :transient t)]
+     ["Quit"
+      ("qq" "Quit" dape-quit :transient nil)
+      ("qk" "Kill" dape-kill :transient nil)]]))
 
 (use-package realgud
   :straight (realgud :build (:not compile))
