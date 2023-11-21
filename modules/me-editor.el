64a360172eec553c53f8ce168acf0e49531b51ad
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-21 19:59:08 +0100

    tweak(editor): make `smartparens` obsolete

--- modules/me-editor.el
+++ modules/me-editor.el
@@ -41,28 +41,6 @@
   :config
   (setq highlight-numbers-generic-regexp (rx (and symbol-start (one-or-more digit)) (optional "." (* digit)) symbol-end)))
 
-(use-package smartparens
-  :straight t
-  :hook (minemacs-after-startup . smartparens-global-mode)
-  :init
-  ;; From Doom Emacs, disable expensive navigation features.
-  (+setq-hook! smartparens-mode
-    sp-navigate-skip-match nil
-    sp-navigate-consider-sgml-tags nil)
-  :config
-  (sp-local-pair 'org-mode "$" "$" :unless '(sp-point-after-word-p))
-  (with-eval-after-load 'evil-mc
-    ;; Make evil-mc cooperate with smartparens better
-    (let ((vars (cdr (assq :default evil-mc-cursor-variables))))
-      (unless (memq (car sp--mc/cursor-specific-vars) vars)
-        (setcdr (assq :default evil-mc-cursor-variables) (append vars sp--mc/cursor-specific-vars))))))
-
-;; Default `smartparens' configuration (for example, do not complete a single
-;; quote)
-(use-package smartparens-config
-  :after smartparens
-  :demand t)
-
 (use-package expreg
   :straight (:host github :repo "casouri/expreg")
   :when (+emacs-features-p 'tree-sitter)
