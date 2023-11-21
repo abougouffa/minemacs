ed68c644bccec956f906acf5c8fff0e07bd3eb8d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-21 18:37:49 +0100

    feat(ui): add `solaire-mode`

--- core/me-core-ui.el
+++ core/me-core-ui.el
@@ -8,19 +8,6 @@
 
 ;;; Code:
 
-;; Apply tweaks
-(+add-hook! '(after-init-hook enable-theme-functions)
-  (defun +theme--tweaks-h (&optional _)
-    "Use smaller font (75% of the default) for line numbers in graphic mode."
-    (when (display-graphic-p)
-      (set-face-attribute 'line-number nil
-                          :background (face-attribute 'default :background)
-                          :height (truncate (* 0.75 (face-attribute 'default :height)))
-                          :weight 'semi-light)
-      (set-face-attribute 'line-number-current-line nil
-                          :height (truncate (* 0.75 (face-attribute 'default :height)))
-                          :weight 'bold))))
-
 ;; Disable previously enabled custom themes before enabling a new one.
 (advice-add
  'load-theme :before
@@ -88,6 +75,13 @@
     '(compilation objed-state misc-info persp-name battery grip irc mu4e gnus
       github debug repl lsp minor-modes input-method indent-info buffer-encoding
       major-mode process vcs checker time "  ")))
+
+(use-package solaire-mode
+  :straight t
+  :hook (minemacs-after-startup . solaire-global-mode)
+  :config
+  (dolist (face '(mode-line mode-line-active mode-line-inactive mode-line-emphasis))
+    (setf (alist-get face solaire-mode-remap-alist) nil)))
 
 (use-package lin
   :straight t
