25b98ebc1eb058ae51a12225f45f4f0c7788d4ec
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-11-26 23:50:30 +0100

    feat(tools): add `nix-ts-mode` and tweak Eglot servers to run for it

--- modules/me-tools.el
+++ modules/me-tools.el
@@ -145,6 +145,16 @@
 (use-package nix-mode
   :straight t)
 
+(use-package nix-ts-mode
+  :straight t
+  :when (+emacs-features-p 'tree-sitter)
+  :mode "\\.nix\\'"
+  :config
+  ;; Register Eglot servers on the `nix-ts-mode' in addition to the already configured `nix-mode'
+  (with-eval-after-load 'eglot
+    (when-let ((server (assoc 'nix-mode eglot-server-programs)))
+      (setcar server '(nix-mode nix-ts-mode)))))
+
 (use-package nix-update
   :straight t)
 
