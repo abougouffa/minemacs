2f3b9e8f1db60e117cfa01f5d2a420770ed1be27
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-12-08 17:25:59 +0100

    feat(ui): add `anzu` to show number of matches in modeline

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -69,6 +69,24 @@
                     evil-delete evil-delete-line evil-delete-whole-line
                     evil-goto-last-change evil-goto-last-change-reverse)))))
 
+;; From Doom Emacs
+(use-package anzu
+  :straight t
+  :custom
+  (anzu-cons-mode-line-p nil) ; We manage our own modeline segments
+  :config
+  ;; Ensure anzu state is cleared when searches & iedit are done
+  (add-hook 'iedit-mode-end-hook #'anzu--reset-status)
+  (advice-add #'evil-force-normal-state :before #'anzu--reset-status)
+  ;; Fix matches segment mirroring across all buffers
+  (mapc #'make-variable-buffer-local
+        '(anzu--total-matched anzu--current-position anzu--state anzu--cached-count
+          anzu--cached-positions anzu--last-command anzu--last-isearch-string anzu--overflow-p)))
+
+(use-package evil-anzu
+  :straight t
+  :hook (evil-mode . global-anzu-mode))
+
 
 (provide 'me-ui)
 
