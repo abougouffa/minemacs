8456f69e23c032b3232db686063c24b487d9ac75
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-12-08 17:44:30 +0100

    chore(ci): automatically detected all available modules

--- .github/workflows/scripts/minemacs-all.d/modules.el
+++ .github/workflows/scripts/minemacs-all.d/modules.el
@@ -8,12 +8,5 @@
 
 ;;; Code:
 
-(setq minemacs-core-modules
-      '(me-splash me-keybindings me-evil me-core-ui me-completion)
-      minemacs-modules
-      '(me-ui me-editor me-daemon me-undo me-multi-cursors me-vc me-project
-        me-prog me-checkers me-debug me-lsp me-emacs-lisp me-common-lisp
-        me-scheme me-clojure me-embedded me-robot me-data me-math me-modeling
-        me-org me-extra me-notes me-email me-rss me-lifestyle me-docs me-calendar
-        me-latex me-biblio me-natural-langs me-files me-tools me-tty me-fun
-        me-media me-workspaces me-binary me-window))
+(setq minemacs-core-modules '(me-splash me-keybindings me-evil me-core-ui me-completion)
+      minemacs-modules (mapcar #'intern (mapcar #'file-name-sans-extension (directory-files minemacs-modules-dir nil "\\.el\\'"))))
