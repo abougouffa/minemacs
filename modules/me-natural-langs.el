f09dd8583cd211e8fb8d60830430afd0535a9d6c
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2023-12-29 13:15:39 +0100

    tweak(jinx): hook if the compilation is easy (Unix or Win+MSYS) [#147]

--- modules/me-natural-langs.el
+++ modules/me-natural-langs.el
@@ -10,14 +10,17 @@
 
 (use-package jinx
   :straight t
-  :when (and (or os/linux os/bsd os/mac) (+emacs-features-p 'modules))
-  :hook (text-mode . jinx-mode)
-  :hook (minemacs-build-functions . jinx--load-module)
+  :when (+emacs-features-p 'modules)
   :init
   (+map! "ts" #'jinx-mode)
-  (+nvmap! "z=" #'jinx-correct))
+  (+nvmap! "z=" #'jinx-correct)
+  ;; Module compilation with libenchant should be easy on Linux, BSD and Mac OS,
+  ;; and on Windows when the installation is done using MSYS2. When Emacs flavor
+  ;; is MinGW, do not setup the hook by default.
+  (when (or os/linux os/bsd os/mac (and os/win (string-suffix-p "msys" system-configuration)))
+   (add-hook 'text-mode-hook #'jinx-mode)))
 
-(unless (or os/win (+emacs-features-p 'modules))
+(unless (+emacs-features-p 'modules)
   (+load minemacs-modules-dir "obsolete/me-spell-fu.el"))
 
 (use-package go-translate
