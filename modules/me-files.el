d35f55c3bb8929ca9e388715648652b2e6be2312
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-01-23 10:39:43 +0100

    tweak(treemacs): smaller font size for root nodes

--- modules/me-files.el
+++ modules/me-files.el
@@ -49,7 +49,10 @@
   :custom
   (treemacs-persist-file (concat minemacs-local-dir "treemacs/persist.el"))
   (treemacs-last-error-persist-file (concat minemacs-local-dir "treemacs/last-error-persist.el"))
-  (treemacs-width 30))
+  (treemacs-width 30)
+  :config
+  ;; Use the same height for the root node (project directory)
+  (set-face-attribute 'treemacs-root-face nil :height 1.0))
 
 (use-package treemacs-nerd-icons
   :straight t
