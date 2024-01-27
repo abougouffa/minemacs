8a876f52b0504a698f0508fbbf206f43192f4a42
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-01-27 23:10:03 +0100

    feat(prog): add `eopengrok` support

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -102,6 +102,18 @@ Fall back to the default `citre--project-root'."
   :straight t
   :commands cscope-create-list-of-files-to-index cscope-index-files)
 
+(use-package eopengrok
+  :straight t
+  :commands
+  eopengrok-mode eopengrok-find-reference eopengrok-find-text eopengrok-find-definition eopengrok-find-custom
+  eopengrok-jump-to-source eopengrok-create-index eopengrok-create-index-with-enable-projects
+  :config
+  (+nmap! :keymaps 'eopengrok-mode-map
+    "n" #'eopengrok-next-line
+    "p" #'eopengrok-previous-line
+    "q" #'eopengrok-quit
+    "RET" #'eopengrok-jump-to-source))
+
 (use-package srefactor
   :straight t)
 
