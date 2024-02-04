0a6f71d848de19033c108203da0b430b69cbb63e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-02-04 16:53:11 +0100

    tweak(ellama): auto load installed models

--- modules/me-ai.el
+++ modules/me-ai.el
@@ -25,7 +25,8 @@
             (message "The Ollama server is already running, call `+ellama-kill-server' to stop it.")
           (if (make-process :name +ellama-process-name :buffer +ellama-server-buffer-name :command '("ollama" "serve"))
               (message "Successfully started Ollama server.")
-            (user-error "Cannot start the Ollama server")))
+            (user-error "Cannot start the Ollama server"))
+          (with-eval-after-load 'ellama (+ellama-set-providers)))
       (user-error "Cannot find the \"ollama\" executable")))
   (defun +ellama-kill-server ()
     "Kill Ellama server."
@@ -36,8 +37,19 @@
               (message "Killed Ollama server.")
             (user-error "Cannot kill the Ollama server"))
         (message "No running Ollama server."))))
-  :custom
-  (ellama-provider (make-llm-ollama :chat-model "llama2:13b" :embedding-model "llama2:13b")))
+  :config
+  (defun +ellama-list-installed-models ()
+    "Return the installed models"
+    (let* ((ret (shell-command-to-string "ollama list"))
+           (models (cdr (string-lines ret))))
+      (if (and (string-match-p "NAME[[:space:]]*ID[[:space:]]*SIZE[[:space:]]*MODIFIED" ret) (length> models 0))
+          (mapcar (lambda (m) (car (string-split m))) models)
+        (user-error "Cannot detect installed models, please make sure Ollama server is started"))))
+  (defun +ellama-set-providers ()
+    (setopt ellama-providers
+            (cl-loop for model in (+ellama-list-installed-models)
+                     collect (cons model (make-llm-ollama :chat-model model :embedding-model model)))
+            ellama-provider (cdr (car ellama-providers)))))
 
 
 (provide 'me-ai)
