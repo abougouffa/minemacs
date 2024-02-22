c3630f7cb30a68c8f7cb2d14e8fce8fa72e57079
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-02-23 00:03:58 +0100

    feat(project): make `project-cmake` obsolete

--- modules/me-project.el
+++ modules/me-project.el
@@ -25,19 +25,6 @@
     (unless (eq ibuffer-sorting-mode 'project-file-relative)
       (ibuffer-do-sort-by-project-file-relative))))
 
-(use-package project-cmake
-  :straight (:host github :repo "juanjosegarciaripoll/project-cmake")
-  :init
-  (+map! :keymaps '(c-mode-map c++-mode-map c-ts-mode-map c++-ts-mode-map)
-    :infix "p"
-    "m" '(nil :wk "project-cmake")
-    "mb" #'project-cmake-build
-    "mg" #'project-cmake-configure
-    "mt" #'project-cmake-test
-    "mI" #'project-cmake-install
-    "ms" #'project-cmake-scan-kits
-    "mS" #'project-cmake-shell))
-
 
 (provide 'me-project)
 
---
+++ modules/obsolete/me-project-cmake.el
@@ -0,0 +1,27 @@
+;;; me-project-cmake.el --- CMake integration for built-in project.el (replaced with projection) -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package project-cmake
+  :straight (:host github :repo "juanjosegarciaripoll/project-cmake")
+  :init
+  (+map! :keymaps '(c-mode-map c++-mode-map c-ts-mode-map c++-ts-mode-map)
+    :infix "p"
+    "m" '(nil :wk "project-cmake")
+    "mb" #'project-cmake-build
+    "mg" #'project-cmake-configure
+    "mt" #'project-cmake-test
+    "mI" #'project-cmake-install
+    "ms" #'project-cmake-scan-kits
+    "mS" #'project-cmake-shell))
+
+
+(provide 'obsolete/me-project-cmake)
+
+;;; me-project-cmake.el ends here
