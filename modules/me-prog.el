6905f2394fd076123760b7b3689b0d7b18e1957b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-03-07 12:23:22 +0100

    tweak(treesitter-context): suppress `treesitter-context-fold-mode`

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -429,6 +429,8 @@ Fall back to the default `citre--project-root'."
 (use-package treesitter-context-fold
   :hook (prog-mode . treesitter-context-fold-mode)
   :when (+emacs-features-p 'tree-sitter)
+  :init
+  (+fn-inhibit-messages! treesitter-context-fold-mode)
   :config
   (require 'treesitter-context))
 
