85025004f1965ee1b0c62dd6a0553b94eb56d6d3
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-05-17 19:56:08 +0200

    refactor(consult-eglot): rewrite a condition

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -228,7 +228,7 @@ Fall back to the default `citre--project-root'."
   :config
   ;; Provide `consult-lsp' functionality from `consult-eglot', useful for
   ;; packages that relays on `consult-lsp' (like `dirvish-subtree').
-  (unless (or (not (+package-disabled-p 'lsp-mode 'me-lsp)) (fboundp 'consult-lsp-file-symbols))
+  (unless (or (not (+package-disabled-p 'lsp-mode 'obsolete/me-lsp)) (fboundp 'consult-lsp-file-symbols))
     (defalias 'consult-lsp-file-symbols #'consult-eglot-symbols)))
 
 (use-package eldoc-box
