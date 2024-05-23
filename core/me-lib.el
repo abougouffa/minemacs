e5de3945aae6e18b90254b76c4cb6fac252c2bf7
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-05-23 15:27:10 +0200

    refactor(core): minor edit

--- core/me-lib.el
+++ core/me-lib.el
@@ -596,7 +596,7 @@ Optionally, check also for the containing MODULE."
 Ensure the path exists, if not create it. The exact behavior is to create the
 parent directory if the path is a file, and if the path is a directory, create
 that directory."
-  (let* ((path (mapconcat #'identity path-parts nil))
+  (let* ((path (apply #'concat path-parts))
          (parent-dir (file-name-directory path)))
     (unless (file-directory-p parent-dir)
       (ignore-errors (mkdir parent-dir t))
