5f4ea3fd7fd558461bc4f40e83762112b7583da0
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-05-23 11:28:38 +0200

    feat(gtd): empty module for Getting Things Done workflow

---
+++ modules/me-gtd.el
@@ -0,0 +1,17 @@
+;;; me-gtd.el --- Getting Things Done (GTD) workflow for Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+;; Mainly from: https://github.com/jjuliano/practical.org.el
+
+
+
+(provide 'me-gtd)
+
+;;; me-gtd.el ends here
