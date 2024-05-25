e8e9d507c59343a85e6c068ae60a28470ca71a6e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-05-26 00:31:59 +0200

    feat(workspaces): add support for `project-tab-groups`

--- modules/me-workspaces.el
+++ modules/me-workspaces.el
@@ -13,6 +13,35 @@
 
 ;;; Code:
 
+(use-package project-tab-groups
+  :straight t
+  :after project
+  :preface
+  (+map! :infix "TAB"
+    "TAB" #'tab-bar-switch-to-tab
+    "o" #'project-switch-project
+    "d" #'tab-bar-close-tab)
+  :custom
+  (project-tab-groups-tab-group-name-function #'+project-tab-groups-name-by-project-root)
+  :init
+  (project-tab-groups-mode 1)
+  :config
+  (defun +project-tab-groups-name-by-project-root (dir)
+    "Derive tab group name for project in DIR."
+    (with-temp-buffer
+      (setq default-directory dir)
+      (hack-dir-local-variables-non-file-buffer)
+      (let ((name (or (and (boundp 'tab-group-name) tab-group-name)
+                      (and (boundp 'project-name) project-name)
+                      (and (fboundp 'project-root)
+                           (when-let ((project-current (project-current)))
+                             (project-root project-current)))
+                      (file-name-nondirectory (directory-file-name dir))))
+            (name-template (or (and (boundp 'tab-group-name-template) tab-group-name-template)
+                               (and (boundp 'project-name-template) project-name-template)
+                               "%s")))
+        (format name-template name)))))
+
 
 (provide 'me-workspaces)
 
