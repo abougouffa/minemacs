85902186a1d9e8983cf53a31730561e5d05a74ea
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-05-27 23:25:05 +0200

    feat(services)!: add a new module for services, move `jiralib2` and `org-jira`

--- modules/me-project.el
+++ modules/me-project.el
@@ -70,42 +70,6 @@
   :init
   (project-x-mode 1))
 
-(use-package jiralib2
-  :straight t
-  :commands +jira-insert-ticket-id +jira-insert-ticket-link
-  :init
-  (defvar-local +jira-open-status '("open" "to do" "in progress"))
-  :config
-  (defun +jira--ticket-annotation-fn (ticket)
-    (let ((item (assoc ticket minibuffer-completion-table)))
-      (when item (concat "    " (cdr item)))))
-
-  (defun +jira-insert-ticket-id ()
-    "Insert ticket ID, choose from states defined in `+jira-open-status'."
-    (interactive)
-    (when-let* ((issues (jiralib2-jql-search (format "assignee=\"%s\" AND status in (%s)"
-                                                     jiralib2-user-login-name
-                                                     (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
-                (tickets (mapcar (lambda (t) (cons (cdr (assoc 'key t)) (cdr (assoc 'summary (cdr (assoc 'fields t)))))) issues)))
-      (insert
-       (if (length= tickets 1)
-           (car (car tickets))
-         (let ((completion-extra-properties '(:annotation-function +jira--ticket-annotation-fn)))
-           (completing-read "Select ticket: " tickets))))))
-
-  (defun +jira-insert-ticket-link ()
-    "Insert ticket link, choose from states defined in `+jira-open-status'."
-    (interactive)
-    (let* ((id (with-temp-buffer (+jira-insert-ticket-id) (buffer-string)))
-           (link (format "%s/browse/%s" jiralib2-url id)))
-      (insert
-       (cond ((derived-mode-p 'org-mode) (format "[[%s][%s]]" link id))
-             ((derived-mode-p 'markdown-mode 'git-commit-mode) (format "[%s](%s)" id link))
-             (t link))))))
-
-(use-package org-jira
-  :straight (:host github :repo "ahungry/org-jira"))
-
 
 (provide 'me-project)
 
