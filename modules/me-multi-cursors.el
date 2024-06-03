c3c85651ff82dbdcf41b8187ce8fc04687a92345
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-03 17:16:14 +0200

    feat(multi-cursors): add initial support for `multiple-cursors`

--- modules/me-multi-cursors.el
+++ modules/me-multi-cursors.el
@@ -12,6 +12,14 @@
   :straight t
   :bind ("C-;" . iedit-mode))
 
+(use-package multiple-cursors
+  :straight t
+  :config
+  (cl-callf append mc--default-cmds-to-run-for-all
+    '(meow-insert meow-append meow-backward-delete
+      meow-delete meow-replace meow-replace-char
+      meow-yank meow-yank-pop meow-kill)))
+
 
 (provide 'me-multi-cursors)
 
