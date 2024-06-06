c6905b53dbb428531d554c538d07d2d4580559c9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-06 09:15:47 +0200

    feat(editor): make `logos` obsolete

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -98,31 +98,6 @@
 (use-package olivetti
   :straight t)
 
-(use-package logos
-  :straight t
-  :custom
-  ;; If you want to use outlines instead of page breaks (the ^L):
-  (logos-outlines-are-pages t)
-  ;; This is the default value for the outlines:
-  (logos-outline-regexp-alist `((emacs-lisp-mode . "^;;;+ ")
-                                (org-mode . "^\\*+ +")
-                                (markdown-mode . "^\\#+ +")))
-  ;; These apply when `logos-focus-mode' is enabled.  Their value is buffer-local.
-  (logos-hide-cursor nil)
-  (logos-hide-mode-line t)
-  (logos-hide-header-line t)
-  (logos-hide-buffer-boundaries t)
-  (logos-hide-fringe t)
-  (logos-variable-pitch nil)
-  (logos-buffer-read-only nil)
-  (logos-scroll-lock nil)
-  :init
-  (let ((map global-map))
-    (define-key map [remap narrow-to-region] #'logos-narrow-dwim)
-    (define-key map [remap forward-page] #'logos-forward-page-dwim)
-    (define-key map [remap backward-page] #'logos-backward-page-dwim)
-    (define-key map (kbd "<f9>") #'logos-focus-mode)))
-
 (use-package nerd-icons-ibuffer
   :straight t
   :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
---
+++ modules/obsolete/me-logos.el
@@ -0,0 +1,38 @@
+;;; me-logos.el --- Focus and narrow -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package logos
+  :straight t
+  :custom
+  ;; If you want to use outlines instead of page breaks (the ^L):
+  (logos-outlines-are-pages t)
+  ;; This is the default value for the outlines:
+  (logos-outline-regexp-alist `((emacs-lisp-mode . "^;;;+ ")
+                                (org-mode . "^\\*+ +")
+                                (markdown-mode . "^\\#+ +")))
+  ;; These apply when `logos-focus-mode' is enabled.  Their value is buffer-local.
+  (logos-hide-cursor nil)
+  (logos-hide-mode-line t)
+  (logos-hide-header-line t)
+  (logos-hide-buffer-boundaries t)
+  (logos-hide-fringe t)
+  (logos-variable-pitch nil)
+  (logos-buffer-read-only nil)
+  (logos-scroll-lock nil)
+  :init
+  (let ((map global-map))
+    (define-key map [remap narrow-to-region] #'logos-narrow-dwim)
+    (define-key map [remap forward-page] #'logos-forward-page-dwim)
+    (define-key map [remap backward-page] #'logos-backward-page-dwim)
+    (define-key map (kbd "<f9>") #'logos-focus-mode)))
+
+
+(provide 'obsolete/me-logos)
+;;; me-logos.el ends here
