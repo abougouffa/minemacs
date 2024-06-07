2e29afae63d9c158b5199e2289016440238fbe5b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-07 15:52:13 +0200

    tweak(jiralib2): enable inserting ticket summary

--- modules/me-services.el
+++ modules/me-services.el
@@ -12,6 +12,7 @@
 (use-package jiralib2
   :straight t
   :commands +jira-insert-ticket-id +jira-insert-ticket-link
+  :autoload +jira-get-ticket
   :init
   (defvar-local +jira-open-status '("open" "to do" "in progress"))
   :config
@@ -19,27 +20,38 @@
     (let ((item (assoc ticket minibuffer-completion-table)))
       (when item (concat "    " (cdr item)))))
 
-  (defun +jira-insert-ticket-id ()
+  (defun +jira-get-ticket ()
     "Insert ticket ID, choose from states defined in `+jira-open-status'."
     (interactive)
     (when-let* ((issues (jiralib2-jql-search (format "assignee=\"%s\" AND status in (%s)"
                                                      jiralib2-user-login-name
                                                      (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
-                (tickets (mapcar (lambda (t) (cons (cdr (assoc 'key t)) (cdr (assoc 'summary (cdr (assoc 'fields t)))))) issues)))
-      (insert
-       (if (length= tickets 1)
-           (car (car tickets))
-         (let ((completion-extra-properties '(:annotation-function +jira--ticket-annotation-fn)))
-           (completing-read "Select ticket: " tickets))))))
+                (tickets (mapcar (lambda (ticket) (cons (cdr (assoc 'key ticket)) (cdr (assoc 'summary (cdr (assoc 'fields ticket)))))) issues)))
+      (if (length= tickets 1)
+          (car tickets)
+        (let ((completion-extra-properties '(:annotation-function +jira--ticket-annotation-fn)))
+          (assoc (completing-read "Select ticket: " tickets) tickets #'string=)))))
 
-  (defun +jira-insert-ticket-link ()
+  (defun +jira-insert-ticket-id (with-summary)
+    "Insert ticket ID, choose from states defined in `+jira-open-status'."
+    (interactive "P")
+    (when-let* ((issues (jiralib2-jql-search (format "assignee=\"%s\" AND status in (%s)"
+                                                     jiralib2-user-login-name
+                                                     (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
+                (tickets (mapcar (lambda (t) (cons (cdr (assoc 'key t)) (cdr (assoc 'summary (cdr (assoc 'fields t)))))) issues))
+                (ticket (+jira-get-ticket)))
+      (insert (format "%s%s" (car ticket) (if with-summary (concat ": " (cdr ticket)) "")))))
+
+  (defun +jira-insert-ticket-link (with-summary)
     "Insert ticket link, choose from states defined in `+jira-open-status'."
-    (interactive)
-    (let* ((id (with-temp-buffer (+jira-insert-ticket-id) (buffer-string)))
+    (interactive "P")
+    (let* ((ticket (+jira-get-ticket))
+           (id (car ticket))
+           (summary (cdr ticket))
            (link (format "%s/browse/%s" jiralib2-url id)))
       (insert
-       (cond ((derived-mode-p 'org-mode) (format "[[%s][%s]]" link id))
-             ((derived-mode-p 'markdown-mode 'git-commit-mode) (format "[%s](%s)" id link))
+       (cond ((derived-mode-p 'org-mode) (format "[[%s][%s%s]]" link id (if with-summary (concat ": " summary) "")))
+             ((derived-mode-p 'markdown-mode 'git-commit-mode) (format "[%s%s](%s)" id (if with-summary (concat ": " summary) "") link))
              (t link))))))
 
 (use-package org-jira
