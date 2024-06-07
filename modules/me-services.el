d14d8698dcd209e731ef2eb5d1dc752db11deddc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-07 18:15:00 +0200

    feat(services): replace `jiralib2` with `jiralib` + port my stuff to it

--- modules/me-services.el
+++ modules/me-services.el
@@ -8,10 +8,12 @@
 
 ;;; Code:
 
+(use-package org-jira
+  :straight (:host github :repo "ahungry/org-jira"))
 
-(use-package jiralib2
-  :straight t
-  :commands +jira-insert-ticket-id +jira-insert-ticket-link
+(use-package jiralib
+  :straight org-jira
+  :commands +jira-insert-ticket-id +jira-insert-ticket-link +jiralib-auto-login
   :autoload +jira-get-ticket
   :init
   (defvar-local +jira-open-status '("open" "to do" "in progress"))
@@ -23,9 +25,9 @@
   (defun +jira-get-ticket ()
     "Insert ticket ID, choose from states defined in `+jira-open-status'."
     (interactive)
-    (when-let* ((issues (jiralib2-jql-search (format "assignee=\"%s\" AND status in (%s)"
-                                                     jiralib2-user-login-name
-                                                     (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
+    (when-let* ((issues (jiralib-do-jql-search (format "assignee=\"%s\" AND status in (%s)"
+                                                       jiralib2-user-login-name
+                                                       (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
                 (tickets (mapcar (lambda (ticket) (cons (cdr (assoc 'key ticket)) (cdr (assoc 'summary (cdr (assoc 'fields ticket)))))) issues)))
       (if (length= tickets 1)
           (car tickets)
@@ -35,11 +37,7 @@
   (defun +jira-insert-ticket-id (with-summary)
     "Insert ticket ID, choose from states defined in `+jira-open-status'."
     (interactive "P")
-    (when-let* ((issues (jiralib2-jql-search (format "assignee=\"%s\" AND status in (%s)"
-                                                     jiralib2-user-login-name
-                                                     (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
-                (tickets (mapcar (lambda (t) (cons (cdr (assoc 'key t)) (cdr (assoc 'summary (cdr (assoc 'fields t)))))) issues))
-                (ticket (+jira-get-ticket)))
+    (when-let* ((ticket (car tickets)))
       (insert (format "%s%s" (car ticket) (if with-summary (concat ": " (cdr ticket)) "")))))
 
   (defun +jira-insert-ticket-link (with-summary)
@@ -48,14 +46,23 @@
     (let* ((ticket (+jira-get-ticket))
            (id (car ticket))
            (summary (cdr ticket))
-           (link (format "%s/browse/%s" jiralib2-url id)))
+           (link (format "%s/browse/%s" jiralib-url id)))
       (insert
        (cond ((derived-mode-p 'org-mode) (format "[[%s][%s%s]]" link id (if with-summary (concat ": " summary) "")))
              ((derived-mode-p 'markdown-mode 'git-commit-mode) (format "[%s%s](%s)" id (if with-summary (concat ": " summary) "") link))
-             (t link))))))
+             (t link)))))
 
-(use-package org-jira
-  :straight (:host github :repo "ahungry/org-jira"))
+  (defun +jiralib-auto-login ()
+    "Auto login to Jira using credentials from `auth-source'."
+    (interactive)
+    (unless jiralib-token
+      (when-let* ((host (if (string= jiralib-host "")
+                            (url-host (url-generic-parse-url jiralib-url))
+                          jiralib-host))
+                  (auth (car (auth-source-search :host jiralib-url)))
+                  (user (plist-get auth :user))
+                  (pass (funcall (plist-get auth :secret))))
+        (jiralib-login user pass)))))
 
 (use-package tributary
   :straight (:host github :repo "mrkrd/tributary" :files (:defaults "confluence.rnc")))
