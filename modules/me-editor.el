1d7413655850d79bf52b1190e0df8870f886b7fb
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-08 18:43:10 +0200

    feat(editor): make `tempel` obsolete

--- modules/me-editor.el
+++ modules/me-editor.el
@@ -8,26 +8,6 @@
 
 ;;; Code:
 
-(use-package tempel
-  :straight t
-  :custom
-  (tempel-trigger-prefix "<") ;; Require trigger prefix before template name when completing.
-  (tempel-path (list (concat minemacs-assets-dir "templates/tempel/*.eld")
-                     (concat minemacs-config-dir "templates/tempel/*.eld")))
-  :bind (("M-\"" . tempel-complete) ;; Alternative tempel-expand
-         ("M-*" . tempel-insert)
-         :map tempel-map
-         ("TAB" . tempel-next)
-         ("<backtab>" . tempel-previous))
-  :hook ((prog-mode text-mode) . +tempel-setup-capf-h)
-  :hook (prog-mode . tempel-abbrev-mode)
-  :config
-  (defun +tempel-setup-capf-h ()
-    (add-hook 'completion-at-point-functions #'tempel-complete -100 t)))
-
-(use-package tempel-collection
-  :straight t)
-
 (use-package wgrep
   :straight t
   :commands wgrep-change-to-wgrep-mode
---
+++ modules/obsolete/me-tempel.el
@@ -0,0 +1,33 @@
+;;; me-tempel.el --- OBSOLETE MODULE/PACKAGE -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package tempel
+  :straight t
+  :custom
+  (tempel-trigger-prefix "<") ;; Require trigger prefix before template name when completing.
+  (tempel-path (list (concat minemacs-assets-dir "templates/tempel/*.eld")
+                     (concat minemacs-config-dir "templates/tempel/*.eld")))
+  :bind (("M-\"" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert)
+         :map tempel-map
+         ("TAB" . tempel-next)
+         ("<backtab>" . tempel-previous))
+  :hook ((prog-mode text-mode) . +tempel-setup-capf-h)
+  :hook (prog-mode . tempel-abbrev-mode)
+  :config
+  (defun +tempel-setup-capf-h ()
+    (add-hook 'completion-at-point-functions #'tempel-complete -100 t)))
+
+(use-package tempel-collection
+  :straight t)
+
+
+(provide 'obsolete/me-tempel)
+;;; me-tempel.el ends here
