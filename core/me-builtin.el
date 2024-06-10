f1ff1728c7a933097f70267e9e1e39eec989ec0e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-10 16:28:00 +0200

    tweak(xref): bind `M-<down-mouse-1>` to `xref-find-references-at-mouse`

--- core/me-builtin.el
+++ core/me-builtin.el
@@ -506,7 +506,10 @@ or file path may exist now."
   :straight t
   :custom
   ;; Use completion in the minibuffer instead of definitions buffer
-  (xref-show-definitions-function #'xref-show-definitions-completing-read))
+  (xref-show-definitions-function #'xref-show-definitions-completing-read)
+  ;; NOTE: Usually, this shorcut can be bound to moves the window (set by the OS
+  ;; window manager), so we need to disable it in the WM for this to work.
+  :bind ("M-<down-mouse-1>" . xref-find-references-at-mouse))
 
 (use-package eglot
   :straight t
