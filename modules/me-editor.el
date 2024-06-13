419f2970df00602746255aed382a936022ec8923
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-13 19:04:36 +0200

    feat(editor): add more searching packages

--- modules/me-editor.el
+++ modules/me-editor.el
@@ -104,6 +104,21 @@
   :straight t
   :bind (:package isearch :map isearch-mode-map ([f2] . cc-isearch-menu-transient)))
 
+(use-package isearch+
+  :straight t)
+
+(use-package isearch-mb
+  :straight t
+  :after isearch
+  :init
+  (isearch-mb-mode 1))
+
+(use-package phi-search
+  :straight t)
+
+(use-package phi-grep
+  :straight t)
+
 
 (provide 'me-editor)
 
