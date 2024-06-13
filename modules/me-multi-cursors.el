abed2148ea1816ed98987d5e400885a878a5d322
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-13 19:02:22 +0200

    tweak(multiple-cursors): better keybindings

--- modules/me-multi-cursors.el
+++ modules/me-multi-cursors.el
@@ -14,15 +14,17 @@
 
 (use-package multiple-cursors
   :straight t
-  :bind (("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)
-         ("C-S-c x" . mc/mark-more-like-this-extended)
-         ("C-S-c a" . mc/mark-all-dwim)
-         ("C-S-c s" . mc/mark-all-symbols-like-this)
-         ("C-S-c C-S-c" . mc/edit-lines)
-         ("C-S-c C-e" . mc/edit-ends-of-lines)
-         ("C-S-c C-a" . mc/edit-beginnings-of-lines)
+  :bind (("C->"           . mc/mark-next-like-this)
+         ("C-<"           . mc/mark-previous-like-this)
+         ("C-c C-<"       . mc/mark-all-like-this)
+         ("C-S-c x"       . mc/mark-more-like-this-extended)
+         ("C-S-c a"       . mc/mark-all-dwim)
+         ("C-S-c s"       . mc/mark-next-symbol-like-this)
+         ("C-S-c S"       . mc/mark-previous-like-this-symbol)
+         ("C-S-c C-s"     . mc/mark-all-symbols-like-this)
+         ("C-S-c C-S-c"   . mc/edit-lines)
+         ("C-S-c C-e"     . mc/edit-ends-of-lines)
+         ("C-S-c C-a"     . mc/edit-beginnings-of-lines)
          ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
 
 
