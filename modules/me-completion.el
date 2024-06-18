552888452815db42adc03c29e436f820f9428849
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-18 20:47:36 +0200

    tweak(nerd-icons-completion): enable globally

--- modules/me-completion.el
+++ modules/me-completion.el
@@ -243,7 +243,9 @@
 
 (use-package nerd-icons-completion
   :straight t
-  :hook (marginalia-mode . nerd-icons-completion-marginalia-setup))
+  :hook (marginalia-mode . nerd-icons-completion-marginalia-setup)
+  :config
+  (nerd-icons-completion-mode 1))
 
 (use-package orderless
   :straight t
