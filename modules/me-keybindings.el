2e29121f20f4b4507eebc1b078c315978e749605
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-06-22 17:12:52 +0200

    refactor: move `which-key` to `me-builtin`, it is builtin in Emacs 30

--- modules/me-keybindings.el
+++ modules/me-keybindings.el
@@ -8,22 +8,6 @@
 
 ;;; Code:
 
-(use-package which-key
-  :straight t
-  :hook (minemacs-lazy . which-key-mode)
-  :custom
-  (which-key-idle-delay 1.0)
-  (which-key-idle-secondary-delay nil)
-  (which-key-ellipsis "..")
-  (which-key-prefix-prefix "+")
-  (which-key-sort-order #'which-key-key-order-alpha)
-  (which-key-min-display-lines 3)
-  (which-key-max-display-columns nil)
-  (which-key-allow-multiple-replacements t) ; Allow multiple rules in `which-key-replacement-alist'
-  :config
-  ;; Setup `which-key' integration with the minibuffer
-  (which-key-setup-minibuffer))
-
 (use-package hydra
   :straight t)
 
