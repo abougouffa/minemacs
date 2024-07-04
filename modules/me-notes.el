adcc1f6b806292890839397e014f9846c3dc3efe
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-07-04 17:56:40 +0200

    feat(notes): add support for `consult-denote` (`consult-notes` replacement)

--- modules/me-notes.el
+++ modules/me-notes.el
@@ -10,19 +10,22 @@
 
 ;; Useful resources:
 ;; https://takeonrules.com/2022/10/01/exploring-the-denote-emacs-package/
-;; https://takeonrules.com/2022/10/02/migration-plan-for-org-roam-notes-to-denote/
-;; https://github.com/bitspook/notes-migrator
+;; https://www.thesing-online.de/blog/20230302T215700--denote-as-a-zettelkasten.html
 
 (use-package denote
   :straight t
   :commands denote-create-note denote-insert-link denote-show-backlinks-buffer
   :hook (dired-mode . denote-dired-mode)
   :custom
-  (denote-prompts '(title keywords)) ; These are the minimum viable prompts for notes
-  (denote-file-type 'org) ; I love org-mode format; reading ahead I'm setting this
   (denote-date-prompt-use-org-read-date t) ; And `org-read-date' is an amazing bit of tech
   :config
   (denote-rename-buffer-mode 1))
+
+(use-package consult-denote
+  :straight t
+  :after consult
+  :init
+  (consult-denote-mode 1))
 
 
 (provide 'me-notes)
