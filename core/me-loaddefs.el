5996c22bbcc0f6c246d492762ff716b7ea900ba1
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-07-06 16:56:34 +0200

    tweak: regenerate autoloads

--- core/me-loaddefs.el
+++ core/me-loaddefs.el
@@ -389,19 +389,19 @@ Show the list of declared external dependencies." t)
 (autoload '+writing-mode "../modules/extras/me-writing-mode" "\
 A mode for writing without distraction.
 
-This is a minor mode.  If called interactively, toggle the
-`+Writing mode' mode.  If the prefix argument is positive, enable
-the mode, and if it is zero or negative, disable the mode.
+This is a minor mode.  If called interactively, toggle the `+Writing
+mode' mode.  If the prefix argument is positive, enable the mode, and if
+it is zero or negative, disable the mode.
 
-If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
-the mode if ARG is nil, omitted, or is a positive number.
-Disable the mode if ARG is a negative number.
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable the
+mode if ARG is nil, omitted, or is a positive number.  Disable the mode
+if ARG is a negative number.
 
 To check whether the minor mode is enabled in the current buffer,
-evaluate `+writing-mode'.
+evaluate the variable `+writing-mode'.
 
-The mode's hook is called both when the mode is enabled and when
-it is disabled.
+The mode's hook is called both when the mode is enabled and when it is
+disabled.
 
 (fn &optional ARG)" t)
 (put '+writing-global-mode 'globalized-minor-mode t)
