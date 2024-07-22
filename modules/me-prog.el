fcc092dcb50b85d40063e3fa3641f2c599a171d9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-07-22 16:33:50 +0200

    fix(xref-union): make sure we disable `etags` in `xref-union`

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -268,7 +268,19 @@
 
 (use-package xref-union
   :straight t
-  :commands xref-union-mode)
+  :commands xref-union-mode
+  :custom
+  ;; BUG+HACK: When in `xref-union-mode', the `xref-union--backend' seems to
+  ;; access all the backends, including the ones that aren't enabled locally.
+  ;; The list includes `etags' which is annoying since it asks about the TAGS
+  ;; file, which interrupts `xref-union' from trying the rest of the backends.
+  ;; So, lets exclude `etags' since I'm not using it, the predicate function can
+  ;; be modified to check for other conditions (for example: enable `etags' in
+  ;; some circumstances)
+  (xref-union-excluded-backends #'+xref-union--exclude-backends-predicate)
+  :config
+  (defun +xref-union--exclude-backends-predicate (backend)
+    (memq backend '(etags--xref-backend))))
 
 (use-package hl-todo
   :straight (:host github :repo "tarsius/hl-todo")
