a2d6892fbc30b9f6d879a723d32736f9e404cf90
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-07-23 21:41:16 +0200

    feat(bitbake): add some helper commands

--- modules/me-embedded.el
+++ modules/me-embedded.el
@@ -27,7 +27,36 @@
   :hook (bitbake-mode . bitbake-electric-mode)
   :config
   (require 'bitbake-insert)
-  (require 'bitbake-electric))
+  (require 'bitbake-electric)
+  :init
+  (defun +bitbake-poky-sources (build-dir &optional include-native)
+    "Get all source directories for BUILD-DIR. Optionally INCLUDE-NATIVE."
+    ;; From the build-dir "yocto-ws/build-MACHINE/", this will extract all source
+    ;; directories of these formats:
+    ;; yocto-ws/build-MACHINE/tmp/work/aarch64-rdk-linux/procps/3.3.16-r0/git/
+    ;; yocto-ws/build-MACHINE/tmp/work/aarch64-rdk-linux/ppp/2.4.7-r0/ppp-2.4.7/
+    (let* (result
+           (base-dir (expand-file-name "tmp/work/" build-dir))
+           (arch-dirs (seq-filter #'file-directory-p (directory-files base-dir t "[^.][^.]?\\'"))))
+      (dolist (arch-dir arch-dirs)
+        (let* ((package-dirs (directory-files arch-dir t "[^.][^.]?\\'"))
+               (package-dirs (if include-native package-dirs (seq-filter (lambda (dir) (not (string-suffix-p "-native" dir))) package-dirs))))
+          (dolist (package-dir package-dirs)
+            (let ((ver-dirs (directory-files package-dir t "[^.][^.]?\\'")))
+              (dolist (ver-dir ver-dirs)
+                (let* ((ver (string-trim-right (file-name-nondirectory (directory-file-name ver-dir)) "-r[[:digit:]]*$"))
+                       (dir-git (expand-file-name "git/" ver-dir))
+                       (dir-non-git (expand-file-name (format "%s-%s/" (file-name-nondirectory (directory-file-name package-dir)) ver) ver-dir)))
+                  (cond ((file-directory-p dir-git)
+                         (push dir-git result))
+                        ((file-directory-p dir-non-git)
+                         (push dir-non-git result)))))))))
+      result))
+
+  (defun +bitbake-insert-poky-sources (build-dir)
+    "Insert poky source directories for BUILD-DIR."
+    (interactive "D")
+    (insert (string-join (+bitbake-poky-sources build-dir) "\n"))))
 
 (use-package mips-mode
   :straight t)
