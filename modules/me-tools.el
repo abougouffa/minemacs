e4945dfc1317e4c088682c3d3e73ba8e01743c6b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-01 18:47:05 +0200

    feat(tools): make `restclient` obsolete (use `hurl-mode` or `verb`)

--- modules/me-tools.el
+++ modules/me-tools.el
@@ -142,33 +142,6 @@
   :config
   (define-key org-mode-map (kbd "C-c C-r") verb-command-map))
 
-(use-package restclient
-  :straight (:host github :repo "pashky/restclient.el" :files ("*.el") :fork (:repo "abougouffa/restclient.el"))
-  :hook (restclient-mode . display-line-numbers-mode)
-  :mode ("\\.http\\'" . restclient-mode)
-  :config
-  (require 'restclient-jq)
-  (+setq-hook! restclient-mode
-    imenu-generic-expression '((nil "^[A-Z]+\s+.+" 0)))
-
-  ;; From Doom Emacs (in case `gnutls-verify-error' policy is set to something)
-  (advice-add
-   #'restclient-http-do :around
-   (satch-defun +restclient--permit-self-signed-ssl:around-a (orig-fn &rest args)
-     "Forces underlying SSL verification to prompt for self-signed or invalid
-certs, rather than reject them silently."
-     (require 'gnutls)
-     (let (gnutls-verify-error) (apply orig-fn args)))))
-
-(use-package restclient-test
-  :straight t)
-
-(use-package ob-restclient
-  :straight t
-  :after org
-  :init
-  (org-babel-do-load-languages 'org-babel-load-languages '((restclient . t))))
-
 (use-package impostman
   :straight t)
 
