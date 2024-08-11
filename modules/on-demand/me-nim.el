4e42677b45061a55d9d61363594f62b67df7b60a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-11 16:32:21 +0200

    tweak: bump packages versions

--- modules/on-demand/me-nim.el
+++ modules/on-demand/me-nim.el
@@ -12,8 +12,12 @@
 (minemacs-register-on-demand-module 'me-nim
   :auto-mode '(("\\.nim\\'" . nim-mode) ("\\.nim\\(ble\\|s\\)\\'" . nimscript-mode-maybe)))
 
+(push 'flycheck-nimsuggest straight-built-in-pseudo-packages)
+
 (use-package nim-mode
   :straight t)
+
+(cl-callf2 remq 'flycheck-nimsuggest straight-built-in-pseudo-packages)
 
 
 (provide 'on-demand/me-nim)
