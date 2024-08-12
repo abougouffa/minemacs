63a0ca2230e802458e9f1ae1281d607c3c6cbb66
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-12 17:18:09 +0200

    feat(prog): make `clang-format` obsolete (use `apheleia` and `format-all`)

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -254,17 +254,6 @@
 (use-package format-all
   :straight t)
 
-;; for bin in $(ls $(dirname $(which clang-13))/clang-*); do ln -s $bin $HOME/.local/bin/$(basename ${bin%-13}); done
-(use-package clang-format
-  :straight t
-  :config
-  (advice-add
-   'clang-format-region :before
-   (satch-defun +clang-format--set-style ()
-     "Automatically set the `clang-format' style if not already set."
-     (unless (local-variable-p 'clang-format-style)
-       (setq-local clang-format-style (+clang-format-get-style))))))
-
 (use-package quickrun
   :straight t)
 
---
+++ modules/obsolete/me-clang-format.el
@@ -0,0 +1,24 @@
+;;; me-clang-format.el --- Clang-format -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "noqryunx.obhtbhssn-rkg@fntrzpbz.pbz")
+
+;;; Commentary:
+
+;;; Code:
+
+;; for bin in $(ls $(dirname $(which clang-13))/clang-*); do ln -s $bin $HOME/.local/bin/$(basename ${bin%-13}); done
+(use-package clang-format
+  :straight t
+  :config
+  (advice-add
+   'clang-format-region :before
+   (satch-defun +clang-format--set-style ()
+     "Automatically set the `clang-format' style if not already set."
+     (unless (local-variable-p 'clang-format-style)
+       (setq-local clang-format-style (+clang-format-get-style))))))
+
+
+(provide 'obsolete/me-clang-format)
+;;; me-clang-format.el ends here
