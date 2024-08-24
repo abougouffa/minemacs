7835185690f25dd34ae6728d4183735f7853a58e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-24 14:58:49 +0200

    feat(emacs-lisp): make `elisp-demos` obsolete

---
+++ modules/obsolete/me-elisp-demos.el
@@ -0,0 +1,19 @@
+;;; me-elisp-demos.el --- elisp-demos -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package elisp-demos
+  :straight t
+  :init
+  (advice-add 'describe-function-1 :after #'elisp-demos-advice-describe-function-1)
+  (advice-add 'helpful-update :after #'elisp-demos-advice-helpful-update))
+
+
+(provide 'obsolete/me-elisp-demos)
+;;; me-elisp-demos.el ends here
