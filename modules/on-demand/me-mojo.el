b237a9c42517209addbefe99805aa1dbfea9c74b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-27 17:15:43 +0200

    feat(on-demand): add support for the Mojo language

---
+++ modules/on-demand/me-mojo.el
@@ -0,0 +1,22 @@
+;;; me-mojo.el --- Mojo language -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+;;;###autoload
+(minemacs-register-on-demand-module 'me-mojo
+  :auto-mode '(("\\.mojo\\'" . mojo-mode) ("\\.🔥\\'" . mojo-mode)))
+
+(use-package mojo
+  :straight (:host github :repo "andcarnivorous/mojo-hl")
+  :commands (mojo-mode mojo-compile)
+  :mode "\\.🔥\\'")
+
+
+(provide 'on-demand/me-mojo)
+;;; me-mojo.el ends here
