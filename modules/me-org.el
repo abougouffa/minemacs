02f433e2403f7420858d485456d3c45f7a45e1dc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-27 12:57:48 +0200

    refactor: remove `me-biblio`, move `citar` & `citar-embark` to `me-org`

--- modules/me-org.el
+++ modules/me-org.el
@@ -83,6 +83,33 @@
   :custom
   (org-fragtog-preview-delay 0.2))
 
+(use-package citar
+  :straight t
+  :after minemacs-first-org-file oc
+  :demand
+  :custom
+  (org-cite-insert-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-activate-processor 'citar)
+  (citar-symbol-separator "  ")
+  :config
+  (with-eval-after-load 'nerd-icons
+    (defun +citar--set-symbols ()
+      (setq citar-symbols
+            `((file ,(nerd-icons-codicon "nf-cod-file_pdf" :face 'error) . " ")
+              (note ,(nerd-icons-faicon "nf-fa-file_text" :face 'warning) . " ")
+              (link ,(nerd-icons-mdicon "nf-md-link" :face 'org-link) . " "))))
+
+    ;; Properly setup citar-symbols
+    (once-x-call '(:check display-graphic-p :hooks server-after-make-frame-hook)
+      #'+citar--set-symbols)))
+
+(use-package citar-embark
+  :straight t
+  :after citar embark
+  :init
+  (citar-embark-mode 1))
+
 
 (provide 'me-org)
 
