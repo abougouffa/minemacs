8047d477b790ba94503ad23a9e80f9611b7646fc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-08-29 18:46:13 +0200

    feat(math): make `matlab-mode` obsolete

---
+++ modules/obsolete/me-matlab.el
@@ -0,0 +1,16 @@
+;;; me-matlab.el --- Matlab -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package matlab-mode
+  :straight (:url "https://git.code.sf.net/p/matlab-emacs/src" :files (:defaults (:exclude "company-matlab-shell.el"))))
+
+
+(provide 'obsolete/me-matlab)
+;;; me-matlab.el ends here
