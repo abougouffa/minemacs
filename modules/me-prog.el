c4d6e515738a69a47e868aafff70e63c43321efd
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-06 10:12:34 +0100

    feat(prog): make `cmake-build` `czm-cpp` obsoletes

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -209,33 +209,6 @@
             (gambol:occur)
           (error (call-interactively #'occur))))))
 
-;; CMake building with multiple targets, run configurations and interactive menu
-(use-package cmake-build
-  :straight (:host github :repo "ultronozm/cmake-build.el")
-  :preface
-  ;; For some reason, Emacs keeps creating a copy of the file under "~/.emacs.d/" even when the package isn't loaded!
-  (setq cmake-build-local-options-file (concat minemacs-local-dir "cmake-build-options.el"))
-  :commands (cmake-build-clean
-             cmake-build-clear-cache-and-configure
-             cmake-build-current
-             cmake-build-debug
-             cmake-build-delete-current-windows
-             cmake-build-other-target
-             cmake-build-run
-             cmake-build-run-cmake
-             cmake-build-set-buffer-local-config
-             cmake-build-set-cmake-profile
-             cmake-build-set-config
-             cmake-build-set-options
-             cmake-build-set-project-build-root
-             cmake-build-set-project-root))
-
-
-;; Helper commands and functions for working with C++ projects
-(use-package czm-cpp
-  :straight (:host github :repo "ultronozm/czm-cpp.el"))
-
-
 ;; Structured editing and navigation in Emacs with Tree-Sitter
 (use-package combobulate
   :straight (:host github :repo "mickeynp/combobulate" :nonrecursive t) ; Cloning the `html-ts-mode' submodule causes problems
---
+++ modules/obsolete/me-cmake-build.el
@@ -0,0 +1,39 @@
+;;; me-cmake-build.el --- CMake build -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+;; CMake building with multiple targets, run configurations and interactive menu
+(use-package cmake-build
+  :straight (:host github :repo "ultronozm/cmake-build.el")
+  :preface
+  ;; For some reason, Emacs keeps creating a copy of the file under "~/.emacs.d/" even when the package isn't loaded!
+  (setq cmake-build-local-options-file (concat minemacs-local-dir "cmake-build-options.el"))
+  :commands (cmake-build-clean
+             cmake-build-clear-cache-and-configure
+             cmake-build-current
+             cmake-build-debug
+             cmake-build-delete-current-windows
+             cmake-build-other-target
+             cmake-build-run
+             cmake-build-run-cmake
+             cmake-build-set-buffer-local-config
+             cmake-build-set-cmake-profile
+             cmake-build-set-config
+             cmake-build-set-options
+             cmake-build-set-project-build-root
+             cmake-build-set-project-root))
+
+
+;; Helper commands and functions for working with C++ projects
+(use-package czm-cpp
+  :straight (:host github :repo "ultronozm/czm-cpp.el"))
+
+
+(provide 'obsolete/me-cmake-build)
+;;; me-cmake-build.el ends here
