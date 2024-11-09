67b9fb27968a0f295d7ce07bc9f503788f2c5008
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-09 21:14:46 +0100

    fix(combobulate): hack to prevent `combobulate` from loading at startup

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -212,11 +212,23 @@
 
 ;; Structured editing and navigation in Emacs with Tree-Sitter
 (use-package combobulate
-  :straight (:host github :repo "mickeynp/combobulate" :nonrecursive t) ; Cloning the `html-ts-mode' submodule causes problems
+  :straight (combobulate
+             :host github
+             :repo "mickeynp/combobulate"
+             :nonrecursive t ; Cloning the `html-ts-mode' submodule causes problems
+             :files (:defaults (:exclude "combobulate.el"))) ; TEMP: The "combobulate.el" contains a lot of autoloads that prevent lazy loading
   :when (and (not os/win) (+emacs-features-p 'tree-sitter)) ; TEMP: disable on Windows
   :custom
   (combobulate-key-prefix "C-c o")
   :config
+  ;; Basically, load the same features that would be loaded by "combobulate.el"
+  (dolist (feature '(combobulate-rules combobulate-procedure combobulate-navigation
+                     combobulate-manipulation combobulate-envelope combobulate-display
+                     combobulate-ui combobulate-misc combobulate-query combobulate-cursor
+                     combobulate-toml combobulate-html combobulate-python combobulate-js-ts
+                     combobulate-css combobulate-yaml combobulate-json combobulate-go))
+    (require feature))
+
   ;; The "M-<up/down/left/right>" keys are used globally by `drag-stuff', lets
   ;; unset them for `combobulate' and use "M-S-<up/down/left/right>" instead.
   (mapc (lambda (k) (keymap-unset combobulate-key-map k 'remove)) '("M-<up>" "M-<down>" "M-<left>" "M-<right>"))
