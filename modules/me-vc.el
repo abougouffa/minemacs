392dcb4a1a1c4878f50f137e993494c934c93194
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-09 21:46:32 +0100

    feat(vc): replace `magit-file-icons` with `magit-iconify`

--- modules/me-vc.el
+++ modules/me-vc.el
@@ -45,11 +45,14 @@
 
 
 ;; File icons for Magit based on `nerd-icons'
-(use-package magit-file-icons
-  :straight t
-  :trigger-commands magit magit-status magit-status-here magit-log magit-log-all
-  :init
-  (magit-file-icons-mode 1))
+(use-package magit-iconify
+  :straight (:host github :repo "justinbarclay/magit-iconify")
+  :after magit
+  :demand
+  :config
+  ;; HACK: By default, `magit-iconify' the icons are shown after running
+  ;; `magit-refresh', this ensures displaying them at startup
+  (advice-add 'magit-status-setup-buffer :after (lambda (&rest _args) (magit-iconify-status-buffer))))
 
 
 ;; Magit extension for "git-imerge"
