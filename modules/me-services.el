c9a323582a432fa9e553a04bfd08e5714c86ad0d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-17 16:53:48 +0100

    tweak(jiralib): better documentation for my custom commands

--- modules/me-services.el
+++ modules/me-services.el
@@ -21,33 +21,41 @@
       (when item (concat "    " (cdr item)))))
 
   (defun +jira-get-ticket ()
-    "Insert ticket ID, choose from states defined in `+jira-open-status'."
-    (interactive)
+    "Get ticket ID, choose from states defined in `+jira-open-status'."
     (when-let* ((user (or jiralib-user-login-name jiralib-user))
-                (issues (jiralib-do-jql-search (format "assignee=\"%s\" AND status in (%s)" user
-                                                       (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
-                (tickets (mapcar (lambda (ticket) (cons (cdr (assoc 'key ticket)) (cdr (assoc 'summary (cdr (assoc 'fields ticket)))))) issues)))
+                (issues (jiralib-do-jql-search
+                         (format "assignee=\"%s\" AND status in (%s)" user
+                                 (string-join (mapcar (apply-partially #'format "%S") +jira-open-status) ", "))))
+                (tickets (mapcar (lambda (ticket)
+                                   (cons (cdr (assoc 'key ticket))
+                                         (cdr (assoc 'summary (cdr (assoc 'fields ticket))))))
+                                 issues)))
       (if (length= tickets 1)
           (car tickets)
         (let ((completion-extra-properties '(:annotation-function +jira--ticket-annotation-fn)))
           (assoc (completing-read "Select ticket: " tickets) tickets #'string=)))))
 
   (defun +jira-insert-ticket-id (with-summary)
-    "Insert ticket ID, choose from states defined in `+jira-open-status'."
+    "Insert ticket ID, optionally WITH-SUMMARY.
+This depends on the states defined in `+jira-open-status'."
     (interactive "P")
     (when-let* ((ticket (+jira-get-ticket)))
       (insert (format "%s%s" (car ticket) (if with-summary (concat ": " (cdr ticket)) "")))))
 
   (defun +jira-insert-ticket-link (with-summary)
-    "Insert ticket link, choose from states defined in `+jira-open-status'."
+    "Insert ticket link, optionally WITH-SUMMARY.
+This depends on the states defined in `+jira-open-status'.
+The link style depends on the current major mode."
     (interactive "P")
     (let* ((ticket (+jira-get-ticket))
            (id (car ticket))
            (summary (cdr ticket))
            (link (format "%s/browse/%s" jiralib-url id)))
       (insert
-       (cond ((derived-mode-p 'org-mode) (format "[[%s][%s%s]]" link id (if with-summary (concat ": " summary) "")))
-             ((derived-mode-p 'markdown-mode 'git-commit-mode 'markdown-ts-mode) (format "[%s%s](%s)" id (if with-summary (concat ": " summary) "") link))
+       (cond ((derived-mode-p 'org-mode)
+              (format "[[%s][%s%s]]" link id (if with-summary (concat ": " summary) "")))
+             ((derived-mode-p 'markdown-mode 'git-commit-mode 'markdown-ts-mode)
+              (format "[%s%s](%s)" id (if with-summary (concat ": " summary) "") link))
              (t link)))))
 
   (defun +jiralib-auto-login ()
