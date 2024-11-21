60624cbe9c29169eb5feb30e68045822b97b9222
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-21 01:33:01 +0100

    feat(ui): make `mixed-pitch` obsolete

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -86,26 +86,6 @@
   :bind ([f10] . lacarte-execute-menu-command))
 
 
-;; Use a variable pitch, keeping fixed pitch where it's sensible
-(use-package mixed-pitch
-  :straight t
-  :custom
-  (mixed-pitch-variable-pitch-cursor nil)
-  :config
-  (setq
-   mixed-pitch-fixed-pitch-faces
-   (delete-dups
-    (append
-     mixed-pitch-fixed-pitch-faces
-     '(font-lock-comment-delimiter-face font-lock-comment-face org-block
-       org-block-begin-line org-block-end-line org-cite org-cite-key
-       org-document-info-keyword org-done org-drawer org-footnote org-formula
-       org-inline-src-block org-latex-and-related org-link org-code org-column
-       org-column-title org-date org-macro org-meta-line org-property-value
-       org-quote org-ref-cite-face org-sexp-date org-special-keyword org-src
-       org-table org-tag org-tag-group org-todo org-verbatim org-verse)))))
-
-
 ;; Display "^L" page breaks as tidy horizontal lines
 (use-package page-break-lines
   :straight t
---
+++ modules/obsolete/me-mixed-pitch.el
@@ -0,0 +1,33 @@
+;;; me-mixed-pitch.el --- Description -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; Use a variable pitch, keeping fixed pitch where it's sensible
+(use-package mixed-pitch
+  :straight t
+  :custom
+  (mixed-pitch-variable-pitch-cursor nil)
+  :config
+  (setq
+   mixed-pitch-fixed-pitch-faces
+   (delete-dups
+    (append
+     mixed-pitch-fixed-pitch-faces
+     '(font-lock-comment-delimiter-face font-lock-comment-face org-block
+       org-block-begin-line org-block-end-line org-cite org-cite-key
+       org-document-info-keyword org-done org-drawer org-footnote org-formula
+       org-inline-src-block org-latex-and-related org-link org-code org-column
+       org-column-title org-date org-macro org-meta-line org-property-value
+       org-quote org-ref-cite-face org-sexp-date org-special-keyword org-src
+       org-table org-tag org-tag-group org-todo org-verbatim org-verse)))))
+
+
+(provide 'obsolete/me-mixed-pitch)
+;;; me-mixed-pitch.el ends here
