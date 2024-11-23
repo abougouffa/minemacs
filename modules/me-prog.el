79f0cf21b53c0e30e008d1d3c47cb14be5471d25
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-23 20:07:03 +0100

    feat(prog): make `treesitter-context` obsolete

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -158,22 +158,6 @@
     [("Q" "Quit" ignore :transient t)]))
 
 
-;; A `treesit'-based package to show code context, dim surrouding text, and fold code
-(use-package treesitter-context
-  :straight (:host github :repo "zbelial/treesitter-context.el")
-  :when (+emacs-options-p 'tree-sitter)
-  :custom
-  (treesitter-context-idle-time 0.5)
-  (treesitter-context-show-context-always nil)
-  :config
-  (defun +treesitter-context--set-colors-from-theme (&rest _args)
-    (setq treesitter-context-background-color (face-background 'hl-line)
-          treesitter-context-border-color (face-foreground 'line-number)))
-
-  (+treesitter-context--set-colors-from-theme)
-  (satch-add-hook '(enable-theme-functions disable-theme-functions) '+treesitter-context--set-colors-from-theme))
-
-
 ;; Extra non-standard functionalities for Eglot
 (use-package eglot-x
   :straight (:host github :repo "nemethf/eglot-x")
