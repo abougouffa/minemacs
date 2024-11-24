2ef76cadd738579c8eae27471efd9a3fe544b20e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-24 14:32:55 +0100

    feat(search): make `gumshoe` obsolete

---
+++ modules/obsolete/me-gumshoe.el
@@ -0,0 +1,19 @@
+;;; me-gumshoe.el --- Description -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2024  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; A smart point tracker
+(use-package gumshoe
+  :straight t
+  :hook (minemacs-lazy . global-gumshoe-mode))
+
+
+(provide 'obsolete/me-gumshoe)
+;;; me-gumshoe.el ends here
