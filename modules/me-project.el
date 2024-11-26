0bc5f8b5b7e3b09ba96b317935c6a5fb75dab9c4
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-26 20:41:54 +0100

    feat(project): restore `ffip`

--- modules/me-project.el
+++ modules/me-project.el
@@ -87,6 +87,26 @@
   :bind (:map projection-map ("D" . #'projection-dape)))
 
 
+;; Quick access to project files using `fd'
+(use-package find-file-in-project
+  :straight t
+  :custom
+  (ffip-use-rust-fd (and (executable-find "fd") t))
+  :config
+  (require 'project) ; for `project--file-completion-table'
+  (advice-add ;; This adds `nerd-icons-completion-mode' support for `ffip'
+   'ffip-completing-read :override
+   (satch-defun +ffip-completing-read (prompt collection &optional action)
+     (when-let* ((selected
+                  (if (= 1 (length collection))
+                      (car collection)
+                    (let ((sel (completing-read prompt (project--file-completion-table collection))))
+                      (or (assoc sel collection) sel)))))
+       (let* ((default-directory (ffip-get-project-root-directory))
+              (result (if (consp selected) (cdr selected) selected)))
+         (if action (funcall action result) result))))))
+
+
 (provide 'me-project)
 
 ;;; me-project.el ends here
