66702a52285260ab4c13be0a4940af8ec5137f77
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-11-26 12:24:07 +0100

    tweak(ffip): integrate with `nerd-icons-completion-mode`

--- modules/obsolete/me-ffip.el
+++ modules/obsolete/me-ffip.el
@@ -12,7 +12,20 @@
 (use-package find-file-in-project
   :straight t
   :custom
-  (ffip-use-rust-fd (and (executable-find "fd") t)))
+  (ffip-use-rust-fd (and (executable-find "fd") t))
+  :config
+  (require 'project) ; for `project--file-completion-table'
+  (advice-add ;; This adds `nerd-icons-completion-mode' support for `ffip'
+   'ffip-completing-read :override
+   (satch-defun +ffip-completing-read (prompt collection &optional action)
+     (when-let* ((selected
+                  (if (= 1 (length collection))
+                      (car collection)
+                    (let ((sel (completing-read prompt (project--file-completion-table collection))))
+                      (or (assoc sel collection) sel)))))
+       (let* ((default-directory (ffip-get-project-root-directory))
+              (result (if (consp selected) (cdr selected) selected)))
+         (if action (funcall action result) result))))))
 
 
 (provide 'obsolete/me-ffip)
