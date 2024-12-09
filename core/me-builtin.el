dbdf16350ce61f3afff6a898314976a928c8672a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-12-09 15:26:08 +0100

    tweak(builtin): restore the default behavior for `ESC`

--- core/me-builtin.el
+++ core/me-builtin.el
@@ -100,9 +100,6 @@
   ;; Use UTF-16-LE in Windows, see: https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
   (set-selection-coding-system (if (+emacs-options-p 'os/win) 'utf-16-le 'utf-8))
   :config
-  ;; Make `ESC' behave like `C-g'
-  (keymap-global-set "<escape>" #'keyboard-escape-quit)
-
   ;; Unbind some annoying commands
   (keymap-global-unset "C-z" 'remove)
   (keymap-global-unset "C-x C-z" 'remove)
