e658569c30f613b55b6ea6ab0f241989b3d0b140
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-01-30 13:56:00 +0100

    tweak(llm-ollama): remove server management, should be done externally

--- modules/me-ai.el
+++ modules/me-ai.el
@@ -16,40 +16,17 @@
 ;; `llm' module for integrating with Ollama
 (use-package llm-ollama
   :straight llm
-  :autoload make-llm-ollama +ollama-serve +ollama-kill-server +ollama-list-installed-models
+  :autoload make-llm-ollama +ollama-list-installed-models
   :config
-  (defvar +ollama-process-name "ellama-server")
-  (defvar +ollama-server-buffer-name " *ellama-server*")
-
-  (defun +ollama-serve ()
-    "Start Ellama server."
-    (interactive)
-    (if (executable-find "ollama")
-        (if (get-process +ollama-process-name)
-            (message "The Ollama server is already running, call `+ollama-kill-server' to stop it.")
-          (if (make-process :name +ollama-process-name :buffer +ollama-server-buffer-name :command '("ollama" "serve"))
-              (message "Successfully started Ollama server.")
-            (user-error "Cannot start the Ollama server"))
-          (with-eval-after-load 'ellama (+ellama-set-available-providers)))
-      (user-error "Cannot find the \"ollama\" executable")))
-
-  (defun +ollama-kill-server ()
-    "Kill Ellama server."
-    (interactive)
-    (let ((ollama (get-process +ollama-process-name)))
-      (if ollama
-          (if (kill-process ollama)
-              (message "Killed Ollama server.")
-            (user-error "Cannot kill the Ollama server"))
-        (message "No running Ollama server."))))
-
   (defun +ollama-list-installed-models ()
     "Return the installed models"
-    (let* ((ret (shell-command-to-string "ollama list"))
-           (models (cdr (string-lines ret))))
-      (if (and (string-match-p "NAME[[:space:]]*ID[[:space:]]*SIZE[[:space:]]*MODIFIED" ret) (length> models 0))
-          (mapcar (lambda (m) (car (string-split m))) models)
-        (user-error "Cannot detect installed models, please make sure Ollama server is started")))))
+    (if (zerop (call-process-shell-command "ollama ps"))
+        (let* ((ret (shell-command-to-string "ollama list"))
+               (models (cdr (string-lines ret))))
+          (if (and (string-match-p "NAME[[:space:]]*ID[[:space:]]*SIZE[[:space:]]*MODIFIED" ret) (length> models 0))
+              (mapcar (lambda (m) (car (string-split m))) models)
+            (user-error "No model available, please pull some Ollama model")))
+      (user-error "Please make sure Ollama server is started"))))
 
 
 ;; A package for interacting with LLMs from Emacs
