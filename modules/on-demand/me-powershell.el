8dcc5f3d6f4b20e3b74e0f9596892988292a8f60
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-04-17 23:17:09 +0200

    tweak(on-demand/powershell): revert the install dir fix, merged :upstream
    Partially reverts aa1c991a490a29b1233342d7de90f152db449a83

--- modules/on-demand/me-powershell.el
+++ modules/on-demand/me-powershell.el
@@ -17,23 +17,7 @@
 (use-package powershell
   :straight t
   :custom
-  (powershell-default-langserver-path (expand-file-name "powershell/" minemacs-local-dir))
-  :config
-  ;; TEMP+BUG: Temporary fix until jschaf/powershell.el#44 gets merged
-  (advice-add
-   'powershell--download-langserver
-   :override
-   (satch-defun +powershell--download-langserver:override-a ()
-     (let* ((download-dir  (expand-file-name "dl" powershell-default-langserver-path))
-            (download-file (expand-file-name "powershell-langserver.zip" download-dir)))
-       (make-directory download-dir :parents)
-       (let* ((version     (powershell--get-latest-release-version))
-              (url         (format "https://github.com/PowerShell/PowerShellEditorServices/releases/download/%s/PowerShellEditorServices.zip" version)))
-         (url-copy-file url download-file 't)
-         (powershell--unzip-file download-file powershell-default-langserver-path)
-         (delete-directory download-dir t)
-         ;; make our function respond with something more interesting than nil :)
-         (message (format "Powershell LangServer version %s downloaded and unpacked to \'%s\'" version powershell-default-langserver-path)))))))
+  (powershell-default-langserver-path (expand-file-name "powershell/" minemacs-local-dir)))
 
 
 (provide 'on-demand/me-powershell)
