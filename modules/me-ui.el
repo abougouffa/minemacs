661de0d9a4f66cfec354b108e0b56d7298628c54
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-04-19 17:57:49 +0200

    tweak(casual): bind `casual-make-tmenu`

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -144,7 +144,8 @@
   :bind (:package re-builder :map reb-mode-map ([f2] . casual-re-builder-tmenu))
   :bind (:package bookmark :map bookmark-bmenu-mode-map ([f2] . casual-bookmarks-tmenu))
   :bind (:package image-mode :map image-mode-map ([f2] . casual-image-tmenu))
-  :bind (:package org-agenda :map org-agenda-mode-map ([f2] . casual-agenda-tmenu)))
+  :bind (:package org-agenda :map org-agenda-mode-map ([f2] . casual-agenda-tmenu))
+  :bind (:package make-mode :map makefile-mode-map ([f2] . casual-make-tmenu)))
 
 
 ;; An opinionated `transient' menu for `avy'
