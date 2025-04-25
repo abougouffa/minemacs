b9521ecb6aa4a0fbdae138177a0de33782226e4d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-04-25 18:06:05 +0200

    refactor(core): merge `minemacs-lazy` into `minemacs-loaded`

--- core/minemacs-loaded.el
+++ core/minemacs-loaded.el
@@ -6,9 +6,6 @@
 
 ;;; Commentary:
 
-;; This feature is loaded at end of init.el (after loading custom-vars.el), it
-;; is used to synchronize loading some other stuff after loading Emacs
-
 ;;; Code:
 
 ;; Run hooks
@@ -18,11 +15,37 @@
          (length minemacs-after-startup-hook))
   (run-hooks 'minemacs-after-startup-hook))
 
-(+load minemacs-core-dir "minemacs-lazy.el")
-
 (+log! "Providing `minemacs-loaded'.")
 
-
 (provide 'minemacs-loaded)
+
+(when minemacs-lazy-hook
+  (if minemacs-not-lazy-p
+      (progn ; If `minemacs-not-lazy-p' is true, force loading lazy hooks immediately
+        (+log! "Loading %d lazy packages immediately." (length minemacs-lazy-hook))
+        (run-hooks 'minemacs-lazy-hook)
+        (provide 'minemacs-lazy))
+    (+log! "Loading %d lazy packages incrementally." (length minemacs-lazy-hook))
+    (cl-callf append minemacs--lazy-high-priority-forms
+      (mapcar #'ensure-list minemacs-lazy-hook)
+      '((provide 'minemacs-lazy))))) ;; Provide `minemacs-lazy' at the end
+
+(defvar minemacs--lazy-high-priority-timer
+  (run-with-timer
+   0.1 0.001
+   (lambda ()
+     (if minemacs--lazy-high-priority-forms
+         (let ((inhibit-message (not minemacs-verbose-p)))
+           (eval (pop minemacs--lazy-high-priority-forms)))
+       (cancel-timer minemacs--lazy-high-priority-timer)))))
+
+(defvar minemacs--lazy-low-priority-timer
+  (run-with-timer
+   0.3 0.001
+   (lambda ()
+     (if minemacs--lazy-low-priority-forms
+         (let ((inhibit-message (not minemacs-verbose-p)))
+           (eval (pop minemacs--lazy-low-priority-forms)))
+       (cancel-timer minemacs--lazy-low-priority-timer)))))
 
 ;;; minemacs-loaded.el ends here
