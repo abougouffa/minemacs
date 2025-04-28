2b367c2d0fbaf2cfcfc68351551d03b6706cdfe5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-04-28 03:22:57 +0200

    chore: add `.dir-locals.el`

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((project-vc-name . "MinEmacs")))
+ (emacs-lisp-mode . ((time-stamp-pattern . "^;; Last modified: %%$")
+                     (time-stamp-format . "%04Y-%02m-%02d")
+                     (eval . (and (fboundp 'apheleia-mode) (apheleia-mode 1))))))
