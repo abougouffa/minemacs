fdac44bbad55b4ee252af2c05e3772b697459fca
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-05-08 14:12:12 +0200

    fix(webkit): disable and don't build the module on Windows

--- modules/me-experimental.el
+++ modules/me-experimental.el
@@ -40,14 +40,18 @@
 
 ;; A Dynamic Module for WebKit, aka a fully fledged browser inside Emacs
 (use-package webkit
-  :straight (:host github :repo "akirakyle/emacs-webkit" :files (:defaults "*.js" "*.css" "*.so") :pre-build ("make"))
+  :straight `( :host github :repo "akirakyle/emacs-webkit"
+               :files (:defaults "*.js" "*.css" "*.so")
+               :pre-build ,(if (featurep 'os/win)
+                               '(message "The `webkit' module won't build on Windows.")
+                             '("make")))
   :when (and (featurep 'feat/modules) (not (featurep 'os/win))))
 
 
 ;; A Dynamic Module for WebKit, aka a fully fledged browser inside Emacs
 (use-package webkit-dark
   :straight webkit
-  :when (featurep 'feat/modules)
+  :when (and (featurep 'feat/modules) (not (featurep 'os/win)))
   :bind (:map webkit-mode-map ("C-c d" . webkit-dark-toggle)))
 
 
