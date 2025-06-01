68085f42e9c4d5317cdc6a5ebf7b081e6cd2fbb9
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-01 22:57:42 +0200

    refactor(reformatter): make use of `+clang-format--get-lang`

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -156,39 +156,27 @@
   :config
   (reformatter-define ref-black
     :program "black"
-    :args (append
-           (when (equal "pyi" (and buffer-file-name (file-name-extension buffer-file-name)))
-             '("--pyi"))
-           '("-"))
+    :args `(,@(when (equal "pyi" (and buffer-file-name (file-name-extension buffer-file-name)))
+                '("--pyi"))
+            "-")
     :lighter "Black ")
 
   (reformatter-define ref-lua-format
     :program "lua-format"
-    :args (append
-           (when-let* ((indent (cond ((derived-mode-p 'lua-ts-mode) lua-ts-indent-offset)
-                                     ((derived-mode-p 'lua-mode) lua-indent-level))))
-             (list (format "--indent-width=%d")))
-           (unless indent-tabs-mode
-             '("--no-use-tab")))
+    :args `(,@(when-let* ((indent (cond ((derived-mode-p 'lua-ts-mode) lua-ts-indent-offset)
+                                        ((derived-mode-p 'lua-mode) lua-indent-level))))
+                `(,(format "--indent-width=%d" indent)))
+            ,@(unless indent-tabs-mode
+                '("--no-use-tab")))
     :lighter "LuaFmt ")
 
   (reformatter-define ref-clang-format
     :program +clang-format-command
-    :args (list
-           (+clang-format-get-style)
-           "-assume-filename"
-           (or (buffer-file-name)
-               (alist-get major-mode
-                          '(((c-mode c-ts-mode)               . ".c")
-                            ((csharp-mode csharp-ts-mode)     . ".cs")
-                            ((c++-mode c++-ts-mode)           . ".cpp")
-                            ((cuda-mode cuda-ts-mode)         . ".cu")
-                            ((glsl-mode glsl-ts-mode)         . ".glsl")
-                            ((java-mode java-ts-mode)         . ".java")
-                            ((objc-mode objc-ts-mode)         . ".m")
-                            ((protobuf-mode protobuf-ts-mode) . ".proto"))
-                          ".c" nil
-                          (+reverse-args #'memq))))
+    :args `(,(+clang-format-get-style)
+            "-assume-filename"
+            ,(or (when-let* ((file (buffer-file-name))) (file-name-nondirectory file))
+                 (when-let* ((ext (car (+clang-format--get-lang)))) (file-name-with-extension "file" ext))
+                 "file.c"))
     :lighter "ClangFmt "))
 
 
