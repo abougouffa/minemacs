afa8a9a67f1372b350a90861f133636432eeac6d
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-02 11:50:25 +0200

    feat(on-demand/ocaml): make unused `dune` obsolete, too big repo

--- modules/on-demand/me-ocaml.el
+++ modules/on-demand/me-ocaml.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-08-11
-;; Last modified: 2025-03-21
+;; Last modified: 2025-06-02
 
 ;;; Commentary:
 
@@ -13,8 +13,7 @@
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-ocaml
   :auto-mode '(("\\.mly\\'" . tuareg-menhir-mode)
-               (("\\.eliomi?\\'" "\\.ml[ip]?\\'") . tuareg-mode)
-               ("\\(?:\\`\\|/\\)dune\\(?:\\.inc\\|\\-project\\|\\-workspace\\)?\\'" . dune-mode))
+               (("\\.eliomi?\\'" "\\.ml[ip]?\\'") . tuareg-mode))
   :interpreter-mode '(("ocamlrun" . tuareg-mode) ("ocaml" . tuareg-mode)))
 
 
@@ -26,11 +25,6 @@
 ;; Select OCaml opam switches via a menu
 (use-package opam-switch-mode
   :straight t)
-
-
-;; Integration with the dune build system
-(use-package dune
-  :straight (:host github :repo "ocaml/dune" :depth 1 :files ("editor-integration/emacs/*.el")))
 
 
 ;; Universal toplevel for OCaml
