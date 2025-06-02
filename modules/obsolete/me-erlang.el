ce6c90e7cda4f8170dbf9d3cf0bbe1a72d4a956e
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-02 11:28:59 +0200

    feat(on-demand): make unused `erlang` obsolete, too big repo

---
+++ modules/obsolete/me-erlang.el
@@ -0,0 +1,20 @@
+;;; me-erlang.el --- Erlang -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+;; Created: 2025-06-02
+;; Last modified: 2025-06-02
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; Major modes for editing and running Erlang files
+(use-package erlang
+  :straight t)
+
+
+(provide 'obsolete/me-erlang)
+;;; me-erlang.el ends here
