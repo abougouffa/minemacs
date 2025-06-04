c3e2e92b08dc4e007bb2933a13e9685e54396bc4
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-04 11:36:09 +0200

    docs: fix email address

--- modules/on-demand/me-bazel.el
+++ modules/on-demand/me-bazel.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2022-2025  Abdelhak Bougouffa
 
-;; Author: Abdelhak Bougouffa  (rot13 "noqryunx.obhtbhssn@cneebg.pbz")
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2025-06-04
 ;; Last modified: 2025-06-04
 
