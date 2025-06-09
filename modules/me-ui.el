75e7a9c000521d364a8a7d23d7e4eb25dd427487
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-09 14:39:18 +0200

    tweak(casual): bind new `casual-calendar` and unify the binding to `C-o`

--- modules/me-ui.el
+++ modules/me-ui.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2022-09-20
-;; Last modified: 2025-05-30
+;; Last modified: 2025-06-09
 
 ;;; Commentary:
 
@@ -133,16 +133,17 @@
 (use-package casual
   :straight (:host github :repo "kickingvegas/casual")
   :bind ("C-o" . casual-editkit-main-tmenu)
-  :bind (:package isearch :map isearch-mode-map ([f2] . casual-isearch-tmenu))
-  :bind (:package dired :map dired-mode-map ([f2] . casual-dired-tmenu))
-  :bind (:package info :map Info-mode-map ([f2] . casual-info-tmenu))
-  :bind (:package calc :map calc-mode-map ([f2] . casual-calc-tmenu))
-  :bind (:package ibuffer :map ibuffer-mode-map ([f2] . casual-ibuffer-tmenu))
-  :bind (:package re-builder :map reb-mode-map ([f2] . casual-re-builder-tmenu))
-  :bind (:package bookmark :map bookmark-bmenu-mode-map ([f2] . casual-bookmarks-tmenu))
-  :bind (:package image-mode :map image-mode-map ([f2] . casual-image-tmenu))
-  :bind (:package org-agenda :map org-agenda-mode-map ([f2] . casual-agenda-tmenu))
-  :bind (:package make-mode :map makefile-mode-map ([f2] . casual-make-tmenu)))
+  :bind (:package bookmark :map bookmark-bmenu-mode-map ("C-o" . casual-bookmarks-tmenu))
+  :bind (:package calc :map calc-mode-map ("C-o" . casual-calc-tmenu))
+  :bind (:package calendar :map calendar-mode-map ("C-o" . casual-calendar))
+  :bind (:package dired :map dired-mode-map ("C-o" . casual-dired-tmenu))
+  :bind (:package ibuffer :map ibuffer-mode-map ("C-o" . casual-ibuffer-tmenu))
+  :bind (:package image-mode :map image-mode-map ("C-o" . casual-image-tmenu))
+  :bind (:package info :map Info-mode-map ("C-o" . casual-info-tmenu))
+  :bind (:package isearch :map isearch-mode-map ("C-o" . casual-isearch-tmenu))
+  :bind (:package make-mode :map makefile-mode-map ("C-o" . casual-make-tmenu))
+  :bind (:package org-agenda :map org-agenda-mode-map ("C-o" . casual-agenda-tmenu))
+  :bind (:package re-builder :map reb-mode-map ("C-o" . casual-re-builder-tmenu)))
 
 
 ;; An opinionated `transient' menu for `avy'
