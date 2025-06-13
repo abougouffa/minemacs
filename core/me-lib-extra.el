302bff351618469eea3274bfc2a21aeebe19bd9b
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-13 02:00:55 +0200

    fix(clang-format): handle the unlimited column limit case (ColumnLimit=0)

--- core/me-lib-extra.el
+++ core/me-lib-extra.el
@@ -924,15 +924,15 @@ When NO-OPT isn non-nil, don't return the \"-style=\" part."
                                  (concat "--assume-filename=dummy." (car lang)) "--dump-config"))
                      (buffer-string))))
             (yaml-hash (yaml-parse-string out))
-            (cl (gethash 'ColumnLimit yaml-hash))
+            (fc (gethash 'ColumnLimit yaml-hash))
             (iw (gethash 'IndentWidth yaml-hash))
             (tw (gethash 'TabWidth yaml-hash))
             (is (if (equal (gethash 'UseTab yaml-hash) "Never") "space" "tab")))
       (progn
-        (editorconfig-set-line-length (number-to-string cl))
+        (setq fill-column (if (zerop fc) nil fc)) ; `editorconfig-set-line-length' doesn't handle the unlimited case
         (editorconfig-set-indentation is (number-to-string iw) (number-to-string tw))
         (when (called-interactively-p 'interactive)
-          (message "Set fill-column=%s, tab-width=%s, indent-offset=%s and indent-style=%s" cl tw iw is)))
+          (message "Set fill-column=%s, tab-width=%s, indent-offset=%s and indent-style=%s" fc tw iw is)))
     (when (called-interactively-p 'interactive)
       (user-error "No applicable \".clang-format\" for buffer %S" (buffer-name)))))
 
