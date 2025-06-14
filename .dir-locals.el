14f2e3d02fff95f3b53d1513becd238988f907d4
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-14 21:53:01 +0200

    chore: auto update the copyright date on save

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,5 +4,7 @@
 ((nil . ((project-vc-name . "MinEmacs")))
  (emacs-lisp-mode . ((time-stamp-pattern . "^;; Last modified: %%$")
                      (time-stamp-format . "%04Y-%02m-%02d")
-                     (eval . (and (fboundp 'apheleia-mode) (apheleia-mode 1)))))
+                     (eval . (and (fboundp 'apheleia-mode) (apheleia-mode 1)))
+                     (copyright-query . nil)
+                     (eval . (add-hook 'before-save-hook #'copyright-update))))
  (text-mode . ((+git-commit-prefix-in-project . conventional))))
