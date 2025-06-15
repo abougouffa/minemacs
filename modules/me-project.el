fc0fef52f38c22bb841a489bda5c8c06dad95b3a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-15 14:57:06 +0200

    tweak(ffip): remove unneeded hack, support added upstream

--- modules/me-project.el
+++ modules/me-project.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2022-10-02
-;; Last modified: 2025-06-11
+;; Last modified: 2025-06-15
 
 ;;; Commentary:
 
@@ -96,20 +96,7 @@
 (use-package find-file-in-project
   :straight t
   :custom
-  (ffip-use-rust-fd (and (executable-find "fd") t))
-  :config
-  (require 'project) ; for `project--file-completion-table'
-  (advice-add ;; This adds `nerd-icons-completion-mode' support for `ffip'
-   'ffip-completing-read :override
-   (satch-defun +ffip-completing-read (prompt collection &optional action)
-     (when-let* ((selected
-                  (if (= 1 (length collection))
-                      (car collection)
-                    (let ((sel (completing-read prompt (project--file-completion-table collection))))
-                      (or (assoc sel collection) sel)))))
-       (let* ((default-directory (ffip-get-project-root-directory))
-              (result (if (consp selected) (cdr selected) selected)))
-         (if action (funcall action result) result))))))
+  (ffip-use-rust-fd (and (executable-find "fd") t)))
 
 
 (provide 'me-project)
