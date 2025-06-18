ade03bbdc4e3285df0ad1ce726cc14d847523ce2
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-06-19 00:33:48 +0200

    tweak(dash-docs): register more docsets

--- modules/me-experimental.el
+++ modules/me-experimental.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-12-11
-;; Last modified: 2025-06-18
+;; Last modified: 2025-06-19
 
 ;;; Commentary:
 
@@ -42,9 +42,12 @@
   (dash-docs-browser-func #'eww-browse-url)
   :init
   (+setq-hook! (sh-mode bash-ts-mode) dash-docs-docsets '("Bash"))
-  (+setq-hook! (python-mode python-ts-mode) dash-docs-docsets '("Python 3" "OpenCV Python" "NumPy" "SciPy"))
+  (+setq-hook! (python-mode python-ts-mode) dash-docs-docsets '("Python 3" "OpenCV Python" "NumPy" "SciPy" "PyTorch" "TensorFlow" "SymPy" "mypy"))
   (+setq-hook! (c-mode c-ts-mode) dash-docs-docsets '("C" "OpenCV" "OpenCV C"))
-  (+setq-hook! (c++-mode c++-ts-mode) dash-docs-docsets '("C++" "OpenCV" "OpenCV C++")))
+  (+setq-hook! (c++-mode c++-ts-mode) dash-docs-docsets '("C++" "OpenCV" "OpenCV C++"))
+  (+setq-hook! (json-mode json-ts-mode) dash-docs-docsets '("jq"))
+  (+setq-hook! cuda-mode dash-docs-docsets '("CUDA"))
+  (+setq-hook! opencl-c-mode dash-docs-docsets '("OpenCL")))
 
 
 ;; Integration of `consult' with `dash-docs'
