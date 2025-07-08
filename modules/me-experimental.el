ba03156dc2b7dfda49431fc76b7541c4b46ffbe1
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-07-08 08:47:32 +0200

    feat(experimental): make `webkit` obsolete

--- modules/me-experimental.el
+++ modules/me-experimental.el
@@ -11,23 +11,6 @@
 ;;; Code:
 
 
-;; A Dynamic Module for WebKit, aka a fully fledged browser inside Emacs
-(use-package webkit
-  :straight `( :host github :repo "akirakyle/emacs-webkit"
-               :files (:defaults "*.js" "*.css" "*.so")
-               :pre-build ,(if (featurep 'os/win)
-                               '(message "The `webkit' module won't build on Windows.")
-                             '("make")))
-  :when (and (featurep 'feat/modules) (not (featurep 'os/win))))
-
-
-;; A Dynamic Module for WebKit, aka a fully fledged browser inside Emacs
-(use-package webkit-dark
-  :straight webkit
-  :when (and (featurep 'feat/modules) (not (featurep 'os/win)))
-  :bind (:map webkit-mode-map ("C-c d" . webkit-dark-toggle)))
-
-
 ;; Offline documentation browser using Dash/Zeal docsets
 (use-package dash-docs
   :straight (:host github :repo "abougouffa/dash-docs")
