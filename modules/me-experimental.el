9b8b9520f551c36ff5c88d040d094de9fe8c69d8
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-07-08 08:48:34 +0200

    feat(prog): move `dash-docs` and `consult-dash` to `me-prog`

--- modules/me-experimental.el
+++ modules/me-experimental.el
@@ -11,29 +11,6 @@
 ;;; Code:
 
 
-;; Offline documentation browser using Dash/Zeal docsets
-(use-package dash-docs
-  :straight (:host github :repo "abougouffa/dash-docs")
-  :custom
-  (dash-docs-docsets-path (concat minemacs-local-dir "docsets/"))
-  (dash-docs-browser-func #'eww-browse-url)
-  :init
-  (+setq-hook! (sh-mode bash-ts-mode) dash-docs-docsets '("Bash"))
-  (+setq-hook! (python-mode python-ts-mode) dash-docs-docsets '("Python 3" "OpenCV Python" "NumPy" "SciPy" "PyTorch" "TensorFlow" "SymPy" "mypy"))
-  (+setq-hook! (c-mode c-ts-mode) dash-docs-docsets '("C" "OpenCV" "OpenCV C"))
-  (+setq-hook! (c++-mode c++-ts-mode) dash-docs-docsets '("C++" "OpenCV" "OpenCV C++"))
-  (+setq-hook! (json-mode json-ts-mode) dash-docs-docsets '("jq"))
-  (+setq-hook! cuda-mode dash-docs-docsets '("CUDA"))
-  (+setq-hook! opencl-c-mode dash-docs-docsets '("OpenCL")))
-
-
-;; Integration of `consult' with `dash-docs'
-(use-package consult-dash
-  :straight t
-  :config
-  (with-eval-after-load 'consult
-    (consult-customize consult-dash :initial (or (thing-at-point 'region t) (thing-at-point 'symbol t)))))
-
 
 (provide 'me-experimental)
 ;;; me-experimental.el ends here
--- modules/me-prog.el
+++ modules/me-prog.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2022-09-17
-;; Last modified: 2025-07-07
+;; Last modified: 2025-07-08
 
 ;;; Commentary:
 
@@ -277,6 +277,31 @@
 (use-package devdocs
   :straight t
   :when (featurep 'feat/libxml2))
+
+
+;; Offline documentation browser using Dash/Zeal docsets
+(use-package dash-docs
+  :straight (:host github :repo "abougouffa/dash-docs")
+  :custom
+  (dash-docs-docsets-path (concat minemacs-local-dir "docsets/"))
+  (dash-docs-browser-func #'eww-browse-url)
+  :init
+  (+setq-hook! (sh-mode bash-ts-mode) dash-docs-docsets '("Bash"))
+  (+setq-hook! (python-mode python-ts-mode) dash-docs-docsets '("Python 3" "OpenCV Python" "NumPy" "SciPy" "PyTorch" "TensorFlow" "SymPy" "mypy"))
+  (+setq-hook! (c-mode c-ts-mode) dash-docs-docsets '("C" "OpenCV" "OpenCV C"))
+  (+setq-hook! (c++-mode c++-ts-mode) dash-docs-docsets '("C++" "OpenCV" "OpenCV C++"))
+  (+setq-hook! (json-mode json-ts-mode) dash-docs-docsets '("jq"))
+  (+setq-hook! cuda-mode dash-docs-docsets '("CUDA"))
+  (+setq-hook! opencl-c-mode dash-docs-docsets '("OpenCL")))
+
+
+;; Integration of `consult' with `dash-docs'
+(use-package consult-dash
+  :straight t
+  :unless (+package-disabled-p 'consult 'me-completion)
+  :config
+  (with-eval-after-load 'consult
+    (consult-customize consult-dash :initial (or (thing-at-point 'region t) (thing-at-point 'symbol t)))))
 
 
 ;; Coccinelle: Complex style-preserving source-to-source transformations
