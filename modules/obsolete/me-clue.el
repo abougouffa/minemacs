bd15756528104213f04c222660e75a3fb907627a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-07-09 22:31:43 +0200

    feat(notes): make unused `clue` obsolete

---
+++ modules/obsolete/me-clue.el
@@ -0,0 +1,24 @@
+;;; me-clue.el --- Description -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
+;; Created: 2025-07-09
+;; Last modified: 2025-07-09
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; Connecting clues (a.k.a., note taking) while reading code
+(use-package clue
+  :straight (:host github :repo "AmaiKinono/clue")
+  :custom
+  (clue-project-root-function
+   (if (fboundp '+citre-dominating-project-root) #'+citre-dominating-project-root #'clue-project-root))
+  :hook (find-file . clue-auto-enable-clue-mode))
+
+
+(provide 'obsolete/me-clue)
+;;; me-clue.el ends here
