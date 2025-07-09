58cc571c4d8e2f7f141dfed90e257c9d1a9bee44
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-07-09 23:52:52 +0200

    feat(prog): make `reformatter` obsolete

--- modules/me-prog.el
+++ modules/me-prog.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2022-09-17
-;; Last modified: 2025-07-08
+;; Last modified: 2025-07-09
 
 ;;; Commentary:
 
@@ -164,36 +164,6 @@
   ;; append the "-style" option
   (let ((clang (assq 'clang-format apheleia-formatters)))
     (setcdr clang (cons +clang-format-command (append (cddr clang) '((+clang-format-get-style)))))))
-
-
-;; Define commands which run reformatters on the current Emacs buffer
-(use-package reformatter
-  :straight t
-  :config
-  (reformatter-define ref-black
-    :program "black"
-    :args `(,@(when (equal "pyi" (and buffer-file-name (file-name-extension buffer-file-name)))
-                '("--pyi"))
-            "-")
-    :lighter "Black ")
-
-  (reformatter-define ref-lua-format
-    :program "lua-format"
-    :args `(,@(when-let* ((indent (cond ((derived-mode-p 'lua-ts-mode) lua-ts-indent-offset)
-                                        ((derived-mode-p 'lua-mode) lua-indent-level))))
-                `(,(format "--indent-width=%d" indent)))
-            ,@(unless indent-tabs-mode
-                '("--no-use-tab")))
-    :lighter "LuaFmt ")
-
-  (reformatter-define ref-clang-format
-    :program +clang-format-command
-    :args `(,(+clang-format-get-style)
-            "-assume-filename"
-            ,(or (when-let* ((file (buffer-file-name))) (file-name-nondirectory file))
-                 (when-let* ((ext (car (+clang-format-get-lang)))) (file-name-with-extension "dummy" ext))
-                 "dummy.c"))
-    :lighter "ClangFmt "))
 
 
 ;; Out of the box code execution from editing buffer
---
+++ modules/obsolete/me-reformatter.el
@@ -0,0 +1,45 @@
+;;; me-reformatter.el --- Description -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "noqryunx.obhtbhssn@tznvy.pbz")
+;; Created: 2025-07-09
+;; Last modified: 2025-07-09
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; Define commands which run reformatters on the current Emacs buffer
+(use-package reformatter
+  :straight t
+  :config
+  (reformatter-define ref-black
+    :program "black"
+    :args `(,@(when (equal "pyi" (and buffer-file-name (file-name-extension buffer-file-name)))
+                '("--pyi"))
+            "-")
+    :lighter "Black ")
+
+  (reformatter-define ref-lua-format
+    :program "lua-format"
+    :args `(,@(when-let* ((indent (cond ((derived-mode-p 'lua-ts-mode) lua-ts-indent-offset)
+                                        ((derived-mode-p 'lua-mode) lua-indent-level))))
+                `(,(format "--indent-width=%d" indent)))
+            ,@(unless indent-tabs-mode
+                '("--no-use-tab")))
+    :lighter "LuaFmt ")
+
+  (reformatter-define ref-clang-format
+    :program +clang-format-command
+    :args `(,(+clang-format-get-style)
+            "-assume-filename"
+            ,(or (when-let* ((file (buffer-file-name))) (file-name-nondirectory file))
+                 (when-let* ((ext (car (+clang-format-get-lang)))) (file-name-with-extension "dummy" ext))
+                 "dummy.c"))
+    :lighter "ClangFmt "))
+
+
+(provide 'obsolete/me-reformatter)
+;;; me-reformatter.el ends here
