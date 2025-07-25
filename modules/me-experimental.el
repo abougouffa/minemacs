68b6ce315f9e81ea0c600ea0f30514deb6d1a3a5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-07-25 17:21:13 +0200

    feat(experimental): add `kmacro-x`, maybe replace `multiple-cursors`

--- modules/me-experimental.el
+++ modules/me-experimental.el
@@ -4,12 +4,21 @@
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-12-11
-;; Last modified: 2025-07-08
+;; Last modified: 2025-07-25
 
 ;;; Commentary:
 
 ;;; Code:
 
+
+;; A collection of commands, modes and functions built on top of the keyboard macros (kmacros)
+(use-package kmacro-x
+  :straight t
+  :hook (minemacs-first-file . kmacro-x-atomic-undo-mode)
+  ;; :bind (("C-<" . kmacro-x-mc-mark-previous)
+  ;;        ("C->" . kmacro-x-mc-mark-next))
+  :custom
+  (kmacro-x-mc-live-preview t))
 
 
 (provide 'me-experimental)
