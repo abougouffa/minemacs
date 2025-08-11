de38a8df0a9fdef7b99f0a5a94291a4baff40b0f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-08-11 12:08:48 +0200

    feat(on-demand): add support for C3

---
+++ modules/on-demand/me-c3.el
@@ -0,0 +1,26 @@
+;;; me-c3.el --- C3 language -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa  (rot13 "noqryunx.obhtbhssn@cneebg.pbz")
+;; Created: 2025-08-11
+;; Last modified: 2025-08-11
+
+;;; Commentary:
+
+;;; Code:
+
+;;;###autoload
+(minemacs-register-on-demand-module 'me-c3
+  :auto-mode '(("\\.c3[it]?\\'" . c3-ts-mode)))
+
+
+(use-package c3-ts-mode
+  :straight (:host github :repo "c3lang/c3-ts-mode")
+  :config
+  (add-to-list 'treesit-language-source-alist '(c3 "https://github.com/c3lang/tree-sitter-c3"))
+  (treesit-ensure-installed 'c3))
+
+
+(provide 'on-demand/me-c3)
+;;; me-c3.el ends here
