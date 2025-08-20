27eff87c69f0f2f543723678a1dd0b71417f69d4
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-08-20 15:18:34 +0200

    tweak(smerge): recenter by default on navigation commands

--- core/extras/me-smerge-x.el
+++ core/extras/me-smerge-x.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2025-07-05
-;; Last modified: 2025-07-05
+;; Last modified: 2025-08-20
 
 ;;; Commentary:
 
@@ -24,25 +24,11 @@
   (goto-char (point-max))
   (smerge-prev))
 
-(defun +smerge-vc-next-conflict-recenter ()
-  "Like `smerge-vc-next-conflict' but recenters the buffer."
-  (interactive)
-  (smerge-vc-next-conflict)
-  ;; Often, after calling `smerge-vc-next-conflict', the cursor will land at
-  ;; the bottom of the window.
-  (recenter-top-bottom (/ (window-height) 8)))
-
-(defun +smerge-next-recenter ()
-  "Like `smerge-next' but recenters the buffer."
-  (interactive)
-  (smerge-next)
-  (recenter-top-bottom (/ (window-height) 8)))
-
-(defun +smerge-prev-recenter ()
-  "Like `smerge-prev' but recenters the buffer."
-  (interactive)
-  (smerge-prev)
-  (recenter-top-bottom (/ (window-height) 8)))
+;; Often, after calling `smerge-next', the cursor will land at the bottom of the
+;; window.
+(defun +recenter:after-a (&rest _args) (recenter-top-bottom (/ (window-height) 8)))
+(dolist (cmd '(smerge-next smerge-prev smerge-vc-next-conflict))
+  (advice-add cmd :after #'+recenter:after-a))
 
 
 (provide 'me-smerge-x)
