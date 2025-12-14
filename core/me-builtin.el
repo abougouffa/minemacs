9c181d09a88436ada0463852307090471e943015
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-12-14 23:23:38 +0100

    tweak(hideshow): remove obsolete option

--- core/me-builtin.el
+++ core/me-builtin.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2023-03-26
-;; Last modified: 2025-12-10
+;; Last modified: 2025-12-14
 
 ;;; Commentary:
 
@@ -472,27 +472,7 @@
     (interactive)
     (if (cl-some (+apply-partially-right #'overlay-get 'hs) (overlays-in (point-min) (point-max)))
         (hs-show-all)
-      (hs-hide-all)))
-
-  ;; Add extra modes support
-  (cl-callf append hs-special-modes-alist
-    '((vimrc-mode "{{{" "}}}" "\"")
-      (ruby-mode "class\\|d\\(?:ef\\|o\\)\\|module\\|[[{]" "end\\|[]}]" "#\\|=begin" ruby-forward-sexp)
-      (ruby-ts-mode
-       "class\\|d\\(?:ef\\|o\\)\\|module\\|[[{]" "end\\|[]}]" "#\\|=begin" ruby-forward-sexp)
-      (matlab-mode "if\\|switch\\|case\\|otherwise\\|while\\|for\\|try\\|catch" "end" nil (lambda (_arg) (matlab-forward-sexp)))
-      (octave-mode "if\\|switch\\|case\\|otherwise\\|while\\|for\\|try\\|catch" "end" nil forward-sexp)
-      (nxml-mode "<!--\\|<[^/>]*[^/]>" "-->\\|</[^/>]*[^/]>" "<!--" sgml-skip-tag-forward nil)
-      (latex-mode
-       ;; LaTeX-find-matching-end needs to be inside the env
-       ("\\\\begin{[a-zA-Z*]+}\\(\\)" 1)
-       "\\\\end{[a-zA-Z*]+}"
-       "%"
-       (lambda (_arg)
-         ;; Don't fold whole document, that's useless
-         (unless (save-excursion (search-backward "\\begin{document}" (line-beginning-position) t))
-           (LaTeX-find-matching-end)))
-       nil))))
+      (hs-hide-all))))
 
 (use-package xref
   :hook (xref--xref-buffer-mode . minemacs-reduce-font-size)
