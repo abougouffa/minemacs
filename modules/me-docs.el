6b439357d0fba89a1cdcebab9f66bc4c2944926a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2025-12-17 16:13:44 +0100

    tweak(dash-docs): register some docsets for C/C++

--- modules/me-docs.el
+++ modules/me-docs.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2022-09-17
-;; Last modified: 2025-12-11
+;; Last modified: 2025-12-15
 
 ;;; Commentary:
 
@@ -64,8 +64,8 @@
   :init
   (+setq-hook! (sh-mode bash-ts-mode) dash-docs-docsets '("Bash"))
   (+setq-hook! (python-mode python-ts-mode) dash-docs-docsets '("Python 3" "OpenCV Python" "NumPy" "SciPy" "PyTorch" "TensorFlow" "SymPy" "mypy"))
-  (+setq-hook! (c-mode c-ts-mode) dash-docs-docsets '("C" "OpenCV" "OpenCV C"))
-  (+setq-hook! (c++-mode c++-ts-mode) dash-docs-docsets '("C++" "OpenCV" "OpenCV C++"))
+  (+setq-hook! (c-mode c-ts-mode) dash-docs-docsets '("C" "OpenCV" "OpenCV C" "GStreamer" "glibc"))
+  (+setq-hook! (c++-mode c++-ts-mode) dash-docs-docsets '("C++" "OpenCV" "OpenCV C++" "GStreamer" "gtkmm" "glibc"))
   (+setq-hook! (json-mode json-ts-mode) dash-docs-docsets '("jq"))
   (+setq-hook! cuda-mode dash-docs-docsets '("CUDA"))
   (+setq-hook! opencl-c-mode dash-docs-docsets '("OpenCL")))
