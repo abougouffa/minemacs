168300e532dcb7a2f9b661d18487d9a0941e1b19
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-01-26 22:42:20 +0100

    refactor(llm-ollama): remove unneeded custom functions

--- modules/me-ai.el
+++ modules/me-ai.el
@@ -18,32 +18,7 @@
 ;; `llm' module for integrating with Ollama
 (use-package llm-ollama
   :straight llm
-  :autoload make-llm-ollama +ollama-list-installed-models
-  :config
-  (defconst +ollama-embedding-models
-    '("paraphrase-multilingual" "granite-embedding" "nomic-embed-text" "mxbai-embed-large"
-      "qwen3-embedding" "bge-m3" "bge-large" "all-minilm" "embeddinggemma" "snowflake-arctic-embed"))
-  (defvar +ollama-prefer-embedding-model nil) ; See https://ollama.com/search?c=embedding
-
-  (defun +ollama-list-installed-models ()
-    "Return the installed models"
-    (if (zerop (call-process-shell-command "ollama ps"))
-        (let* ((ret (shell-command-to-string "ollama list"))
-               (models (cdr (string-lines ret))))
-          (if (and (string-match-p "NAME[[:space:]]*ID[[:space:]]*SIZE[[:space:]]*MODIFIED" ret) (length> models 0))
-              (mapcar (lambda (m) (car (string-split m))) models)
-            (user-error "No model available, please pull some Ollama model")))
-      (user-error "Please make sure Ollama server is started")))
-
-  (defun +ollama-list-installed-embedding-models ()
-    (seq-intersection (+ollama-list-installed-models) +ollama-embedding-models #'string-prefix-p))
-
-  (defun +ollama-get-default-embedding-model ()
-    (let ((embedding-models (+ollama-list-installed-embedding-models)))
-      (or (and +ollama-prefer-embedding-model
-               (cl-find +ollama-prefer-embedding-model embedding-models :test #'string-prefix-p))
-          (car embedding-models)
-          (car (+ollama-list-installed-models))))))
+  :autoload make-llm-ollama)
 
 
 ;; A package for interacting with LLMs from Emacs
@@ -57,7 +32,7 @@
   :straight (:host github :repo "MatthewZMD/aidermacs")
   :custom
   (aidermacs-default-model ; Get the first available model
-   (cl-loop for model in (+ollama-list-installed-models)
+   (cl-loop for model in (ellama-get-ollama-model-names)
             when (cl-member model '("devstral" "qwen2.5-coder" "deepseek-coder-v2" "command-r7b"
                                     "codellama" "codegemma" "codestral" "codegeex4"
                                     "starcoder2" "granite-code" "mistral-nemo"
