248bc2096f11a2c0a7ec0178b663de17ae05cdf5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-03-29 00:40:57 +0100

    feat(on-demand/ocaml): replace the whole stack with `neocaml` and `ocaml-eglot`

--- modules/on-demand/me-ocaml.el
+++ modules/on-demand/me-ocaml.el
@@ -1,10 +1,10 @@
 ;;; me-ocaml.el --- OCaml support -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+;; Copyright (C) 2022-2026  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-08-11
-;; Last modified: 2025-07-07
+;; Last modified: 2026-03-29
 
 ;;; Commentary:
 
@@ -12,46 +12,31 @@
 
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-ocaml
-  :auto-mode '(("\\.mly\\'" . tuareg-menhir-mode)
-               (("\\.eliomi?\\'" "\\.ml[ip]?\\'") . tuareg-mode)
-               ("\\(?:\\`\\|/\\)dune\\(?:\\.inc\\|\\-project\\|\\-workspace\\)?\\'" . dune-mode))
-  :interpreter-mode '(("ocamlrun" . tuareg-mode) ("ocaml" . tuareg-mode)))
+  :auto-mode `(("\\.mly\\'" . neocaml-menhir-mode)
+               ("\\.mll\\'" . neocaml-ocamllex-mode)
+               ("\\.ml\\'" . neocaml-mode)
+               ("\\.mli\\'" . neocaml-interface-mode)
+               ("[./]opam\\'" . neocaml-opam-mode)
+               ("/dune\\(-project\\|-workspace\\)\\'" . neocaml-dune-mode)
+               (,(rx "." (or "cmi" "cmo" "cmx" "cma" "cmxa" "cmxs" "cmt" "cmti") eol) . neocaml-objinfo-mode))
+  :interpreter-mode '(("ocamlrun" . neocaml-mode) ("ocaml" . neocaml-mode)))
 
 
-;; Major mode and REPL for the OCaml programming language
-(use-package tuareg
-  :straight t)
+;;  A modern, TreeSitter-powered, Emacs major mode for OCaml
+(use-package neocaml
+  :straight (:host github :repo "bbatsov/neocaml")
+  :config
+  ;; Register neocaml modes with Eglot
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs '((neocaml-mode neocaml-interface-mode) . ("ocamllsp")))))
 
 
-;; Select OCaml opam switches via a menu
-(use-package opam-switch-mode
-  :straight t)
-
-
-(defconst +dune-path (concat minemacs-on-demand-modules-dir "third-party/dune/"))
-
-;; Integration with the dune build system
-(use-package dune
-  :load-path +dune-path
-  :commands (dune-mode dune-promote dune-runtest-and-promote)
-  :mode ("\\(?:\\`\\|/\\)dune\\(?:\\.inc\\|\\-project\\|\\-workspace\\)?\\'" . dune-mode))
-
-
-;; Integration with dune --watch tasks
-(use-package dune-watch
-  :after dune
-  :demand)
-
-
-;; Integrate `dune' with `flymake'
-(use-package dune-flymake
-  :after dune
-  :demand)
-
-
-;; Universal toplevel for OCaml
-(use-package utop
-  :straight t)
+;; An overlay on Eglot for editing OCaml code using LSP
+(use-package ocaml-eglot
+  :straight (:host github :repo "tarides/ocaml-eglot")
+  :hook
+  (neocaml-base-mode . ocaml-eglot)
+  (ocaml-eglot . eglot-ensure))
 
 
 (provide 'on-demand/me-ocaml)
