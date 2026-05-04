615305275ae8ba295eabd23f643a380ad1e9dc21
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-05-04 17:01:42 +0200

    tweak(on-demand/pcap): register the `aps` and `cscpkt` extensions

--- modules/on-demand/me-pcap.el
+++ modules/on-demand/me-pcap.el
@@ -1,10 +1,10 @@
 ;;; me-pcap.el --- PCAP support -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+;; Copyright (C) 2022-2026  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-08-10
-;; Last modified: 2025-03-21
+;; Last modified: 2026-05-04
 
 ;;; Commentary:
 
@@ -12,7 +12,7 @@
 
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-pcap
-  :auto-mode '(("\\.\\(?:5vw\\|apc\\|bfr\\|cap\\|dmp\\|eth\\|fdc\\|mplog\\|n\\(?:cf\\|tar\\)\\|p\\(?:cap\\(?:ng\\)?\\|kt\\)\\|rf5\\|s\\(?:noop\\|yc\\)\\|t\\(?:pc\\|r\\(?:c[01]\\|[1c]\\)\\)\\|vn\\(?:tc\\)?\\|wpz\\)$" . pcap-mode)))
+  :auto-mode '(("\\.\\(?:5vw\\|ap[cs]\\|bfr\\|c\\(?:ap\\|scpkt\\)\\|dmp\\|eth\\|fdc\\|mplog\\|n\\(?:cf\\|tar\\)\\|p\\(?:cap\\(?:ng\\)?\\|kt\\)\\|rf5\\|s\\(?:noop\\|yc\\)\\|t\\(?:pc\\|r\\(?:c[01]\\|[1c]\\)\\)\\|vn\\(?:tc\\)?\\|wpz\\)$" . pcap-mode)))
 
 
 ;; Major mode for working with PCAP files via Wireshark's `tshark' tool
@@ -21,7 +21,7 @@
   ;; An extensive list of extensions can be found at: https://tshark.dev/formats/magic_numbers
   :mode (rx "." (or "pcap" "pcapng" "ntar" "5vw" "ncf" "tr1" "trc" "trc0" "trc1"
                     "cap" "fdc" "syc" "bfr" "dmp" "rf5" "snoop" "eth" "pkt"
-                    "tpc" "apc" "wpz" "vntc" "vn" "mplog")
+                    "tpc" "apc" "wpz" "vntc" "vn" "mplog" "aps" "cscpkt")
             eol)
   :bind (:map pcap-mode-map ("n" . next-line) ("p" . previous-line))
   :config
