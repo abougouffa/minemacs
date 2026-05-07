e60f555206782dccc260aa0c3eaf05031d2becef
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-05-08 00:50:12 +0200

    tweak: remove some unused stuff

--- core/me-lib-x.el
+++ core/me-lib-x.el
@@ -1248,13 +1248,6 @@ This command removes new line characters between lines."
 ;;; Project tweaks
 
 ;;;###autoload
-(defun +project-gdb ()
-  "Invoke `gdb' in the project's root."
-  (interactive)
-  (let ((default-directory (project-root (project-current t))))
-    (call-interactively #'gdb)))
-
-;;;###autoload
 (defun +project-list-cleanup ()
   "Forget all duplicate known projects (like /home/user/proj and ~/proj)."
   (interactive)
@@ -1268,21 +1261,6 @@ This command removes new line characters between lines."
           (+shutup!
            (project-forget-project proj)
            (project-remember-project (project-current nil proj-abbrev))))))))
-
-;;;###autoload
-(defvar +project-root-wildcards '("~/Projects/*/*"))
-
-;; Inspired by: https://mastodon.catgirl.cloud/@mekeor/113251381004866734
-;;;###autoload
-(defun +project-root-initialize ()
-  "Initialize project list from `+project-root-wildcards'."
-  (interactive)
-  (project-forget-zombie-projects (called-interactively-p 'interactive)) ; Forget non-existent projects
-  (mapc ; Remember some Git repositories as projects by path
-   (lambda (dir)
-     (when-let* ((project (project--find-in-directory dir)))
-       (project-remember-project project)))
-   (seq-filter #'file-directory-p (mapcan #'file-expand-wildcards +project-root-wildcards))))
 
 
 
