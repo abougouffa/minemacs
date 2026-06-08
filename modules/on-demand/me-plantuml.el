6813ed128191dd2df95fba01767aa2a1ff63038a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-06-08 11:27:52 +0200

    fix(on-demand/plantuml): update config, fix capf and bump .jar version

--- modules/on-demand/me-plantuml.el
+++ modules/on-demand/me-plantuml.el
@@ -1,10 +1,10 @@
 ;;; me-plantuml.el --- PlantUML -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+;; Copyright (C) 2022-2026  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-08-10
-;; Last modified: 2026-01-25
+;; Last modified: 2026-06-08
 
 ;;; Commentary:
 
@@ -12,39 +12,28 @@
 
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-plantuml
-  :auto-mode '(("\\.\\(plantuml\\|pum\\|plu\\)\\'" . plantuml-mode)))
+  :auto-mode '(("\\.\\(plantuml\\|pum\\|plu\\|puml\\)\\'" . plantuml-mode)))
 
 
 ;; Major mode for PlantUML
 (use-package plantuml-mode
   :straight t
-  :hook (plantuml-mode . +plantuml-mode-setup)
+  :hook (plantuml-mode . +plantuml--fix-completion-at-point)
   :custom
   (plantuml-indent-level 2)
+  :preface
+  ;; FIX: `plantuml-mode' includes a Capf function, but by default, it hooks the
+  ;; legacy `plantuml-complete-symbol' in `completion-at-point-functions', which
+  ;; is very annoying
+  (defun +plantuml--fix-completion-at-point ()
+    (remove-hook 'completion-at-point-functions #'plantuml-complete-symbol t)
+    (add-hook 'completion-at-point-functions #'plantuml-completion-at-point-function nil t))
   :config
   ;; Use the executable if available or automatically download the latest version of PlantUML
   (if (executable-find "plantuml")
       (setopt plantuml-default-exec-mode 'executable)
     (setopt plantuml-default-exec-mode 'jar
-            plantuml-jar-path (+github-download-release "plantuml/plantuml" "plantuml-\\([[:digit:]]*\\.\\)\\{3\\}jar" nil :ver "v1.2026.1")))
-
-  ;; Define `capf' function, based on `plantuml-complete-symbol'
-  (defun +plantuml-completion-at-point ()
-    "Perform symbol-at-pt completion on word before cursor."
-    (when (derived-mode-p 'plantuml-mode) ; do not fire up on other modes
-      (let* ((end-pos (point))
-             (sym-at-pt (or (thing-at-point 'symbol) ""))
-             (max-match (try-completion sym-at-pt plantuml-kwdList)))
-        (unless (null max-match)
-          (list (- end-pos (length sym-at-pt))
-                end-pos
-                (if (eq max-match t)
-                    (list keyword)
-                  (all-completions sym-at-pt plantuml-kwdList)))))))
-
-  ;; Add support for `capf'
-  (defun +plantuml-mode-setup ()
-    (add-to-list 'completion-at-point-functions #'+plantuml-completion-at-point)))
+            plantuml-jar-path (+github-download-release "plantuml/plantuml" "plantuml-\\([[:digit:]]*\\.\\)\\{3\\}jar" nil :ver "v1.2026.5"))))
 
 
 ;; Add `flymake' support for editing PlantUML files
