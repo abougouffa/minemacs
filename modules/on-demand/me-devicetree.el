f77583d5a07be149c20b4ff0c04fd2c7759cf767
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-07-16 13:05:48 +0200

    feat(on-demand/devicetree): add support for `devicetree-ts-mode`

--- modules/on-demand/me-devicetree.el
+++ modules/on-demand/me-devicetree.el
@@ -1,10 +1,10 @@
 ;;; me-devicetree.el --- Devicetree support -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+;; Copyright (C) 2022-2026  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-08-10
-;; Last modified: 2025-03-21
+;; Last modified: 2026-07-16
 
 ;;; Commentary:
 
@@ -12,13 +12,22 @@
 
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-devicetree
-  :auto-mode '(("\\.dtsi?\\'" . dts-mode)
+  :auto-mode '(("\\.dtsi?\\'" . devicetree-ts-mode)
                (".+\\.dtb\\|dtbo\\'" . virtual-dts-mode)))
 
 
 ;; Major mode for DeviceTree source code
 (use-package dts-mode
   :straight t)
+
+
+;; Tree-sitter support for DeviceTree
+(use-package devicetree-ts-mode
+  :straight t
+  :when (featurep 'feat/tree-sitter)
+  :config
+  (add-to-list 'treesit-language-source-alist '(devicetree "https://github.com/joelspadin/tree-sitter-devicetree"))
+  (treesit-ensure-installed 'devicetree))
 
 
 ;; Major mode for Device Tree Binary (`*.dtb') files
