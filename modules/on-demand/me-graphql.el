0cd0494b8dec1e9ef4b95d7067c9951d5cae10e5
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-07-16 13:23:10 +0200

    feat(on-demand/graphql): add support for `graphql-ts-mode`

--- modules/on-demand/me-graphql.el
+++ modules/on-demand/me-graphql.el
@@ -1,10 +1,10 @@
 ;;; me-graphql.el --- GraphQL -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Abdelhak Bougouffa
+;; Copyright (C) 2022-2026  Abdelhak Bougouffa
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-08-10
-;; Last modified: 2025-03-21
+;; Last modified: 2026-07-16
 
 ;;; Commentary:
 
@@ -12,12 +12,21 @@
 
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-graphql
-  :auto-mode '((("\\.gql\\'" "\\.graphql\\'") . graphql-mode)))
+  :auto-mode '((("\\.gql\\'" "\\.graphqls?\\'") . graphql-mode)))
 
 
 ;; Major mode for editing GraphQL schemas
 (use-package graphql-mode
   :straight t)
+
+
+;; Tree-sitter based GraphQL mode
+(use-package graphql-ts-mode
+  :straight t
+  :when (featurep 'feat/tree-sitter)
+  :config
+  (add-to-list 'treesit-language-source-alist '(graphql "https://github.com/bkegley/tree-sitter-graphql"))
+  (treesit-ensure-installed 'graphql))
 
 
 (provide 'on-demand/me-graphql)
