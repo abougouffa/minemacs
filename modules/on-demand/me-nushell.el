80c69897f6b59c51214f6233e4187d4c4f40340a
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-08-07 13:28:31 +0200

    tweak(on-demand/nushell): use the new tree-sitter mode when possible

--- modules/on-demand/me-nushell.el
+++ modules/on-demand/me-nushell.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa  (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024-11-30
-;; Last modified: 2026-04-27
+;; Last modified: 2026-08-07
 
 ;;; Commentary:
 
@@ -12,13 +12,23 @@
 
 ;;;###autoload
 (minemacs-register-on-demand-module 'me-nushell
-  :auto-mode '(("\\.nu\\'" . nushell-mode))
-  :interpreter-mode '(("nu" . nushell-mode)))
+  :auto-mode '(("\\.nu\\'" . nushell-ts-mode))
+  :interpreter-mode '(("nu" . nushell-ts-mode)))
+
+
+;; Major mode for Nushell scripts (deprecated, used as a fallback if case `treesit' is not available)
+(use-package nushell-mode
+  :straight t
+  :unless (featurep 'feat/tree-sitter))
 
 
 ;; Major mode for Nushell scripts
-(use-package nushell-mode
-  :straight t)
+(use-package nushell-ts-mode
+  :straight t
+  :when (featurep 'feat/tree-sitter)
+  :config
+  (add-to-list 'treesit-language-source-alist '(nu "https://github.com/nushell/tree-sitter-nu"))
+  (treesit-ensure-installed 'nu))
 
 
 (provide 'on-demand/me-nushell)
