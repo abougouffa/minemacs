6af00308f9fc94ddf0b692f70108359259ddaa11
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2026-08-19 11:28:34 +0200

    tweak(hideshow): re-bind `hs-prefix-map` instead of individual commands

--- core/me-builtin.el
+++ core/me-builtin.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abdelhak Bougouffa (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2023-03-26
-;; Last modified: 2026-08-15
+;; Last modified: 2026-08-19
 
 ;;; Commentary:
 
@@ -449,20 +449,9 @@
   :hook ((prog-mode conf-mode nxml-mode) . +hs-minor-mode-maybe) ; Hide/show code blocks, a.k.a. code folding
   :custom
   (hs-hide-comments-when-hiding-all nil)
-  :bind (:map hs-minor-mode-map
-              ("C-c @ C-f"     . +hs-toggle-all)
-              ;; The key "à = @" in AZERTY keyboards, the @ need "Alt Gr" to insert it
-              ("C-c C-à C-f"   . +hs-toggle-all)
-              ("C-c C-à C-h"   . hs-hide-block)
-              ("C-c C-à C-s"   . hs-show-block)
-              ("C-c C-à C-M-h" . hs-hide-all)
-              ("C-c C-à C-M-s" . hs-show-all)
-              ("C-c C-à C-l"   . hs-hide-level)
-              ("C-c C-à C-c"   . hs-toggle-hiding)
-              ("C-c C-à C-a"   . hs-show-all)
-              ("C-c C-à C-t"   . hs-hide-all)
-              ("C-c C-à C-d"   . hs-hide-block)
-              ("C-c C-à C-e"   . hs-toggle-hiding))
+  ;; The key "à" is the same as "@" in AZERTY keyboards, the @ need "Alt Gr" to insert it
+  :bind (:map hs-minor-mode-map ("C-c C-à" . hs-prefix-map))
+  :bind (:map hs-prefix-map ("C-f" . +hs-toggle-all))
   :init
   (defun +hs-minor-mode-maybe () ; Fail sailently
     (condition-case err (hs-minor-mode 1) (error (+log! "`hs-minor-mode': %s" (error-message-string err)))))
